b574570f13e6c84b7b8c20e5f56db5d327ece385
matheusps <cout.matheusps@gmail.com>
2020-09-02 18:12:08 -0300

    fix(font): bundle within code

--- styleguides/admin-ui/tsdx.config.js
+++ styleguides/admin-ui/tsdx.config.js
@@ -3,7 +3,7 @@ const url = require('@rollup/plugin-url')
 module.exports = {
   rollup(config) {
     config.plugins = [
-      url({ include: ['**/*.woff', '**/*.woff2'] }),
+      url({ include: ['**/*.woff', '**/*.woff2'], limit: Infinity }),
       ...config.plugins,
     ]
 
