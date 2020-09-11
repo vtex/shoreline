3503ab43b466a1f28ab1a7d10299e20cffad7eaf
matheusps <cout.matheusps@gmail.com>
2020-09-11 15:53:53 -0300

    test(card): add card test

--- styleguides/admin-ui/src/components/Card/index.tsx
+++ styleguides/admin-ui/src/components/Card/index.tsx
@@ -1,33 +1,37 @@
-import React, { Ref } from 'react'
+import React, { ReactNode, Ref } from 'react'
 import { forwardRef } from '@vtex-components/utils'
 import { SxStyleProp } from 'theme-ui'
 
 import { Box } from '../Box'
 
-export const Card = forwardRef(
-  (
-    { children, sx }: React.PropsWithChildren<CardProps>,
-    ref: Ref<HTMLDivElement>
-  ) => {
-    return (
-      <Box
-        sx={{
-          padding: 13,
-          backgroundColor: 'background',
-          border: 'solid',
-          borderColor: 'muted.3',
-          borderWidth: 1,
-          borderRadius: 3,
-          ...sx,
-        }}
-        ref={ref}
-      >
-        {children}
-      </Box>
-    )
-  }
-)
+export const Card = forwardRef((props: CardProps, ref: Ref<HTMLDivElement>) => {
+  const { sx, children } = props
+
+  return (
+    <Box
+      sx={{
+        padding: 13,
+        backgroundColor: 'background',
+        border: 'solid',
+        borderColor: 'muted.3',
+        borderWidth: 1,
+        borderRadius: 3,
+        ...sx,
+      }}
+      ref={ref}
+    >
+      {children}
+    </Box>
+  )
+})
 export interface CardProps {
-  /** ThemeUI style props */
-  sx: SxStyleProp
+  /**
+   * ThemeUI style props
+   * @default {}
+   * */
+  sx?: SxStyleProp
+  /**
+   * React children prop
+   * */
+  children?: ReactNode
 }
