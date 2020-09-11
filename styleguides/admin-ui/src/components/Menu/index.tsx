d0f1a50c34456b2147e4cc5252b52faf177c7555
matheusps <cout.matheusps@gmail.com>
2020-09-11 14:16:19 -0300

    feat(menu): add menu component to admin-ui

--- styleguides/admin-ui/src/components/Menu/index.tsx
+++ styleguides/admin-ui/src/components/Menu/index.tsx
@@ -1,24 +1,74 @@
 /** @jsx jsx */
-import { jsx, Flex } from 'theme-ui'
-import { Fragment, cloneElement } from 'react'
+import { jsx, Flex, SxStyleProp } from 'theme-ui'
+import {
+  Fragment,
+  cloneElement,
+  Children,
+  ReactElement,
+  FunctionComponentElement,
+  Ref,
+  forwardRef,
+} from 'react'
 import {
   useMenuState,
-  Menu as BaseMenu,
+  Menu as RekitMenu,
   MenuItem,
   MenuButton,
+  MenuState,
 } from 'reakit/Menu'
-import { get } from '@vtex-components/theme'
+import { get, mergeSx } from '@vtex-components/theme'
 
-export function Menu({ disclosure, items, ...props }: any) {
-  const menu = useMenuState()
+import { Button, ButtonProps } from '../Button'
+
+export interface MenuProps extends Pick<MenuState, 'placement'> {
+  disclosure: FunctionComponentElement<unknown>
+  children: ReactElement[]
+  label: string
+  sx?: SxStyleProp
+  disabled?: boolean
+  loop?: boolean
+}
+
+function Menu(props: MenuProps) {
+  const {
+    disclosure,
+    children,
+    label,
+    disabled = false,
+    placement = 'bottom',
+    loop = false,
+    sx = {},
+    ...baseProps
+  } = props
+
+  const menu = useMenuState({ orientation: 'vertical', placement, loop })
+
+  const styles = mergeSx<SxStyleProp>(
+    {
+      flexDirection: 'column',
+      background: (theme) => get(theme, 'colors.background'),
+      border: (theme) =>
+        `${get(theme, 'sizes.1')} solid ${get(theme, 'colors.muted.2')}`,
+      marginTop: 4,
+      padding: 5,
+      minWidth: 20,
+      borderRadius: 3,
+    },
+    sx
+  )
 
   return (
     <Fragment>
-      <MenuButton {...menu} ref={disclosure.ref} {...disclosure.props}>
+      <MenuButton
+        {...menu}
+        disabled={disabled}
+        ref={disclosure.ref}
+        {...disclosure.props}
+      >
         {(disclosureProps) => cloneElement(disclosure, disclosureProps)}
       </MenuButton>
-      <BaseMenu
-        aria-label="custom menu"
+      <RekitMenu
+        aria-label={label}
         sx={{
           border: 0,
           background: 'none',
@@ -26,29 +76,42 @@ export function Menu({ disclosure, items, ...props }: any) {
           outline: 'none',
         }}
         {...menu}
-        {...props}
+        {...baseProps}
+        disabled={disabled}
       >
-        <Flex
-          sx={{
-            flexDirection: 'column',
-            justifyItems: 'center',
-            justifyContent: 'center',
-            background: (theme) => get(theme, 'colors.background'),
-            border: (theme) =>
-              `${get(theme, 'sizes.1')} solid ${get(theme, 'colors.muted.2')}`,
-            marginTop: 4,
-            padding: 5,
-            minWidth: 19,
-            borderRadius: 4,
-          }}
-        >
-          {items.map((item: any, i: number) => (
-            <MenuItem {...menu} {...item.props} key={i}>
-              {(itemProps) => cloneElement(item, itemProps)}
+        <Flex sx={styles}>
+          {Children.map(children, (child, index) => (
+            <MenuItem {...menu} {...child.props} key={index}>
+              {(itemProps) => cloneElement(child, itemProps)}
             </MenuItem>
           ))}
         </Flex>
-      </BaseMenu>
+      </RekitMenu>
     </Fragment>
   )
 }
+
+export type MenuItemProps = Omit<ButtonProps, 'variant' | 'iconPosition'>
+
+function Item(props: MenuItemProps, ref: Ref<HTMLButtonElement>) {
+  const { sx = {}, ...buttonProps } = props
+  const styles = mergeSx<SxStyleProp>(
+    {
+      color: 'text',
+      ':hover': {
+        color: 'text',
+      },
+      width: 'full',
+      div: {
+        justifyContent: 'flex-start',
+      },
+    },
+    sx
+  )
+
+  return <Button ref={ref} sx={styles} variant="subtle" {...buttonProps} />
+}
+
+Menu.Item = forwardRef(Item)
+
+export { Menu }
