ab2261f25a125a5a54f32554315fcaeedbacc5e8
marianabianca <maridbianca@gmail.com>
2020-09-11 10:53:53 -0300

    refactor: add changes suggested in review
    - Add placement, visible and arrow props, and change the code to work with these props

--- components/tooltip/src/index.tsx
+++ components/tooltip/src/index.tsx
@@ -1,40 +1,107 @@
-import React, { ReactElement, PropsWithRef } from 'react'
+/** @jsx jsx */
+import { jsx, Box, SxStyleProp } from 'theme-ui'
+import { FunctionComponentElement, Fragment, cloneElement } from 'react'
 import {
   useTooltipState,
   Tooltip as ReakitTooltip,
   TooltipReference,
   TooltipProps as ReakitProps,
-  TooltipArrow,
 } from 'reakit/Tooltip/'
-import { Box, SxStyleProp } from 'theme-ui'
+import { useComponentSx, mergeSx } from '@vtex-components/theme'
 
+export type Placement =
+  | 'auto-start'
+  | 'auto'
+  | 'auto-end'
+  | 'top-start'
+  | 'top'
+  | 'top-end'
+  | 'right-start'
+  | 'right'
+  | 'right-end'
+  | 'bottom-end'
+  | 'bottom'
+  | 'bottom-start'
+  | 'left-end'
+  | 'left'
+  | 'left-start'
+
+/**
+ * Elementary tooltip component that can be reused by all VTEX Styleguides.
+ * You can use reakit full features (except the 'as' prop) and theme-ui's sx.
+ * It renders a tooltip element by default.
+ * This is a styled base component, so any system can theme it.
+ * You may configure your `components.tooltip` property of the theme object.
+ * @example
+ * ```jsx
+ * import { Button, ButtonProps } from `@vtex-components/button`
+ *
+ * const theme = {
+ *  components: {
+ *    tooltip: { ... }
+ *  }
+ * }
+ *
+ * <Tooltip label="Tooltip text here"><button>Children</button></Tooltip>
+ * ```
+ */
 function Tooltip(props: TooltipProps) {
-  const { sx = {}, children, label, ...tooltipProps } = props
-  const tooltip = useTooltipState({ placement: 'bottom' })
+  const {
+    sx = {},
+    children,
+    label,
+    arrow,
+    placement,
+    visible,
+    ...tooltipProps
+  } = props
+
+  const tooltip = useTooltipState({ placement: placement ?? 'top', visible })
+
+  const styles = useComponentSx('tooltip', {})
 
   return (
-    <>
-      <TooltipReference {...tooltip} {...children.props}>
-        {(referenceProps) =>
-          React.cloneElement(children, { ...referenceProps })
-        }
+    <Fragment>
+      <TooltipReference {...tooltip} {...children.props} ref={children.ref}>
+        {(referenceProps) => cloneElement(children, { ...referenceProps })}
       </TooltipReference>
       <ReakitTooltip {...tooltip} {...tooltipProps}>
-        <TooltipArrow {...tooltip} />
-        <Box sx={sx}>{label}</Box>
+        {arrow && cloneElement(arrow, { ...tooltip })}
+        <Box sx={mergeSx<SxStyleProp>(styles, sx)}>{label}</Box>
       </ReakitTooltip>
-    </>
+    </Fragment>
   )
 }
 
 export interface TooltipProps extends Omit<ReakitProps, 'as'> {
   /**
+   * The element that triggers the tooltip
+   */
+  children: FunctionComponentElement<unknown>
+  /**
+   * Text shown inside the tooltip
+   */
+  label: string
+  /**
+   * Arrow component
+   * @default {}
+   */
+  arrow?: FunctionComponentElement<unknown>
+  /**
    * ThemeUI sx prop
    * @default {}
    */
-  children: ReactElement<PropsWithRef<{}>>
   sx?: SxStyleProp
-  label: string
+  /**
+   * The size of the arrow, if the arrow is enabled
+   * @default The reakit default size
+   */
+  placement?: Placement
+  /**
+   * Whether the tooltip is visible or not
+   * @default false
+   */
+  visible?: boolean
 }
 
 export default Tooltip
