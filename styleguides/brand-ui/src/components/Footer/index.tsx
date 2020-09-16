032e46ba821411b109f95b5b4393b255f9b02de8
Lucas Medeiros <lucasmed812@gmail.com>
2020-09-16 08:42:55 -0300

    chore: change footer border-color for rgba

--- styleguides/brand-ui/src/components/Footer/index.tsx
+++ styleguides/brand-ui/src/components/Footer/index.tsx
@@ -181,7 +181,7 @@ const SocialMediaIcon = forwardRef(
 const Extra = ({ children }: PropsWithChildren<{}>) => (
   <Box
     sx={{
-      borderTop: '1px solid #222C44',
+      borderTop: '1px solid rgba(255, 255, 255, 0.06)',
     }}
   >
     <Flex
