fd01c92754a0a8858e50b05928ebe90d45c1784f
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-09-23 19:44:30 -0300

    fix: remove theme from index

--- styleguides/brand-ui/src/index.ts
+++ styleguides/brand-ui/src/index.ts
@@ -1,6 +1,5 @@
 export { ThemeProvider } from './theme'
 export { Box, Flex } from 'theme-ui'
-export { useTheme, Theme } from '@vtex-components/theme'
 export { Button, ButtonProps } from './components/Button'
 export { FooterLanding } from './components/FooterLanding'
 export {
