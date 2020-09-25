de860d0cd91c21f86de02b12ba827ef43fd6142a
lucasaarcoverde <lucasaarcoverde@gmail.com>
2020-09-25 10:10:48 -0300

    fix(card): styling with tokens not working

--- styleguides/admin-ui/src/components/Card/index.tsx
+++ styleguides/admin-ui/src/components/Card/index.tsx
@@ -1,16 +1,21 @@
-import styled from '@emotion/styled'
+import React, { Ref } from 'react'
+import { forwardRef } from '@vtex-components/utils'
 
 import { Box, BoxProps as CardProps } from '../Box'
 
-export const Card = styled(Box)<CardProps>``
-
-Card.defaultProps = {
-  bg: 'background',
-  bc: 'muted.3',
-  p: '13',
-  bw: '1',
-  bs: 'solid',
-  br: '3',
-}
+export const Card = forwardRef((props: CardProps, ref: Ref<HTMLDivElement>) => {
+  return (
+    <Box
+      ref={ref}
+      bg="background"
+      bc="muted.3"
+      p="13"
+      bw="1"
+      bs="solid"
+      br="3"
+      {...props}
+    />
+  )
+})
 
 export { CardProps }
