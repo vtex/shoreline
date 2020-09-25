9a10682b9b748426ebbd93db16127eb9bfc8998a
matheusps <cout.matheusps@gmail.com>
2020-09-25 12:02:45 -0300

    fix(build): fix 0.40.2

--- styleguides/admin-ui/src/components/Menu/Stateless.tsx
+++ styleguides/admin-ui/src/components/Menu/Stateless.tsx
@@ -140,7 +140,7 @@ export interface StatelessMenuProps {
   /**
    * Menu items
    */
-  children: ReactElement[] | ReactElement
+  children: ReactElement | ReactElement[]
   /**
    * aria-label of menu
    */
