35e8853884ca79747ba61d70fa592c720f08e598
lucasaarcoverde <lucasaarcoverde@gmail.com>
2020-10-08 17:51:06 -0300

    chore(doc-template): add doc template path to storybook config

--- styleguides/admin-ui/.storybook/main.js
+++ styleguides/admin-ui/.storybook/main.js
@@ -1,5 +1,9 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../templates/*.stories.mdx',
+  ],
   addons: [
     '@storybook/addon-links',
     {
