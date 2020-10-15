ff792453fd3b24989d06d6bd7f1d7d73b5d2dd20
Lucas Medeiros <lucasmed812@gmail.com>
2020-10-15 16:02:46 -0300

    refactor: change props on card footer, header, and body

--- styleguides/brand-ui/src/components/Card/index.tsx
+++ styleguides/brand-ui/src/components/Card/index.tsx
@@ -3,15 +3,21 @@ import { Flex, Box, BoxProps, Image as ReakitImage, ImageProps } from 'theme-ui'
 import React, { Ref, PropsWithChildren } from 'react'
 import { forwardRef } from '@vtex-components/utils'
 
-const Header = (props: BoxProps) => <Box {...props} variant="card.header" />
+const Header = (props: PropsWithChildren<BoxProps>) => (
+  <Box {...props} variant="card.header" />
+)
 
-const Body = (props: BoxProps) => <Box {...props} variant="card.body" />
+const Body = (props: PropsWithChildren<BoxProps>) => (
+  <Box {...props} variant="card.body" />
+)
 
 const Image = forwardRef((props: ImageProps, ref: Ref<HTMLImageElement>) => (
   <ReakitImage {...props} ref={ref} />
 ))
 
-const Footer = (props: BoxProps) => <Flex {...props} variant="card.footer" />
+const Footer = (props: PropsWithChildren<BoxProps>) => (
+  <Flex {...props} variant="card.footer" />
+)
 
 export const Card = ({
   el = 'div',
