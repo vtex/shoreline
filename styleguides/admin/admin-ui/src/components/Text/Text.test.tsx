a6ec650a931a6bcaf0800586a4397e1a44b2d435
matheusps <cout.matheusps@gmail.com>
2020-10-28 07:11:35 -0300

    chore(avatar): migrate avatar to new structure
    now tests will run after a push

--- styleguides/admin/admin-ui/src/components/Text/Text.test.tsx
+++ styleguides/admin/admin-ui/src/components/Text/Text.test.tsx
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
 import serializer from 'jest-emotion'
 
-import { ThemeProvider } from '../../theme'
+import { ThemeProvider } from '../../system'
 import { Text, TextVariant } from './index'
 
 expect.addSnapshotSerializer(serializer)
