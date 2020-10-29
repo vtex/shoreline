a196b0ef45ea32e999ef170bdf34d6e915050ec9
matheusps <cout.matheusps@gmail.com>
2020-10-29 07:46:32 -0300

    feat(heading): add component

--- styleguides/admin/admin-ui/src/components/Heading/index.tsx
+++ styleguides/admin/admin-ui/src/components/Heading/index.tsx
@@ -1,18 +1,19 @@
 import { Box as ReakitBox } from 'reakit'
 import { ReactNode, forwardRef, Ref } from 'react'
 import { createElement } from '@vtex/admin-ui-system'
+import { TextPattern } from '@vtex/admin-ui-theme'
 
-import { useHeadingLevel } from './context'
 import { useComponent } from '../../hooks/useComponent'
+import { Overridable } from '../../types'
 
-export const unstableHeading = forwardRef(function Heading(
+export const Heading = forwardRef(function Heading(
   props: HeadingProps,
   ref: Ref<HTMLHeadingElement>
 ) {
-  const headingProps = useComponent({ props: { text: 'headline', ...props } })
-  const level = useHeadingLevel()
-
-  const element = props.element ?? `h${level}`
+  const { element, ...htmlProps } = props
+  const headingProps = useComponent({
+    props: { text: 'headline', ...htmlProps },
+  })
 
   return createElement({
     ref,
@@ -22,7 +23,14 @@ export const unstableHeading = forwardRef(function Heading(
   })
 })
 
-export interface HeadingProps {
+export interface HeadingProps extends Overridable, TextPattern {
+  /**
+   * Element to render
+   * @default h1
+   */
   element?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+  /**
+   * heading children
+   */
   children?: ReactNode
 }
