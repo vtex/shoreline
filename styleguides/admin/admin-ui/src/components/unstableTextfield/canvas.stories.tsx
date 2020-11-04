0ff0e862122f66ac0d092c21c6abaa34be0e3195
lucasaarcoverde <lucasaarcoverde@gmail.com>
2020-11-04 11:51:55 -0300

    feat(text-field): add TextField component

--- styleguides/admin/admin-ui/src/components/unstableTextfield/canvas.stories.tsx
+++ styleguides/admin/admin-ui/src/components/unstableTextfield/canvas.stories.tsx
@@ -1,166 +1,83 @@
-import React, { useRef } from 'react'
-import { Story, Meta } from '@storybook/react'
+import React, { useState } from 'react'
+import { Meta } from '@storybook/react'
 
 import { unstableTextField as TextField } from './index'
+import { useInputState } from '../unstableInput'
+import { IconBook } from '../../icons'
+import { Box } from '../Box'
 
 export default {
   title: 'system-next/text-field',
 } as Meta
 
-export const Field: Story = () => {
-  return <TextField id="username" label="Username" />
-}
-
-export const Ref: Story = () => {
-  const ref = useRef<HTMLInputElement>(null)
-
-  const handleFocus = () => {
-    if (ref.current) {
-      ref.current.focus()
-    }
-  }
+export const Example = () => {
+  const [value, setValue] = useState('')
 
   return (
-    <>
-      <TextField ref={ref} id="with-ref" label="With Ref" />
-      <button onClick={handleFocus}>Focus</button>
-    </>
+    <Box width={300}>
+      <TextField
+        state={{
+          value,
+          onChange: (e) => setValue(e.target.value),
+          onClear: () => setValue(''),
+        }}
+        id="textfield-1"
+        label="Label"
+        helperText="Helper Text"
+        charLimit={120}
+      />
+    </Box>
   )
 }
 
-// export const Sx: Story = () => {
-//   return (
-//     <ThemeProvider >
-//       <Box
-//         sx={{
-//           fontSize: 64,
-//         }}
-//       >
-//         Huge Text
-//       </Box>
-//     </ThemeProvider>
-//   )
-// }
+export const UsingInputHook = () => {
+  const input = useInputState()
 
-// export const ConsumeTheme: Story = () => {
-//   return (
-//     <ThemeProvider >
-//       <Box
-//         sx={{
-//           fontSize: 64,
-//           bg: 'primary.base',
-//           color: 'primary.contrast',
-//           borderRadius: 4,
-//         }}
-//       >
-//         Primary Box
-//       </Box>
-//     </ThemeProvider>
-//   )
-// }
+  return (
+    <Box width={300}>
+      <TextField
+        state={input}
+        id="textfield-2"
+        label="Label"
+        helperText="Helper Text"
+        charLimit={120}
+        suffix="Kg"
+        icon={<IconBook />}
+      />
+    </Box>
+  )
+}
 
-// export const CustomTheme: Story = () => {
-//   return (
-//     <ThemeProvider
-//       theme={{
-//         ...unstableTheme,
-//         box: {
-//           default: {
-//             bg: 'background',
-//             color: 'text',
-//             padding: 4,
-//             margin: 2,
-//           },
-//           inverted: {
-//             bg: 'text',
-//             color: 'background',
-//             padding: 4,
-//             margin: 2,
-//           },
-//         },
-//       }}
-//     >
-//       <Box
-//         sx={{
-//           variant: 'box.default',
-//         }}
-//       >
-//         Default Box
-//       </Box>
-//       <Box
-//         sx={{
-//           variant: 'box.inverted',
-//         }}
-//       >
-//         Inverted Box
-//       </Box>
-//     </ThemeProvider>
-//   )
-// }
+export const Basic = () => {
+  const input = useInputState()
 
-// export const StyleProps: Story = () => {
-//   return (
-//     <ThemeProvider >
-//       <Box
-//         bg="primary.base"
-//         color="primary.contrast"
-//         borderRadius="3"
-//         sx={{
-//           variant: 'text.headline',
-//         }}
-//       >
-//         Primary Box
-//       </Box>
-//     </ThemeProvider>
-//   )
-// }
+  return (
+    <Box width={300}>
+      <TextField
+        state={input}
+        id="textfield-3"
+        label="Label"
+        helperText="Helper Text"
+        charLimit={120}
+      />
+    </Box>
+  )
+}
 
-// export const Ref: Story = () => {
-//   const ref = useRef<HTMLInputElement>(null)
-//   const handleFocus = () => {
-//     if (ref.current) {
-//       ref.current.focus()
-//     }
-//   }
+export const Error = () => {
+  const input = useInputState()
 
-//   return (
-//     <ThemeProvider >
-//       <Box ref={ref} borderRadius="3" borderStyle="solid" element="section" />
-//       <button onClick={handleFocus}>Focus</button>
-//     </ThemeProvider>
-//   )
-// }
-
-// export const FullUse: Story = () => {
-//   const ref = useRef<HTMLInputElement>(null)
-//   const handleFocus = () => {
-//     if (ref.current) {
-//       ref.current.focus()
-//     }
-//   }
-
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <Box
-//         ref={ref}
-//         borderRadius={4}
-//         borderStyle="solid"
-//         fontSize={18}
-//         use={Input}
-//         type="text"
-//       />
-//       <button onClick={handleFocus}>Focus</button>
-//     </ThemeProvider>
-//   )
-// }
-
-// export const Flexbox: Story = () => {
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <Box display="flex" width="100%" direction="column">
-//         <Box width="20%" height={120} bg="primary" />
-//         <Box width="20%" height={120} bg="secondary" />
-//       </Box>
-//     </ThemeProvider>
-//   )
-// }
+  return (
+    <Box width={300}>
+      <TextField
+        state={input}
+        id="textfield-4"
+        label="Label"
+        errorMessage="Error Message"
+        charLimit={120}
+        suffix="Kg"
+        icon={<IconBook />}
+      />
+    </Box>
+  )
+}
