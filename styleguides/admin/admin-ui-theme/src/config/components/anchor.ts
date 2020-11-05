06fb7dd9a49de7f63fd4eaa8314cb86993004292
ingridpitta <ingridpitta056@gmail.com>
2020-11-05 12:03:24 -0300

    feat: anchor [WIP]

---
+++ styleguides/admin/admin-ui-theme/src/config/components/anchor.ts
@@ -0,0 +1,13 @@
+export default {
+  font: 'inherit',
+  color: 'primary.base',
+  textDecoration: 'none',
+  ':active': {
+    color: 'primary.active',
+    textDecoration: 'underline',
+  },
+  ':hover': {
+    color: 'primary.hover',
+    textDecoration: 'underline',
+  },
+}
