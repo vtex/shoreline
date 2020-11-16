51c5cc36b0dd70819fa69ff7de7d92cd2880e51b
JulianaMeyerArruda <59611647+JulianaMeyerArruda@users.noreply.github.com>
2020-11-16 19:03:53 -0300

    Update DocsBackNext.tsx

--- styleguides/admin/admin-site/src/components/DocsBackNext.tsx
+++ styleguides/admin/admin-site/src/components/DocsBackNext.tsx
@@ -29,7 +29,7 @@ export default function DocsBackNext({
   return (
     <Box
       styles={{
-        color: 'text',
+        color: 'text.primary',
         li: {
           listStyle: 'none',
         },
