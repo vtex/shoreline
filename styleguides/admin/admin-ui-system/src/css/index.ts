0cbee1c8aa35cba5b56b18786cfce6ca263562a5
matheusps <cout.matheusps@gmail.com>
2020-11-30 17:44:32 -0300

    feat(system): create new style resolver

---
+++ styleguides/admin/admin-ui-system/src/css/index.ts
@@ -0,0 +1,168 @@
+import {
+  CSSObject,
+  StyleProp,
+  ThemeDerivedStyles,
+  Theme,
+  StyleObject,
+} from './types'
+import { scales, Scales } from './scales'
+import { get } from '../util'
+
+export const defaultBreakpoints = [40, 52, 64].map((n) => `${n}em`)
+
+const defaultTheme = {
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72],
+}
+
+const aliases = {
+  bg: 'backgroundColor',
+} as const
+
+type Aliases = typeof aliases
+
+export const multiples = {
+  marginX: ['marginLeft', 'marginRight'],
+  marginY: ['marginTop', 'marginBottom'],
+  paddingX: ['paddingLeft', 'paddingRight'],
+  paddingY: ['paddingTop', 'paddingBottom'],
+  size: ['width', 'height'],
+}
+
+const transformations = [
+  'margin',
+  'marginTop',
+  'marginRight',
+  'marginBottom',
+  'marginLeft',
+  'marginX',
+  'marginY',
+  'marginBlock',
+  'marginBlockEnd',
+  'marginBlockStart',
+  'marginInline',
+  'marginInlineEnd',
+  'marginInlineStart',
+  'top',
+  'bottom',
+  'left',
+  'right',
+].reduce(
+  (acc, curr) => ({
+    ...acc,
+    /** Transform negative values */
+    [curr]: (scale: object, value: string | number) => {
+      if (typeof value !== 'number' || value >= 0) {
+        if (typeof value === 'string' && value.startsWith('-')) {
+          const valueWithoutMinus = value.substring(1)
+          const n = get(scale, valueWithoutMinus, valueWithoutMinus)
+
+          return `-${n}`
+        }
+
+        return get(scale, value, value)
+      }
+
+      const absolute = Math.abs(value)
+      const n = get(scale, absolute, absolute)
+
+      if (typeof n === 'string') return `-${n}`
+
+      return Number(n) * -1
+    },
+  }),
+  {}
+)
+
+const responsive = (styles: Exclude<StyleProp, ThemeDerivedStyles>) => (
+  theme?: Theme
+) => {
+  const next: Exclude<StyleProp, ThemeDerivedStyles> = {}
+  const breakpoints =
+    (theme && (theme.breakpoints as string[])) || defaultBreakpoints
+
+  const mediaQueries = [
+    null,
+    ...breakpoints.map((n) => `@media screen and (min-width: ${n})`),
+  ]
+
+  for (const k in styles) {
+    const key = k as keyof typeof styles
+    let value = styles[key]
+
+    if (typeof value === 'function') {
+      value = value(theme ?? {})
+    }
+
+    if (value == null) continue
+
+    if (!Array.isArray(value)) {
+      next[key] = value
+      continue
+    }
+
+    for (let i = 0; i < value.slice(0, mediaQueries.length).length; i++) {
+      const media = mediaQueries[i]
+
+      if (!media) {
+        next[key] = value[i]
+        continue
+      }
+
+      next[media] = next[media] ?? {}
+      if (value[i] == null) continue
+      ;(next[media] as Record<string, unknown>)[key] = value[i]
+    }
+  }
+
+  return next
+}
+
+type CssPropsArgument = { theme: Theme } | Theme
+
+export const css = (args: StyleProp = {}) => (
+  props: CssPropsArgument = {}
+): CSSObject => {
+  const theme: Theme = {
+    ...defaultTheme,
+    ...('theme' in props ? props.theme : props),
+  }
+
+  const result: CSSObject = {}
+  let obj = typeof args === 'function' ? args(theme) : args
+
+  if (obj.themeKey) {
+    obj = { ...get(theme, obj.themeKey), ...obj }
+    delete obj.themeKey
+  }
+
+  const styles = responsive(obj)(theme)
+
+  for (const key in styles) {
+    const x = styles[key as keyof typeof styles]
+    const val = typeof x === 'function' ? x(theme) : x
+
+    if (val && typeof val === 'object') {
+      result[key] = css(val as StyleObject)(theme)
+      continue
+    }
+
+    const prop = key in aliases ? aliases[key as keyof Aliases] : key
+    const scaleName = prop in scales ? scales[prop as keyof Scales] : undefined
+    const scale = get(theme, scaleName as string, get(theme, prop, {}))
+    const transform: Function = get(transformations, prop, get)
+    const value = transform(scale, val, val)
+
+    if (prop in multiples) {
+      const dirs = multiples[prop as keyof typeof multiples]
+
+      for (let i = 0; i < dirs.length; i++) {
+        result[dirs[i]] = value
+      }
+    } else {
+      result[prop] = value
+    }
+  }
+
+  return result
+}
---
+++ styleguides/admin/admin-ui-system/src/css/types.ts
@@ -0,0 +1,329 @@
+import * as CSS from 'csstype'
+
+type StandardCSSProperties = CSS.Properties<number | string>
+
+/**
+ * The `css` function accepts arrays as values for mobile-first responsive styles.
+ * Note that this extends to non-theme values also. For example `display=['none', 'block']`
+ * will also works.
+ *
+ * For more information see: https://styled-system.com/responsive-styles
+ */
+export type ResponsiveStyleValue<T> = T | Array<T | null | undefined>
+
+/**
+ * All non-vendor-prefixed CSS properties. (Allow `number` to support CSS-in-JS libs,
+ * since they are converted to pixels)
+ */
+export type EmotionCSSProperties = CSS.StandardProperties<number | string> &
+  CSS.SvgProperties<number | string> &
+  CSS.VendorProperties<number | string>
+
+/**
+ * Map of all CSS pseudo selectors (`:hover`, `:focus`, ...)
+ */
+export type CSSPseudoSelectorProps = { [K in CSS.Pseudos]?: StyleProp }
+
+/**
+ * CSS as POJO that is compatible with CSS-in-JS libaries.
+ * TODO: use emotion 11
+ * Copied directly from [emotion](https://github.com/emotion-js/emotion/blob/ca3ad1c1dcabf78a95b55cc2dc94cad1998a3196/packages/serialize/types/index.d.ts#L45) types
+ */
+export type CSSObject = CSSPropertiesWithMultiValues &
+  CSSPseudosForCSSObject &
+  CSSOthersObjectForCSSObject
+
+type CSSPropertiesWithMultiValues = {
+  [K in keyof EmotionCSSProperties]: EmotionCSSProperties[K]
+}
+type CSSPseudosForCSSObject = { [K in CSS.Pseudos]?: CSSObject }
+type CSSInterpolation = undefined | number | string | CSSObject
+interface CSSOthersObjectForCSSObject {
+  [propertiesName: string]: CSSInterpolation
+}
+
+interface AliasesCSSProperties {
+  /**
+   * The **`background-color`** CSS property sets the background color of an element.
+   *
+   * **Initial value**: `transparent`
+   *
+   * | Chrome | Firefox | Safari |  Edge  |  IE   |
+   * | :----: | :-----: | :----: | :----: | :---: |
+   * | **1**  |  **1**  | **1**  | **12** | **4** |
+   *
+   * @see https://developer.mozilla.org/docs/Web/CSS/background-color
+   */
+  bg?: StandardCSSProperties['backgroundColor']
+  /**
+   * The **`marginX`** is shorthand for using both **`margin-left`** and **`margin-right`** CSS properties. They set the margin area on the left and right side of an element. A positive value
+   * places it farther from its neighbors, while a negative value places it closer.
+   *
+   * **Initial value**: `0`
+   *
+   * | Chrome | Firefox | Safari |  Edge  |  IE   |
+   * | :----: | :-----: | :----: | :----: | :---: |
+   * | **1**  |  **1**  | **1**  | **12** | **3** |
+   *
+   * @see https://styled-system.com/#margin-props
+   * @see https://developer.mozilla.org/docs/Web/CSS/margin-left
+   * @see https://developer.mozilla.org/docs/Web/CSS/margin-right
+   */
+  marginX?: StandardCSSProperties['marginLeft']
+  /**
+   * The **`marginY`** is shorthard for using both **`margin-top`** and **`margin-bottom`** CSS properties. They set the margin area on the top and bottom of an element. A positive value places
+   * it farther from its neighbors, while a negative value places it closer.
+   *
+   * **Initial value**: `0`
+   *
+   * | Chrome | Firefox | Safari |  Edge  |  IE   |
+   * | :----: | :-----: | :----: | :----: | :---: |
+   * | **1**  |  **1**  | **1**  | **12** | **3** |
+   *
+   * @see https://styled-system.com/#margin-props
+   * @see https://developer.mozilla.org/docs/Web/CSS/margin-top
+   * @see https://developer.mozilla.org/docs/Web/CSS/margin-bottom
+   */
+  marginY?: StandardCSSProperties['marginTop']
+  /**
+   * The **`paddingX`** is shorthand property for CSS properties **`padding-left`** and **`padding-right`**. They set the width of the padding area on the left and right side of an element.
+   *
+   * **Initial value**: `0`
+   *
+   * | Chrome | Firefox | Safari |  Edge  |  IE   |
+   * | :----: | :-----: | :----: | :----: | :---: |
+   * | **1**  |  **1**  | **1**  | **12** | **4** |
+   *
+   * @see https://styled-system.com/#padding-props
+   * @see https://developer.mozilla.org/docs/Web/CSS/padding-left
+   * @see https://developer.mozilla.org/docs/Web/CSS/padding-right
+   */
+  paddingX?: StandardCSSProperties['paddingLeft']
+  /**
+   * The **`paddingY`** is shorthand property for CSS properties **`padding-top`** and **`padding-bottom`**. They set the width of the padding area on the top and bottom of an element.
+   *
+   * **Initial value**: `0`
+   *
+   * | Chrome | Firefox | Safari |  Edge  |  IE   |
+   * | :----: | :-----: | :----: | :----: | :---: |
+   * | **1**  |  **1**  | **1**  | **12** | **4** |
+   *
+   * @see https://styled-system.com/#padding-props
+   * @see https://developer.mozilla.org/docs/Web/CSS/padding-top
+   * @see https://developer.mozilla.org/docs/Web/CSS/padding-bottom
+   */
+  paddingY?: StandardCSSProperties['paddingTop']
+  size?: StandardCSSProperties['width']
+}
+
+interface OverwriteCSSProperties {
+  /**
+   * The **`box-shadow`** CSS property adds shadow effects around an element's frame. You can set multiple effects separated by commas. A box shadow is described by X and Y offsets relative to the
+   * element, blur and spread radii, and color.
+   *
+   * **Initial value**: `none`
+   *
+   * | Chrome  | Firefox | Safari  |  Edge  |  IE   |
+   * | :-----: | :-----: | :-----: | :----: | :---: |
+   * | **10**  |  **4**  | **5.1** | **12** | **9** |
+   * | 1 _-x-_ |         | 3 _-x-_ |        |       |
+   *
+   * @see https://developer.mozilla.org/docs/Web/CSS/box-shadow
+   */
+  boxShadow?: CSS.Property.BoxShadow | number
+  /**
+   * The **`font-weight`** CSS property specifies the weight (or boldness) of the font. The font weights available to you will depend on the `font-family` you are using. Some fonts are only
+   * available in `normal` and `bold`.
+   *
+   * **Initial value**: `normal`
+   *
+   * | Chrome | Firefox | Safari |  Edge  |  IE   |
+   * | :----: | :-----: | :----: | :----: | :---: |
+   * | **2**  |  **1**  | **1**  | **12** | **3** |
+   *
+   * @see https://developer.mozilla.org/docs/Web/CSS/font-weight
+   */
+  fontWeight?: CSS.Property.FontWeight | string
+
+  /**
+   * The **`border-top-style`** CSS property sets the line style of an element's top `border`.
+   *
+   * **Initial value**: `none`
+   *
+   * | Chrome | Firefox | Safari |  Edge  |   IE    |
+   * | :----: | :-----: | :----: | :----: | :-----: |
+   * | **1**  |  **1**  | **1**  | **12** | **5.5** |
+   *
+   * @see https://developer.mozilla.org/docs/Web/CSS/border-top-style
+   */
+  borderTopStyle?: CSS.Property.BorderTopStyle | string
+  /**
+   * The **`border-top-width`** CSS property sets the width of the top border of an element.
+   *
+   * **Initial value**: `medium`
+   *
+   * | Chrome | Firefox | Safari |  Edge  |  IE   |
+   * | :----: | :-----: | :----: | :----: | :---: |
+   * | **1**  |  **1**  | **1**  | **12** | **4** |
+   *
+   * @see https://developer.mozilla.org/docs/Web/CSS/border-top-width
+   */
+  borderTopWidth?: CSS.Property.BorderTopWidth<never> | string
+  /**
+   * The **`border-bottom-style`** CSS property sets the line style of an element's bottom `border`.
+   *
+   * **Initial value**: `none`
+   *
+   * | Chrome | Firefox | Safari |  Edge  |   IE    |
+   * | :----: | :-----: | :----: | :----: | :-----: |
+   * | **1**  |  **1**  | **1**  | **12** | **5.5** |
+   *
+   * @see https://developer.mozilla.org/docs/Web/CSS/border-bottom-style
+   */
+  borderBottomStyle?: CSS.Property.BorderBottomStyle | string
+  /**
+   * The **`border-right-style`** CSS property sets the line style of an element's right `border`.
+   *
+   * **Initial value**: `none`
+   *
+   * | Chrome | Firefox | Safari |  Edge  |   IE    |
+   * | :----: | :-----: | :----: | :----: | :-----: |
+   * | **1**  |  **1**  | **1**  | **12** | **5.5** |
+   *
+   * @see https://developer.mozilla.org/docs/Web/CSS/border-right-style
+   */
+  borderRightStyle?: CSS.Property.BorderRightStyle | string
+  /**
+   * The **`border-left-style`** CSS property sets the line style of an element's left `border`.
+   *
+   * **Initial value**: `none`
+   *
+   * | Chrome | Firefox | Safari |  Edge  |   IE    |
+   * | :----: | :-----: | :----: | :----: | :-----: |
+   * | **1**  |  **1**  | **1**  | **12** | **5.5** |
+   *
+   * @see https://developer.mozilla.org/docs/Web/CSS/border-left-style
+   */
+  borderLeftStyle?: CSS.Property.BorderLeftStyle | string
+  /**
+   * The **`border-radius`** CSS property rounds the corners of an element's outer border edge. You can set a single radius to make circular corners, or two radii to make elliptical corners.
+   *
+   * | Chrome  | Firefox | Safari  |  Edge  |  IE   |
+   * | :-----: | :-----: | :-----: | :----: | :---: |
+   * |  **4**  |  **4**  |  **5**  | **12** | **9** |
+   * | 1 _-x-_ |         | 3 _-x-_ |        |       |
+   *
+   * @see https://developer.mozilla.org/docs/Web/CSS/border-radius
+   */
+  borderRadius?: CSS.Property.BorderRadius<string | number>
+
+  /**
+   * The **`z-index`** CSS property sets the z-order of a positioned element and its descendants or flex items. Overlapping elements with a larger z-index cover those with a smaller one.
+   *
+   * **Initial value**: `auto`
+   *
+   * | Chrome | Firefox | Safari |  Edge  |  IE   |
+   * | :----: | :-----: | :----: | :----: | :---: |
+   * | **1**  |  **1**  | **1**  | **12** | **4** |
+   *
+   * @see https://developer.mozilla.org/docs/Web/CSS/z-index
+   */
+  zIndex?: CSS.Property.ZIndex | string
+}
+
+/**
+ * Map of all available CSS properties (including aliases and overwrites)
+ * and their raw value.
+ */
+export interface ExtendedCSSProps
+  extends Omit<EmotionCSSProperties, keyof OverwriteCSSProperties>,
+    AliasesCSSProperties,
+    OverwriteCSSProperties {}
+
+export type StylePropertyValue<T> =
+  | ResponsiveStyleValue<Exclude<T, undefined>>
+  | ((theme: Theme) => ResponsiveStyleValue<Exclude<T, undefined>> | undefined)
+  | StyleProp
+
+export type CSSProps = {
+  [K in keyof ExtendedCSSProps]: StylePropertyValue<ExtendedCSSProps[K]>
+}
+
+export interface ThemeKeyProp {
+  /**
+   * **`themeKey`** is used to consume complex object styles.
+   *
+   * @example
+   * const theme = {
+   *   components.button: {
+   *     primary: {
+   *       padding: 3,
+   *       color: 'white',
+   *       bg: 'primary',
+   *       borderRadius: 'default',
+   *     },
+   *   },
+   * }
+   *
+   * const result = css({
+   *   variant: 'components.button.primary',
+   * })(theme)
+   */
+  themeKey?: string
+}
+
+export interface ThemeDerivedStyles {
+  (theme: Theme): StyleObject
+}
+
+export interface CSSOthersObject {
+  // we want to match CSS selectors
+  // but index signature needs to be a supertype
+  // so as a side-effect we allow unknown CSS properties (Emotion does too)
+  [k: string]: StylePropertyValue<string | number> | undefined | null
+}
+
+export type StyleObject = CSSProps &
+  CSSPseudoSelectorProps &
+  CSSOthersObject &
+  ThemeKeyProp
+
+/**
+ * The `StyleProp` extends [style props](https://emotion.sh/docs/object-styles)
+ * such that properties that are part of the `Theme` will be transformed to
+ * their corresponding values. Other valid CSS properties are also allowed.
+ */
+export type StyleProp = StyleObject | ThemeDerivedStyles
+
+/**
+ * An array or object (possibly nested) of related CSS properties
+ */
+export type Scale<T> = T[] | { [K: string]: T | Scale<T>; [I: number]: T }
+
+export interface Theme {
+  breakpoints?: string[]
+  mediaQueries?: { [size: string]: string }
+  space?: Scale<CSS.Property.Margin<number | string>>
+  fontSizes?: Scale<CSS.Property.FontSize<number>>
+  fonts?: Scale<CSS.Property.FontFamily>
+  fontWeights?: Scale<CSS.Property.FontWeight>
+  lineHeights?: Scale<CSS.Property.LineHeight<string | 0 | number>>
+  letterSpacings?: Scale<CSS.Property.LetterSpacing<string | 0 | number>>
+  sizes?: Scale<CSS.Property.Height<{}> | CSS.Property.Width<{}>>
+  borders?: Scale<CSS.Property.Border<{}>>
+  borderStyles?: Scale<CSS.Property.Border<{}>>
+  borderWidths?: Scale<CSS.Property.BorderWidth<string | 0 | number>>
+  radii?: Scale<CSS.Property.BorderRadius<string | 0 | number>>
+  shadows?: Scale<CSS.Property.BoxShadow>
+  zIndices?: Scale<CSS.Property.ZIndex>
+  colorStyles?: Scale<CSSProps>
+  textStyles?: Scale<CSSProps>
+  opacities?: Scale<CSS.Property.Opacity>
+  transitions?: Scale<CSS.Property.Transition>
+  /**
+   * Define the colors that are available through this theme
+   */
+  colors?: {
+    [k: string]: CSS.Property.Color | Scale<CSS.Property.Color> | undefined
+  }
+}
