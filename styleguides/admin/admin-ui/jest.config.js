f87531c46b508322486ad5d5b8167b684c7a69f7
matheusps <cout.matheusps@gmail.com>
2020-12-15 13:30:21 -0300

    test(admin-ui): fix collect coverage & add input password tests

--- styleguides/admin/admin-ui/jest.config.js
+++ styleguides/admin/admin-ui/jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   verbose: true,
   setupFilesAfterEnv: ['./setupTests.ts'],
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/*.stories.tsx',
+    '!**/dist/**',
+    '!**/.storybook/**',
+  ],
 }
