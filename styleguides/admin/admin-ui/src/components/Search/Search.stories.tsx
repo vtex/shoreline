ee33f75736f793acafb23867d242f64986dade3e
matheusps <cout.matheusps@gmail.com>
2021-02-04 17:04:15 -0300

    feat(search): create component

---
+++ styleguides/admin/admin-ui/src/components/Search/Search.stories.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { Story, Meta } from '@storybook/react'
+
+import { Search, SearchProps } from './index'
+
+export default {
+  title: 'forms/Search',
+  component: Search,
+} as Meta
+
+export const Playground: Story<SearchProps> = (args) => {
+  const [value, setValue] = useState('')
+  return (
+    <Search
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      {...args}
+    />
+  )
+}
+
+Playground.args = {
+  id: 'search',
+  placeholder: 'Search for a product, category or brand',
+  onSubmit: () => alert('submited'),
+  loading: false,
+}
