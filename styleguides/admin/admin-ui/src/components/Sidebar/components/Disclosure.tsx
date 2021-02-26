e1d946a4866cf7e6c330657e15f57ee7fdfc098f
Marcelo Cardoso <marcelovicentegc@gmail.com>
2021-02-26 12:41:35 -0300

    chore(sidebar): leverage reakit's menu powers

---
+++ styleguides/admin/admin-ui/src/components/Sidebar/components/Disclosure.tsx
@@ -0,0 +1,64 @@
+import React, { cloneElement, ReactNode } from 'react'
+import { MenuStateReturn, ReakitMenuButton } from './AriaSidebar'
+import { ButtonProps, Button } from '../../Button'
+
+export interface SidebarDisclosureProps extends ButtonProps {
+  icon: ReactNode
+  onClick: (event: React.MouseEvent<any, MouseEvent>) => void
+  selected?: boolean
+  secret: {
+    state: MenuStateReturn
+  }
+}
+
+export function SidebarDisclosure(props: SidebarDisclosureProps) {
+  const {
+    icon,
+    onClick,
+    selected,
+    secret: { state },
+  } = props
+
+  const children = (
+    <Button
+      variant="tertiary"
+      icon={icon}
+      styleOverrides={{
+        backgroundColor: selected ? '#EAF0FD' : 'unset',
+        'div > svg': {
+          color: selected ? 'unset' : 'black',
+          opacity: selected ? 1 : 0.6,
+        },
+      }}
+      {...props}
+      onClick={(event) => {
+        onClick(event)
+      }}
+    />
+  )
+
+  return (
+    <ReakitMenuButton {...state} {...children.props}>
+      {(enhancedProps) =>
+        cloneElement(
+          <Button
+            variant="tertiary"
+            icon={icon}
+            styleOverrides={{
+              backgroundColor: selected ? '#EAF0FD' : 'unset',
+              'div > svg': {
+                color: selected ? 'unset' : 'black',
+                opacity: selected ? 1 : 0.6,
+              },
+            }}
+            {...props}
+            onClick={(event) => {
+              onClick(event)
+            }}
+          />,
+          enhancedProps
+        )
+      }
+    </ReakitMenuButton>
+  )
+}
