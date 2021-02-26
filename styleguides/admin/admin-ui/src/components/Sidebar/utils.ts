0b07db541883b8815ab8309c63ee1fe1c47d3b56
Marcelo Cardoso <marcelovicentegc@gmail.com>
2021-02-26 13:49:10 -0300

    chore: reintroduce secret props to handle reakit's menu state

--- styleguides/admin/admin-ui/src/components/Sidebar/utils.ts
+++ styleguides/admin/admin-ui/src/components/Sidebar/utils.ts
@@ -1,1 +1,7 @@
+import { MenuStateReturn } from './components/AriaSidebar'
+
 export type CornerScope = 'top' | 'bottom'
+
+export interface SidebarSecretProps {
+  secret: { state: MenuStateReturn }
+}
