cafb06759242535ac6857bba923257298b4c43aa
Marcelo Cardoso <marcelovicentegc@gmail.com>
2021-03-01 14:20:01 -0300

    chore: wrap sidebar children under reakit's menu bar

--- styleguides/admin/admin-ui/src/components/Sidebar/components/Item.tsx
+++ styleguides/admin/admin-ui/src/components/Sidebar/components/Item.tsx
@@ -1,4 +1,4 @@
-import React, { cloneElement, Fragment } from 'react'
+import React, { cloneElement } from 'react'
 import { SidebarDisclosure, SidebarDisclosureProps } from './Disclosure'
 import { ReakitMenuItem, useMenuState } from './AriaSidebar'
 import { SystemComponent } from '../../../types'
@@ -12,8 +12,43 @@ export interface SidebarItemProps
   sections?: Omit<SidebarSectionProps, 'secret'>[]
 }
 
+/**
+ * SidebarItem corresponds to an item of the sidebar's
+ * first level. It must used along with the Sidebar.Header
+ * or Sidebar.Footer, both inside the Sidebar. That is because
+ * they share the same context, and to enforce the proper
+ * usage of this component.
+ *
+ * @example
+ * ```jsx
+ * import { Sidebar } from `@vtex/admin-ui`
+ *
+ * <Sidebar>
+ *    <Sidebar.Header>
+ *      <Sidebar.Item
+ *         selected={false}
+ *         onClick={() => console.log("Hello")}
+ *         sections={[
+ *            title: "Example",
+ *            children: [
+ *              <Sidebar.SubItem
+ *                onClick={() => console.log("Hi")}>
+ *                Example
+ *              </Sidebar.SubItem>
+ *            ]
+ *         ]}
+ *      />
+ *    </Sidebar.Header>
+ * </Sidebar>
+ * ```
+ */
 export function SidebarItem(props: Omit<SidebarItemProps, 'secret'>) {
   const { collapsed, sections, ...baseProps } = props
+
+  const {
+    // @ts-ignore
+    secret: { state: parentState },
+  } = props
 
   const state = useMenuState({
     orientation: 'vertical',
@@ -21,28 +56,33 @@ export function SidebarItem(props: Omit<SidebarItemProps, 'secret'>) {
   })
 
   return (
-    <Fragment>
-      <SidebarDisclosure
-        {...props}
-        secret={{
-          state,
-        }}
-      />
-      {sections?.map(
-        ({ title, children }, index) =>
-          children.length > 0 && (
-            <ReakitMenuItem {...state} key={index}>
-              {(itemProps) =>
-                cloneElement(
-                  <Sidebar.Section title={title} {...baseProps}>
-                    {children}
-                  </Sidebar.Section>,
-                  { ...itemProps, secret: { state } }
-                )
-              }
-            </ReakitMenuItem>
-          )
+    <ReakitMenuItem {...parentState}>
+      {(itemProps) => (
+        <>
+          <SidebarDisclosure
+            {...props}
+            {...itemProps}
+            secret={{
+              state,
+            }}
+          />
+          {sections?.map(
+            ({ title, children }, index) =>
+              children.length > 0 && (
+                <ReakitMenuItem {...state} key={index}>
+                  {(itemProps) =>
+                    cloneElement(
+                      <Sidebar.Section title={title} {...baseProps}>
+                        {children}
+                      </Sidebar.Section>,
+                      { ...itemProps, secret: { state } }
+                    )
+                  }
+                </ReakitMenuItem>
+              )
+          )}
+        </>
       )}
-    </Fragment>
+    </ReakitMenuItem>
   )
 }
