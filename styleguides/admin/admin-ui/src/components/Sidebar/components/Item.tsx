92e148071e0dcecd504b8d0117216285b198c07e
Marcelo Cardoso <marcelovicentegc@gmail.com>
2021-03-01 21:31:49 -0300

    chore(sidebar): smooth sidebar arrow navigation

--- styleguides/admin/admin-ui/src/components/Sidebar/components/Item.tsx
+++ styleguides/admin/admin-ui/src/components/Sidebar/components/Item.tsx
@@ -1,9 +1,11 @@
 import React, { cloneElement } from 'react'
 import { SidebarDisclosure, SidebarDisclosureProps } from './Disclosure'
-import { ReakitMenuItem, useMenuState } from './AriaSidebar'
+import { ReakitMenuItem, ReakitMenu, useMenuState } from './AriaSidebar'
 import { SystemComponent } from '../../../types'
 import { SidebarSectionProps } from './Section'
 import { Sidebar } from '..'
+import { useSystem } from '@vtex/admin-core'
+import { useSidebarContext } from '../context'
 
 export interface SidebarItemProps
   extends SidebarDisclosureProps,
@@ -14,6 +16,8 @@ export interface SidebarItemProps
 
 export function SidebarItem(props: Omit<SidebarItemProps, 'secret'>) {
   const { collapsed, sections, onClick, ...baseProps } = props
+  const { cn } = useSystem()
+  const { direction } = useSidebarContext()
 
   const {
     // @ts-ignore
@@ -24,6 +28,8 @@ export function SidebarItem(props: Omit<SidebarItemProps, 'secret'>) {
     orientation: 'vertical',
     loop: true,
   })
+
+  const sectionState = useMenuState()
 
   return (
     <ReakitMenuItem {...parentState}>
@@ -41,21 +47,37 @@ export function SidebarItem(props: Omit<SidebarItemProps, 'secret'>) {
               state,
             }}
           />
-          {sections?.map(
-            ({ title, children }, index) =>
-              children.length > 0 && (
-                <ReakitMenuItem {...state} key={index}>
-                  {(itemProps) =>
-                    cloneElement(
-                      <Sidebar.Section title={title} {...baseProps}>
-                        {children}
-                      </Sidebar.Section>,
-                      { ...itemProps, secret: { state } }
-                    )
-                  }
-                </ReakitMenuItem>
-              )
-          )}
+          <ReakitMenu
+            className={cn({
+              [direction]: `56px !important`,
+              transform: 'unset !important',
+              outline: 'none',
+              backgroundColor: '#F8F9FA',
+              height: '100%',
+              padding: '1.875rem 0.875rem',
+              borderRight: '1px solid #E0E2E7',
+            })}
+            aria-label={'Tooltip title should come here'}
+            {...sectionState}
+            {...state}
+            {...baseProps}
+          >
+            {sections?.map(
+              ({ title, children }, index) =>
+                children.length > 0 && (
+                  <ReakitMenuItem {...state} key={index}>
+                    {(itemProps) =>
+                      cloneElement(
+                        <Sidebar.Section title={title} {...baseProps}>
+                          {children}
+                        </Sidebar.Section>,
+                        { ...itemProps, secret: { state } }
+                      )
+                    }
+                  </ReakitMenuItem>
+                )
+            )}
+          </ReakitMenu>
         </>
       )}
     </ReakitMenuItem>
