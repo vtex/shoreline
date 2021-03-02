24d15374347277c43c83314a06cd2a59dc900d65
Marcelo Cardoso <marcelovicentegc@gmail.com>
2021-03-02 20:15:35 -0300

    chore(sidebar): move some styles to admin-ui-theme

--- styleguides/admin/admin-ui/src/components/Sidebar/components/Backdrop.tsx
+++ styleguides/admin/admin-ui/src/components/Sidebar/components/Backdrop.tsx
@@ -1,15 +1,6 @@
 import React from 'react'
 import { Box } from '../../Box'
-import { SCALES } from '../utils'
 
 export function SidebarBackdrop() {
-  return (
-    <Box
-      styles={{
-        minWidth: SCALES.COLLAPSIBLE_AREA_WIDTH,
-        maxWidth: SCALES.COLLAPSIBLE_AREA_WIDTH,
-      }}
-      tabIndex={0}
-    />
-  )
+  return <Box themeKey={'components.sidebar.backdrop'} tabIndex={0} />
 }
