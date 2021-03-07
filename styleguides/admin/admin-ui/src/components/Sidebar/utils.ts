8dd5791b0ba12d66b8dd086e0d8c8a443eabe977
Marcelo Cardoso <marcelovicentegc@gmail.com>
2021-03-07 10:35:07 -0300

    chore: let motion handle animations/transitions

--- styleguides/admin/admin-ui/src/components/Sidebar/utils.ts
+++ styleguides/admin/admin-ui/src/components/Sidebar/utils.ts
@@ -1,4 +1,11 @@
+import { Variants } from 'framer-motion'
 import { CompositeStateReturn } from './components/Aria'
+
+interface VariantsArgs {
+  direction: AnchorDirection
+  currentItemIsCollapsible: boolean | null
+  selected?: boolean
+}
 
 export type CornerScope = 'top' | 'bottom'
 
@@ -12,9 +19,10 @@ export interface SidebarSecretProps {
 
 export type AnchorDirection = 'left' | 'right'
 
-export interface CurrentItem {
+export interface Item {
   scope: CornerScope
   index: number
+  isCollapsible: boolean
 }
 
 export enum ArrowKeys {
@@ -28,3 +36,50 @@ export const SCALES = {
   FIXED_AREA_WIDTH: '3.5rem',
   COLLAPSED_BACKDROP_WIDTH: '0.625rem',
 }
+
+export const SidebarItemVariants = ({
+  direction,
+  currentItemIsCollapsible,
+  selected,
+}: VariantsArgs): Variants => {
+  return {
+    expanded: () => ({
+      [direction]: SCALES.FIXED_AREA_WIDTH,
+      transition: {
+        damping: 50,
+      },
+      transitionEnd: {
+        zIndex: 0,
+      },
+    }),
+    collapsed: () => ({
+      [direction]:
+        selected && currentItemIsCollapsible ? '-8.125rem' : '-13.5rem',
+      transition: {
+        damping: 50,
+      },
+      zIndex: -1,
+    }),
+  }
+}
+
+export const BackdropVariants = ({
+  width,
+}: {
+  width: number | string
+}): Variants => ({
+  expanded: () => ({
+    minWidth: width,
+    width,
+    transition: {
+      damping: 50,
+    },
+  }),
+  collapsed: () => ({
+    minWidth: width,
+    width,
+    transition: {
+      damping: 50,
+    },
+  }),
+})
