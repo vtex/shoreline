c023a690e7a049a5eb9a30ab9bdbe45782cc9da7
Lucas Santana <lcssantana03@gmail.com>
2021-03-11 15:52:39 -0300

    feat(sortable-table): create useTableSort hook
    BREAKING CHANGE: 🧨 \

---
+++ styleguides/admin/admin-ui/src/components/Table/useTableSort.ts
@@ -0,0 +1,84 @@
+import { useReducer, useCallback } from 'react'
+
+enum SortOrder {
+  ASC = 'ASC',
+  DSC = 'DSC',
+}
+
+enum ActionType {
+  SortASC = 'SORT_ASC',
+  SortDSC = 'SORT_DSC',
+  Clear = 'CLEAR',
+}
+
+interface State {
+  order?: SortOrder
+  by?: string | number | symbol
+}
+
+interface Action {
+  type: ActionType
+  payload?: {
+    id: string | number | symbol
+  }
+}
+
+const clearState: State = {
+  by: undefined,
+  order: undefined,
+}
+
+export interface Sort {
+  sorted: State
+  sort: (id: string | number | symbol) => void
+  clear: () => void
+}
+
+export function useTableSort(initialState?: Partial<State>) {
+  const [sorted, dispatch] = useReducer(reducer, {
+    ...clearState,
+    ...initialState,
+  })
+
+  const sort = useCallback(
+    (id: string | number | symbol) => {
+      const { by, order } = sorted
+      if (!by || by !== id || (by && order !== SortOrder.ASC)) {
+        dispatch({ type: ActionType.SortASC, payload: { id } })
+      } else {
+        dispatch({ type: ActionType.SortDSC, payload: { id } })
+      }
+    },
+    [sorted, dispatch]
+  )
+
+  function clear() {
+    dispatch({ type: ActionType.Clear })
+  }
+
+  return { sorted, sort, clear }
+}
+
+function reducer(state: State, action: Action) {
+  switch (action.type) {
+    case ActionType.SortASC: {
+      const { id } = action.payload ?? { id: undefined }
+      return {
+        by: id,
+        order: SortOrder.ASC,
+      }
+    }
+    case ActionType.SortDSC: {
+      const { id } = action.payload ?? { id: undefined }
+      return {
+        by: id,
+        order: SortOrder.DSC,
+      }
+    }
+    case ActionType.Clear: {
+      return clearState
+    }
+    default:
+      return state
+  }
+}
