8e16fd66de4c7ee4299d8bc50af808d5a87d952f
coltran3 <luca.santos>
2021-04-12 11:09:07 -0400

    feat(table-empty-state): add table state to stories

--- styleguides/admin/admin-ui/src/components/Table/stories/states.stories.tsx
+++ styleguides/admin/admin-ui/src/components/Table/stories/states.stories.tsx
@@ -5,6 +5,7 @@ import { StatefulTable, StatefulTableProps } from '../index'
 import { baseResolvers } from '../resolvers/base'
 import { Box } from '@vtex/admin-primitives'
 import { Button } from '../../Button'
+import { TableStates } from '../components/States'
 
 export default {
   title: 'admin-ui/Table/States',
@@ -121,5 +122,59 @@ export function DataFetch() {
         length={4}
       />
     </Box>
+  )
+}
+
+export function Empty() {
+  return (
+    <TableStates state="notFoundItems">
+      <StatefulTable
+        density="compact"
+        columns={[
+          {
+            id: 'location',
+            header: 'Location',
+            width: 148,
+          },
+          {
+            id: 'date',
+            header: 'Date',
+            width: 148,
+          },
+          {
+            id: 'status',
+            header: 'Status',
+            width: 156,
+          },
+        ]}
+        items={[
+          {
+            id: 1,
+            location: 'São Paulo, SP',
+            date: '8/7/2020, 23:29',
+            status: `Delivered`,
+          },
+          {
+            id: 2,
+            location: 'São Paulo, SP',
+            date: '6/7/2020, 21:12',
+            status: `Arrived at São Paulo`,
+          },
+          {
+            id: 3,
+            location: 'São Paulo, SP',
+            date: '5/7/2020, 13:04',
+            status: `On its way from Rio de Janeiro to São Paulo`,
+          },
+          {
+            id: 4,
+            location: 'Itaquaquecetuba, SP',
+            date: '4/7/2020, 14:48',
+            status: `Object dispatched at the post office`,
+          },
+        ]}
+        length={4}
+      />
+    </TableStates>
   )
 }
