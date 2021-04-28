d8398eb48f47e2b4f58fda41a61904ae65ad44e7
Marcelo Cardoso <marcelovicentegc@gmail.com>
2021-04-28 11:05:22 -0300

    chore(toast): remove unecessary component from storybook stories

--- styleguides/admin/admin-ui/src/components/Toast/Toast.stories.tsx
+++ styleguides/admin/admin-ui/src/components/Toast/Toast.stories.tsx
@@ -16,71 +16,69 @@ export function Basic() {
   const toaster = useToaster()
 
   return (
-    <Set spacing={3} orientation="vertical">
-      <Set>
-        <Button
-          onClick={() =>
-            toaster.notify({
-              message: 'Type here a short message',
-            })
-          }
-        >
-          Notify info
-        </Button>
-        <Button
-          onClick={() =>
-            toaster.notify({
-              type: 'success',
-              message: 'Type here a short message',
-            })
-          }
-        >
-          Notify success
-        </Button>
-        <Button
-          onClick={() =>
-            toaster.notify({
-              type: 'error',
-              message: 'Type here a short message',
-            })
-          }
-        >
-          Notify error
-        </Button>
-        <Button
-          onClick={() =>
-            toaster.notify({
-              type: 'warning',
-              message: 'Type here a short message',
-            })
-          }
-        >
-          Notify warning
-        </Button>
-        <Button
-          onClick={() =>
-            toaster.notify({
-              type: 'success',
-              message: "I'm customized 😎!",
-              csx: {
-                background:
-                  'radial-gradient(ellipse at top, #e66465, transparent),radial-gradient(ellipse at bottom, #4d9f0c, transparent);',
-                color: 'white',
-                borderRadius: '8px',
-                height: '72px',
-                padding: '22px',
-                whiteSpace: 'nowrap',
-                overflow: 'hidden',
-              },
-              iconProps: {
-                children: <Box>🔥</Box>,
-              },
-            })
-          }
-        >
-          Custom toast
-        </Button>
-      </Set>
+    <Set>
+      <Button
+        onClick={() =>
+          toaster.notify({
+            message: 'Type here a short message',
+          })
+        }
+      >
+        Notify info
+      </Button>
+      <Button
+        onClick={() =>
+          toaster.notify({
+            type: 'success',
+            message: 'Type here a short message',
+          })
+        }
+      >
+        Notify success
+      </Button>
+      <Button
+        onClick={() =>
+          toaster.notify({
+            type: 'error',
+            message: 'Type here a short message',
+          })
+        }
+      >
+        Notify error
+      </Button>
+      <Button
+        onClick={() =>
+          toaster.notify({
+            type: 'warning',
+            message: 'Type here a short message',
+          })
+        }
+      >
+        Notify warning
+      </Button>
+      <Button
+        onClick={() =>
+          toaster.notify({
+            type: 'success',
+            message: "I'm customized 😎!",
+            csx: {
+              background:
+                'radial-gradient(ellipse at top, #e66465, transparent),radial-gradient(ellipse at bottom, #4d9f0c, transparent);',
+              color: 'white',
+              borderRadius: '8px',
+              height: '72px',
+              padding: '22px',
+              whiteSpace: 'nowrap',
+              overflow: 'hidden',
+            },
+            iconProps: {
+              children: <Box>🔥</Box>,
+            },
+          })
+        }
+      >
+        Custom toast
+      </Button>
     </Set>
   )
 }
@@ -89,63 +87,60 @@ export function Interactive() {
   const toaster = useToaster()
 
   return (
-    <Set spacing={3} orientation="vertical">
-      <Set>
-        <Button
-          onClick={() =>
-            toaster.notify({
-              message: 'Type here a short message',
-              dismissible: true,
-            })
-          }
-        >
-          Dismissable
-        </Button>
-        <Button
-          onClick={() =>
-            toaster.notify({
-              type: 'success',
-              message: 'Type here a short message',
-              action: {
-                children: <span>Action</span>,
-                onClick: () => alert('Toast callback'),
-              },
-            })
-          }
-        >
-          Toast with action
-        </Button>
-        <Button
-          onClick={() =>
-            toaster.notify({
-              type: 'warning',
-              message: 'Type here a short message',
-              dismissible: true,
-              action: {
-                children: <span>Action</span>,
-                onClick: () => alert('Toast callback'),
-              },
-            })
-          }
-        >
-          Dismissable toast with action
-        </Button>
-        <Button
-          onClick={() =>
-            toaster.notify({
-              message:
-                'Type here a longer message but not much longer than that',
-              dismissible: true,
-              action: {
-                children: <span>Action</span>,
-                onClick: () => alert('Toast callback'),
-              },
-            })
-          }
-        >
-          With long message
-        </Button>
-      </Set>
+    <Set>
+      <Button
+        onClick={() =>
+          toaster.notify({
+            message: 'Type here a short message',
+            dismissible: true,
+          })
+        }
+      >
+        Dismissable
+      </Button>
+      <Button
+        onClick={() =>
+          toaster.notify({
+            type: 'success',
+            message: 'Type here a short message',
+            action: {
+              children: <span>Action</span>,
+              onClick: () => alert('Toast callback'),
+            },
+          })
+        }
+      >
+        Toast with action
+      </Button>
+      <Button
+        onClick={() =>
+          toaster.notify({
+            type: 'warning',
+            message: 'Type here a short message',
+            dismissible: true,
+            action: {
+              children: <span>Action</span>,
+              onClick: () => alert('Toast callback'),
+            },
+          })
+        }
+      >
+        Dismissable toast with action
+      </Button>
+      <Button
+        onClick={() =>
+          toaster.notify({
+            message: 'Type here a longer message but not much longer than that',
+            dismissible: true,
+            action: {
+              children: <span>Action</span>,
+              onClick: () => alert('Toast callback'),
+            },
+          })
+        }
+      >
+        With long message
+      </Button>
     </Set>
   )
 }
