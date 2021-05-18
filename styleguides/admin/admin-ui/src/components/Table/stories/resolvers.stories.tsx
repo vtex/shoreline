fae1aab871b45dc8d22ae0eb0b47cb1d6c7d0bc1
coltran3 <luca.santos>
2021-05-18 08:22:20 -0400

    feat(checkbox-propagation): add table stories with checkbox and rowclick

--- styleguides/admin/admin-ui/src/components/Table/stories/resolvers.stories.tsx
+++ styleguides/admin/admin-ui/src/components/Table/stories/resolvers.stories.tsx
@@ -210,6 +210,62 @@ export const SelectionInit = () => {
   )
 }
 
+export function SelectionWithRowClick() {
+  return (
+    <StatefulTable
+      columns={[
+        {
+          id: 'checkboxes',
+          resolver: { type: 'selection', mapId: (item) => item.id },
+        },
+        {
+          id: 'location',
+          header: 'Location',
+          width: 148,
+        },
+        {
+          id: 'date',
+          header: 'Date',
+          width: 148,
+        },
+        {
+          id: 'status',
+          header: 'Status',
+          width: 156,
+        },
+      ]}
+      items={[
+        {
+          id: 1,
+          location: 'São Paulo, SP',
+          date: '8/7/2020, 23:29',
+          status: `Delivered`,
+        },
+        {
+          id: 2,
+          location: 'São Paulo, SP',
+          date: '6/7/2020, 21:12',
+          status: `Arrived at São Paulo`,
+        },
+        {
+          id: 3,
+          location: 'São Paulo, SP',
+          date: '5/7/2020, 13:04',
+          status: `On its way from Rio de Janeiro to São Paulo`,
+        },
+        {
+          id: 4,
+          location: 'Itaquaquecetuba, SP',
+          date: '4/7/2020, 14:48',
+          status: `Object dispatched at the post office`,
+        },
+      ]}
+      onRowClick={({ location }) => {
+        alert(`${location} was clicked`)
+      }}
+    />
+  )
+}
 export const Currency = () => {
   const currencies = [
     {
