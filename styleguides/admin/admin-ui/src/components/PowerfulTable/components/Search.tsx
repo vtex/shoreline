c3765972672fc5f386f0dba610f21f73d3ac112c
coltran3 <luca.santos>
2021-06-15 08:37:43 -0400

    fix(search-container-csx): remove unecessary height prop

--- styleguides/admin/admin-ui/src/components/PowerfulTable/components/Search.tsx
+++ styleguides/admin/admin-ui/src/components/PowerfulTable/components/Search.tsx
@@ -2,12 +2,11 @@ import React from 'react'
 import { Search, SearchProps } from '../../Search'
 
 export function TableSearch(props: SearchProps) {
-  const { height = 'small', containerCsx, ...searchProps } = props
+  const {  containerCsx, ...searchProps } = props
 
   return (
     <Search
       containerCsx={{ width: '25%', minWidth: '12.25rem', ...containerCsx }}
-      height={height}
       {...searchProps}
     />
   )
