4e6c72f9db819dc64b03093412e99b6d38e2301d
gtkatakura <gt.katakura@gmail.com>
2021-07-12 10:45:26 -0300

    feat(eslint-plugin): new rule create-tag-component-outside-render-phase

---
+++ packages/eslint-plugin-admin-ui/src/utils/estree/isFunctionNode.ts
@@ -0,0 +1,9 @@
+import * as ESTree from 'estree'
+
+export const isFunctionNode = (node: ESTree.Node): node is ESTree.Function => {
+  return (
+    node.type === 'ArrowFunctionExpression' ||
+    node.type === 'FunctionExpression' ||
+    node.type === 'FunctionDeclaration'
+  )
+}
