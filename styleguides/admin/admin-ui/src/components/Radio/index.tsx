e7129f14713d7669267026e2995690bd593bf217
lucasaarcoverde <lucasaarcoverde@gmail.com>
2021-07-15 14:41:18 -0300

    refactor(radio): migrate radio component to onda-react

--- styleguides/admin/admin-ui/src/components/Radio/index.tsx
+++ styleguides/admin/admin-ui/src/components/Radio/index.tsx
@@ -1,51 +1,103 @@
-import {
-  Radio as ReakitRadio,
-  RadioProps as ReakitRadioProps,
-  RadioStateReturn,
-} from 'reakit/Radio'
-import { useSystem, createComponent } from '@vtex/admin-core'
+import { Radio as ReakitRadio, RadioStateReturn } from 'reakit/Radio'
+import { jsx, ComponentProps } from '@vtex/onda-react'
 
-import { SystemComponentProps } from '../../types'
+export const Radio = jsx(ReakitRadio)(
+  {
+    appearance: 'none',
+    margin: 0,
+    position: 'relative',
+    cursor: 'pointer',
+    borderStyle: 'solid',
+    borderWidth: '1px',
+    borderColor: 'mid.secondary',
+    backgroundColor: 'transparent',
+    borderRadius: 'circle',
+    ':after': {
+      content: '""',
+      display: 'block',
+      borderRadius: 'circle',
+      backgroundColor: 'light.primary',
+      opacity: 0,
+    },
+    ':checked': {
+      backgroundColor: 'blue',
+      borderColor: 'blue',
+      ':after': {
+        opacity: 1,
+      },
+      ':hover': {
+        backgroundColor: 'blue.hover',
+        borderColor: 'blue.hover',
+      },
+      ':active': {
+        borderColor: 'blue.pressed',
+        backgroundColor: 'blue.pressed',
+      },
+    },
+    ':disabled': {
+      cursor: 'not-allowed',
+      backgroundColor: 'mid.tertiary',
+      borderColor: 'mid.primary',
+      ':after': {
+        backgroundColor: 'mid.primary',
+      },
+    },
+    ':hover': {
+      borderColor: 'dark.primary',
+    },
+    ':active': {
+      borderColor: 'dark.secondary',
+      backgroundColor: 'blue.secondary',
+    },
+    ':focus:not([data-focus-visible-added])': {
+      outline: 'none',
+      boxShadow: 'none',
+    },
+    ':focus': {
+      outline: 'none',
+      boxShadow: 'focus',
+    },
+    variants: {
+      size: {
+        regular: {
+          height: 20,
+          width: 20,
+          padding: '5px',
+          ':after': {
+            width: 8,
+            height: 8,
+          },
+        },
+        small: {
+          height: 16,
+          width: 16,
+          padding: 1,
+          ':after': {
+            width: 6,
+            height: 6,
+          },
+        },
+      },
+    },
+  },
+  {
+    options: ['state'],
+    useOptions: (options: RadioOptions, props) => {
+      const { state } = options
 
-export const Radio = createComponent(ReakitRadio, useRadio)
+      return { ...props, ...state }
+    },
+  }
+)
 
-export function useRadio(props: RadioProps) {
-  const { size = 'regular', state, csx, ...htmlProps } = props
-  const { cn } = useSystem()
-
-  const className = cn({
-    themeKey: `components.radio.${size}`,
-    ...csx,
-  })
-
-  return { className, ...state, ...htmlProps }
+Radio.defaultProps = {
+  size: 'regular',
 }
 
-type AbstractRadioProps = Pick<
-  ReakitRadioProps,
-  | 'value'
-  | 'disabled'
-  | 'required'
-  | 'name'
-  | 'id'
-  | 'onBlur'
-  | 'onClick'
-  | 'onFocus'
-  | 'checked'
-  | 'aria-label'
-  | 'aria-labelledby'
->
-
-export interface RadioProps extends SystemComponentProps<AbstractRadioProps> {
-  /**
-   * Radio size
-   * @default 'regular'
-   */
-  size?: 'regular' | 'small'
-  /**
-   * useRadio() hook return
-   */
+interface RadioOptions {
   state: RadioStateReturn
 }
+
+export type RadioProps = ComponentProps<typeof Radio>
 
 export { useRadioState, RadioStateReturn } from 'reakit/Radio'
