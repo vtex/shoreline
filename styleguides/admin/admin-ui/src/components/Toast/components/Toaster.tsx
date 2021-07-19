10ea13943ade7fdb9375a3b32369c5d050af3b73
matheusps <cout.matheusps@gmail.com>
2021-07-19 18:36:03 -0300

    fix(admin-ui/toast): toast manager overflows other elements, even without any toast

--- styleguides/admin/admin-ui/src/components/Toast/components/Toaster.tsx
+++ styleguides/admin/admin-ui/src/components/Toast/components/Toaster.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { tag } from '@vtex/onda-react'
 import { Toast } from './Toast'
 import { ToasterProps } from './typings'
 import { AnimatePresence, AnimateSharedLayout } from 'framer-motion'
 import { StyleObject } from '@vtex/admin-core'
+
+const HEIGHT_DELAY = 250
+const MIN_HEIGHT_VISIBLE = '4.5rem'
+const MIN_HEIGHT_HIDDEN = '0rem'
 
 /**
  * This component is responsible for rendering the toasts.
@@ -11,17 +15,31 @@ import { StyleObject } from '@vtex/admin-core'
  */
 export function Toaster(props: ToasterProps) {
   const { state } = props
+  const toastStack = useMemo(() => state['bottom-right'], [state])
+  const [minHeight, setMinHeight] = useState(MIN_HEIGHT_HIDDEN)
+
+  useEffect(() => {
+    const visible = toastStack && toastStack.length > 0
+    
+    if (visible) {
+      setMinHeight(MIN_HEIGHT_VISIBLE)
+    } else {
+      /**
+       * This delay is needed due framer-motion's AnimateSharedLayout
+       */
+      setTimeout(() => setMinHeight(MIN_HEIGHT_HIDDEN), HEIGHT_DELAY)
+    }
+  }, [toastStack])
 
   const csx: StyleObject = {
+    minHeight,
     position: 'fixed',
     bottom: '3rem',
     zIndex: 'over',
-    left: 0,
     right: '2rem',
     textAlign: 'center',
     marginLeft: 'auto',
     maxWidth: '23.375rem',
-    minHeight: '4.5rem',
     listStyle: 'none',
     '> *:not(:last-child)': {
       marginBottom: '0.75rem',
@@ -32,11 +50,11 @@ export function Toaster(props: ToasterProps) {
     <tag.ul data-testid="onda-toaster" csx={csx}>
       <AnimateSharedLayout>
         <AnimatePresence data-testid="onda-toaster-container">
-          {state['bottom-right'].map((toast) => (
+          {toastStack.map((toast) => (
             <Toast
               key={`${toast.position}-${toast.id}`}
               {...toast}
-              stack={state['bottom-right'].map((toast) => toast.id)}
+              stack={toastStack.map((toast) => toast.id)}
             />
           ))}
         </AnimatePresence>
