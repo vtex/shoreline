bfd384dc69eb14be0a3423dfc7c19a03a99e421c
lafray <lais.ribeiro@vtex.com.br>
2021-08-18 17:31:40 -0300

    fix(usequerystate): add query state contex to fix conflicts caused by using multiple instances

---
+++ packages/hooks/src/useQueryStateContext.tsx
@@ -0,0 +1,44 @@
+import type { ReactNode } from 'react'
+import React, { useContext, useEffect, useState, createContext } from 'react'
+
+import { isBrowser } from '@vtex/onda-util'
+import invariant from 'tiny-invariant'
+
+export interface QueryStateContextProps {
+  queryParams: URLSearchParams
+}
+
+const QueryStateContext = createContext<QueryStateContextProps>({
+  queryParams: new URLSearchParams(isBrowser ? window.location.search : ''),
+})
+
+export function useQueryStateContext() {
+  const context = useContext(QueryStateContext)
+
+  invariant(context, `Do not use useQueryState outside of QueryStateContext`)
+
+  return context
+}
+
+export function QueryStateProvider({ children }: { children: ReactNode }) {
+  const [queryParams, setQueryParams] = useState(
+    new URLSearchParams(isBrowser ? window.location.search : '')
+  )
+
+  useEffect(() => {
+    if (!isBrowser) return
+    window.onpopstate = function onPopstateChange() {
+      setQueryParams(new URLSearchParams(window.location.search))
+    }
+
+    return () => {
+      window.onpopstate = () => {}
+    }
+  }, [])
+
+  return (
+    <QueryStateContext.Provider value={{ queryParams }}>
+      {children}
+    </QueryStateContext.Provider>
+  )
+}
