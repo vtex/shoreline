766c56e5590839917411433267857c9be79862ab
lucasaarcoverde <lucasaarcoverde@gmail.com>
2021-09-30 10:17:01 -0300

    fix(page-actions): apply margin only between the directly children

--- packages/admin-ui/src/components/Page/components/PageActions.ts
+++ packages/admin-ui/src/components/Page/components/PageActions.ts
@@ -3,7 +3,7 @@ import { jsx } from '@vtex/admin-ui-react'
 export const PageActions = jsx('div')({
   display: 'flex',
   alignItems: 'center',
-  '* + *': {
+  '> * + *': {
     marginLeft: 4,
   },
 })
