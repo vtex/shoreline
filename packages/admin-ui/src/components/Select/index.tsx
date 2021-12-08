a5483f75761abbf5c3f1757cf4da1fc4dcd7999b
matheusps <cout.matheusps@gmail.com>
2021-12-08 08:47:00 -0300

    feat(select): change from a custom select, to a native one

--- packages/admin-ui/src/components/Select/index.tsx
+++ packages/admin-ui/src/components/Select/index.tsx
@@ -1,88 +1,76 @@
-import { useSelect, UseSelectReturnValue } from 'downshift'
 import type { Ref } from 'react'
 import React from 'react'
-import { BrowserView, MobileView } from 'react-device-detect'
 import { forwardRef } from '@vtex/admin-ui-util'
 
 import { Box } from '../Box'
 import type { SystemComponent } from '../../types'
-import { Text } from '../Text'
-import { DesktopSelect } from './DesktopSelect'
-import { MobileSelect } from './MobileSelect'
+import { FieldContainer, FieldDetails } from '../Field'
+
+import { IconCaret } from '@vtex/admin-ui-icons'
+import { jsx } from '@vtex/admin-ui-react'
+import * as style from './Select.style'
+
+import { Label } from '../Label'
+
+const SelectField = jsx('select')({
+  ...style.baseline,
+  variants: {
+    tone: {
+      neutral: style.neutral,
+      critical: style.critical,
+    },
+  },
+})
 
 export const Select = forwardRef(
-  <T,>(props: SelectProps<T>, ref: Ref<HTMLDivElement>) => {
+  (props: SelectProps, ref: Ref<HTMLSelectElement>) => {
     const {
       csx,
       label,
-      state,
-      items,
-      renderItem,
       helperText,
-      disabled,
       tone = 'neutral',
       criticalText,
-      block,
-      ...restProps
+      value,
+      id,
+      ...selectProps
     } = props
-
-    const selectProps = {
-      csx,
-      label,
-      state,
-      items,
-      renderItem,
-      disabled,
-      tone,
-      block,
-    }
 
     const message = tone === 'critical' ? criticalText : helperText
 
-    const styles = block ? { ...csx, width: 'full' } : csx
-
     return (
-      <Box {...restProps} csx={styles} ref={ref}>
-        <MobileView>
-          <MobileSelect {...selectProps} />
-        </MobileView>
+      <FieldContainer>
+        <Box csx={{ ...style.container, ...csx }}>
+          <Label
+            htmlFor={id}
+            csx={style.label({
+              active: !!value,
+            })}
+          >
+            {label}
+          </Label>
+          <SelectField
+            tone={tone}
+            ref={ref}
+            value={value}
+            id={id}
+            {...selectProps}
+          />
+          <IconCaret direction="down" size={24} csx={style.caret} />
+        </Box>
 
-        <BrowserView>
-          <DesktopSelect {...selectProps} />
-        </BrowserView>
-
-        {message && (
-          <Box csx={{ paddingTop: 1 }}>
-            <Text variant="small" tone={tone} csx={{ lineHeight: 1.5 }}>
-              {message}
-            </Text>
-          </Box>
-        )}
-      </Box>
+        {message && <FieldDetails message={message} tone={tone} />}
+      </FieldContainer>
     )
   }
 )
 
-export { useSelect as useSelectState }
-export { UseSelectReturnValue }
-
-export interface SelectProps<T> extends SystemComponent {
+export interface SelectProps
+  extends Omit<React.ComponentPropsWithoutRef<'select'>, 'css'>,
+    SystemComponent {
   /**
    * Select label
    */
   label: string
-  /**
-   * return of useSelectState
-   */
-  state: UseSelectReturnValue<T>
-  /**
-   * array of items
-   */
-  items: T[]
-  /**
-   * Custom item rendering
-   */
-  renderItem?: (item: T | null) => string
   /**
    * Select helper text
    */
@@ -96,12 +84,4 @@ export interface SelectProps<T> extends SystemComponent {
    * Select critical message
    */
   criticalText?: string
-  /**
-   * Flag to indicate if select is disabled
-   */
-  disabled?: boolean
-  /**
-   * Whether is a block-level element or not
-   * */
-  block?: boolean
 }
--- packages/formik/src/components/FormikSelect/FormikSelect.test.tsx
+++ packages/formik/src/components/FormikSelect/FormikSelect.test.tsx
@@ -1,82 +1,41 @@
 import React from 'react'
-import { render, screen, waitFor } from '../../test-utils'
 import userEvent from '@testing-library/user-event'
-import { IntlProvider, useIntl } from 'react-intl'
 import { Form, Formik } from 'formik'
-import { Button, Text } from '@vtex/admin-ui'
+import { Button } from '@vtex/admin-ui'
 
 import { FormikSelect } from './index'
+import { render, screen, waitFor } from '../../test-utils'
 
-describe('Select tests', () => {
+const options = ['option 1', 'option 2', 'option 3', 'error']
+
+const optNodes = options.map((value) => (
+  <option value={value} data-testid={value} key={value}>
+    {value}
+  </option>
+))
+
+const sharedProps = {
+  name: 'value',
+  children: <>{optNodes}</>,
+  'data-testid': 'select',
+  label: 'Select label',
+}
+
+describe('FormikSelect', () => {
   it('change value in formik by input component', async () => {
     const handleSubmit = jest.fn()
-    const options = ['option 1', 'option 2', 'option 3', 'error']
 
     render(
       <Formik initialValues={{ value: '' }} onSubmit={handleSubmit}>
         <Form id="form-admin-formik-input">
-          <FormikSelect
-            name="value"
-            items={options}
-            data-testid="text-field"
-            label="TextField label"
-          />
+          <FormikSelect {...sharedProps} />
           <Button type="submit" size="small" children="Submit" />
         </Form>
       </Formik>
     )
 
-    userEvent.click(screen.getByRole('button', { name: /TextField label/i }))
-    userEvent.click(screen.getByText(options[2]))
-
-    userEvent.click(screen.getByRole('button', { name: 'Submit' }))
-
-    await waitFor(() =>
-      expect(handleSubmit).toHaveBeenCalledWith(
-        {
-          value: options[2],
-        },
-        expect.anything()
-      )
-    )
-  })
-
-  it('change value in formik by setFieldValue', async () => {
-    const handleSubmit = jest.fn()
-    const options = ['option 1', 'option 2', 'option 3', 'error']
-
-    render(
-      <Formik initialValues={{ value: '' }} onSubmit={handleSubmit}>
-        {({ setFieldValue }) => (
-          <Form id="form-admin-formik-input">
-            <FormikSelect
-              name="value"
-              items={options}
-              data-testid="text-field"
-              label="TextField label"
-            />
-            <Button
-              size="small"
-              children="Change Value"
-              onClick={() => setFieldValue('value', options[0])}
-            />
-            <Button type="submit" size="small" children="Submit" />
-          </Form>
-        )}
-      </Formik>
-    )
-
-    const select = screen.getByRole('button', { name: /TextField label/i })
-
-    userEvent.click(select)
-    userEvent.click(screen.getByText(options[2]))
-    expect(select).toHaveTextContent(options[2])
-
-    userEvent.click(screen.getByRole('button', { name: 'Change Value' }))
-
-    expect(select).toHaveTextContent(options[0])
-
-    userEvent.click(screen.getByRole('button', { name: 'Submit' }))
+    userEvent.selectOptions(screen.getByTestId('select'), options[0])
+    userEvent.click(screen.getByRole('button'))
 
     await waitFor(() =>
       expect(handleSubmit).toHaveBeenCalledWith(
@@ -88,95 +47,49 @@ describe('Select tests', () => {
     )
   })
 
-  it('change initial value in formik', async () => {
+  it('change value in formik by setFieldValue', async () => {
     const handleSubmit = jest.fn()
-    const options = ['option 1', 'option 2', 'option 3', 'error']
-
-    const { rerender } = render(
-      <Formik
-        enableReinitialize
-        initialValues={{ value: '' }}
-        onSubmit={handleSubmit}
-      >
-        <Form id="form-admin-formik-input">
-          <FormikSelect
-            name="value"
-            data-testid="text-field"
-            label="TextField label"
-            items={options}
-          />
-          <Button type="submit" size="small" children="Submit" />
-        </Form>
-      </Formik>
-    )
-
-    const select = screen.getByRole('button', { name: /TextField label/i })
-
-    userEvent.click(select)
-    userEvent.click(screen.getByText(options[2]))
-    await waitFor(() => expect(select).toHaveTextContent(options[2]))
-
-    rerender(
-      <Formik
-        enableReinitialize
-        initialValues={{ value: options[0] }}
-        onSubmit={handleSubmit}
-      >
-        <Form id="form-admin-formik-input">
-          <FormikSelect
-            name="value"
-            items={options}
-            data-testid="text-field"
-            label="TextField label"
-          />
-          <Button type="submit" size="small" children="Submit" />
-        </Form>
-      </Formik>
-    )
-
-    await waitFor(() => expect(select).toHaveTextContent(options[0]))
-  })
-
-  it('set toched when click and untouched when reset forms', async () => {
-    const handleSubmit = jest.fn()
-    const options = ['option 1', 'option 2', 'option 3', 'error']
 
     render(
-      <Formik initialValues={{ value: false }} onSubmit={handleSubmit}>
-        {({ touched, resetForm }) => (
+      <Formik initialValues={{ value: '' }} onSubmit={handleSubmit}>
+        {({ setFieldValue }) => (
           <Form id="form-admin-formik-input">
-            <FormikSelect
-              name="value"
-              items={options}
-              data-testid="text-field"
-              label="TextField label"
-            />
+            <FormikSelect {...sharedProps} />
             <Button
+              data-testid="set-button"
               size="small"
-              children="Reset Form"
-              onClick={() => resetForm()}
+              children="Change Value"
+              onClick={() => setFieldValue('value', options[0])}
             />
-            <Text tone="muted">
-              <pre>{JSON.stringify(touched)}</pre>
-            </Text>
             <Button type="submit" size="small" children="Submit" />
           </Form>
         )}
       </Formik>
     )
 
-    expect(await screen.findByText('{}')).not.toBeNull()
+    const select = screen.getByTestId('select')
+    const setButton = screen.getByTestId('set-button')
+    const submitButton = screen.getByRole('button', { name: 'Submit' })
 
-    userEvent.click(screen.getByRole('button', { name: /TextField label/i }))
-    expect(await screen.findByText('{"value":true}')).not.toBeNull()
+    userEvent.selectOptions(select, options[1])
+    userEvent.click(setButton)
 
-    userEvent.click(screen.getByRole('button', { name: 'Reset Form' }))
-    expect(await screen.findByText('{}')).not.toBeNull()
+    expect(screen.getByTestId('select')).toHaveTextContent(options[0])
+
+    userEvent.click(submitButton)
+
+    await waitFor(() =>
+      expect(handleSubmit).toHaveBeenCalledWith(
+        {
+          value: options[0],
+        },
+        expect.anything()
+      )
+    )
   })
 
   it('error in forms', async () => {
     const handleSubmit = jest.fn()
-    const options = ['option 1', 'option 2', 'option 3', 'error']
 
     const validate = () => ({ value: 'Error message' })
 
@@ -187,126 +100,15 @@ describe('Select tests', () => {
         onSubmit={handleSubmit}
       >
         <Form id="form-admin-formik-input">
-          <FormikSelect
-            name="value"
-            items={options}
-            data-testid="text-field"
-            label="TextField label"
-          />
+          <FormikSelect {...sharedProps} />
           <Button type="submit" size="small" children="Submit" />
         </Form>
       </Formik>
     )
 
-    const select = screen.getByRole('button', { name: /TextField label/i })
+    const select = screen.getByTestId('select')
 
-    userEvent.click(select)
-    userEvent.click(screen.getByText(options[2]))
-
-    expect(await screen.findByText('Error message')).not.toBeNull()
-  })
-
-  it('error in forms with intl', async () => {
-    const options = ['option 1', 'option 2', 'option 3', 'error']
-    const messagesEN = {
-      'admin/admin-formik.error.message': 'Error message',
-    }
-
-    const Content = () => {
-      const handleSubmit = jest.fn()
-      const { formatMessage } = useIntl()
-      const validate = () => ({ value: 'admin/admin-formik.error.message' })
-
-      return (
-        <Formik
-          initialValues={{ value: '' }}
-          validate={validate}
-          onSubmit={handleSubmit}
-        >
-          <Form id="form-admin-formik-input">
-            <FormikSelect
-              name="value"
-              items={options}
-              data-testid="text-field"
-              label="TextField label"
-              formatMessage={(errorCode) => formatMessage({ id: errorCode })}
-            />
-            <Button type="submit" size="small" children="Submit" />
-          </Form>
-        </Formik>
-      )
-    }
-
-    render(
-      <IntlProvider locale="en" messages={messagesEN}>
-        <Content />
-      </IntlProvider>
-    )
-
-    const select = screen.getByRole('button', { name: /TextField label/i })
-
-    userEvent.click(select)
-    userEvent.click(screen.getByText(options[2]))
-
-    expect(await screen.findByText('Error message')).not.toBeNull()
-  })
-
-  it('Select with array of objects', async () => {
-    const handleSubmit = jest.fn()
-    const options = [
-      { id: 'option-1', label: 'Option 1' },
-      { id: 'option-2', label: 'Option 2' },
-      { id: 'option-3', label: 'Option 3' },
-      { id: 'option-error', label: 'Invalid Option' },
-    ]
-
-    const validate = (values: { value: { id: string; label: string } }) => {
-      if (values.value.id === '')
-        return { value: { id: 'This field is required.' } }
-      if (values.value.id === 'option-error')
-        return { value: { id: 'Error message' } }
-
-      return {}
-    }
-
-    render(
-      <Formik
-        initialValues={{ value: { id: '', label: '' } }}
-        onSubmit={handleSubmit}
-        validate={validate}
-      >
-        <Form id="form-admin-formik-input">
-          <FormikSelect
-            name="value"
-            items={options}
-            data-testid="text-field"
-            label="TextField label"
-            itemToString={(item) => (item ? item.label : '')}
-            renderItem={(item) => (item ? item.label : '')}
-          />
-          <Button type="submit" size="small" children="Submit" />
-        </Form>
-      </Formik>
-    )
-
-    userEvent.click(screen.getByRole('button', { name: /TextField label/i }))
-    userEvent.click(screen.getByText(options[1].label))
-
-    userEvent.click(screen.getByRole('button', { name: 'Submit' }))
-
-    await waitFor(() =>
-      expect(handleSubmit).toHaveBeenCalledWith(
-        {
-          value: options[1],
-        },
-        expect.anything()
-      )
-    )
-
-    userEvent.click(screen.getByRole('button', { name: /TextField label/i }))
-    userEvent.click(screen.getByText(options[3].label))
-
-    userEvent.click(screen.getByRole('button', { name: 'Submit' }))
+    userEvent.selectOptions(select, options[2])
 
     expect(await screen.findByText('Error message')).not.toBeNull()
   })
