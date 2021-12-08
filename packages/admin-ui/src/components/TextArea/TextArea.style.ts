9f783c68495f6896599189b4337a71aaac6ce176
matheusps <cout.matheusps@gmail.com>
2021-12-08 17:18:50 -0300

    chore(textarea): migrate color tokens

---
+++ packages/admin-ui/src/components/TextArea/TextArea.style.ts
@@ -0,0 +1,42 @@
+import type { StyleProp } from '@vtex/admin-ui-core'
+import { focusVisible } from '@vtex/admin-ui-core'
+
+function css(csx: StyleProp): StyleProp {
+  return csx
+}
+
+export const baseline = css({
+  fontFamily: 'sans',
+  paddingTop: 24,
+  height: 100,
+  resize: 'none',
+  fontSettings: 'regular',
+  width: 'full',
+  paddingLeft: 3,
+  paddingRight: 4,
+  borderRadius: 'default',
+  marginY: 1,
+  fontSize: 1,
+  outline: 0,
+  transition: 'snap',
+  color: '$form.neutral',
+  ':disabled': {
+    bg: '$disabled',
+    color: '$disabled',
+    border: '$disabled',
+  },
+})
+
+export const toneVariant = (tone: 'neutral' | 'critical') =>
+  css({
+    bg: `$form.${tone}`,
+    border: `$form.${tone}`,
+    ':not(:focus):hover': {
+      border: `$form.${tone}Hover`,
+    },
+    ':focus': {
+      border: `$form.${tone}Focus`,
+      boxShadow: `$ring.${tone}`,
+    },
+    ...focusVisible(tone),
+  })
