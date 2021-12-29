aa92965e7f0d08b761e3041e9a4e2863f2b1dccd
lucasaarcoverde <lucasaarcoverde@gmail.com>
2021-12-29 16:45:30 -0300

    docs(gatsby-site): delete gatsby documentation site

--- packages/site/gatsby-ssr.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import { PageLayout } from './src/components/PageLayout'
-
-export const wrapPageElement = ({ element, props }) => {
-  return <PageLayout {...props}>{element}</PageLayout>
-}
