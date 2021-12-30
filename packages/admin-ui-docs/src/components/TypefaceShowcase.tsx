e2b4b3163972d47e2dcb389e59dfdf153f6a7e2d
matheusps <cout.matheusps@gmail.com>
2021-12-30 11:43:07 -0300

    docs(typography): enhance typography preview

--- packages/admin-ui-docs/src/components/TypefaceShowcase.tsx
+++ packages/admin-ui-docs/src/components/TypefaceShowcase.tsx
@@ -2,89 +2,31 @@ import React, { useState } from 'react'
 import { tag, Flex, jsx } from '@vtex/admin-ui'
 import { Code } from './Code'
 
-const Range = jsx('input')({
+const Input = jsx('input')({
   width: '100%',
-  margin: '8px 0',
+  height: '2rem',
+  border: '$neutral',
   backgroundColor: 'transparent',
-  WebkitAppearance: 'none',
-
-  ':focus': { outline: 'none' },
-  '::-webkit-slider-runnable-track': {
-    background: 'rgba(0, 0, 0, 0.6)',
-    border: '1.1px solid rgba(24, 213, 1, 0)',
-    borderRadius: '25px',
-    width: '100%',
-    height: '2px',
-    cursor: 'pointer',
-  },
-  '::-webkit-slider-thumb': {
-    marginTop: '-9.1px',
-    width: '18px',
-    height: '18px',
-    background: '#21211f',
-    border: '0.2px solid rgba(131, 229, 132, 0)',
-    borderRadius: '12px',
-    cursor: 'pointer',
-    WebkitAppearance: 'none',
-  },
-  ':focus::-webkit-slider-runnable-track': {
-    background: '#4d4d4d',
-  },
-  '::-moz-range-track': {
-    background: 'rgba(0, 0, 0, 0.6)',
-    border: '1.1px solid rgba(24, 213, 1, 0)',
-    borderRadius: '25px',
-    width: '100%',
-    height: '2px',
-    cursor: 'pointer',
-  },
-  '::-moz-range-thumb': {
-    width: '18px',
-    height: '18px',
-    background: '#21211f',
-    border: '0.2px solid rgba(131, 229, 132, 0)',
-    borderRadius: '12px',
-    cursor: 'pointer',
-  },
-  '::-ms-track': {
-    background: 'transparent',
-    borderColor: 'transparent',
-    borderWidth: '10.6px 0',
-    color: 'transparent',
-    width: '100%',
-    height: '2px',
-    cursor: 'pointer',
-  },
-  '::-ms-fill-lower': {
-    background: '#000000',
-    border: '1.1px solid rgba(24, 213, 1, 0)',
-    borderRadius: '50px',
-  },
-  '::-ms-fill-upper': {
-    background: 'rgba(0, 0, 0, 0.6)',
-    border: '1.1px solid rgba(24, 213, 1, 0)',
-    borderRadius: '50px',
-  },
-  '::-ms-thumb': {
-    width: '18px',
-    height: '18px',
-    background: '#21211f',
-    border: '0.2px solid rgba(131, 229, 132, 0)',
-    borderRadius: '12px',
-    cursor: 'pointer',
-    marginTop: '0px',
-  },
-  ':focus::-ms-fill-lower': {
-    background: 'rgba(0, 0, 0, 0.6)',
-  },
-  ':focus::-ms-fill-upper': { background: '#4d4d4d' },
-  '@supports (-ms-ime-align:auto)': { margin: '0' },
 })
 
-Range.defaultProps = {
-  type: 'range',
+Input.defaultProps = {
+  type: 'number',
   min: 0,
 }
+
+const Select = jsx('select')({
+  width: '100%',
+  height: '2rem',
+  border: '$neutral',
+  backgroundColor: 'transparent',
+})
+
+const Container = jsx('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  borderRadius: 8,
+  border: '$neutral',
+})
 
 const Row = jsx('tr')({})
 
@@ -94,20 +36,17 @@ const Cell = jsx('td')({
 })
 
 export function TypefaceShowcase() {
-  const [fontSize, setFontSize] = useState(80)
+  const [fontSize, setFontSize] = useState(24)
   const [wght, setWght] = useState(500)
   const [wdth, setWdth] = useState(1000)
   const [ital, setItal] = useState(0)
+  const [lineHeight, setLineHeight] = useState(1)
+  const [letterSpacing, setLetterSpacing] = useState(0)
+  const [content, setContent] = useState('All Products')
+  const [space, setSpace] = useState('8px')
 
   return (
-    <tag.div
-      csx={{
-        display: 'flex',
-        flexDirection: 'column',
-        borderRadius: 8,
-        border: 'default',
-      }}
-    >
+    <Container>
       <Flex
         justify="center"
         align="center"
@@ -119,12 +58,15 @@ export function TypefaceShowcase() {
       >
         <tag.p
           csx={{
-            padding: 2,
+            paddingY: 2,
+            paddingX: space,
             fontSize,
             fontVariationSettings: `"WGHT" ${wght}, "WDTH" ${wdth}, "ITAL" ${ital}`,
+            lineHeight,
+            letterSpacing: `${letterSpacing}em`,
           }}
         >
-          VTEX Trust
+          {content}
         </tag.p>
       </Flex>
       <Flex
@@ -144,43 +86,102 @@ export function TypefaceShowcase() {
           <tag.table csx={{ width: '100%' }}>
             <tag.tbody>
               <Row>
+                <Cell>Content Type</Cell>
+                <Cell>
+                  <Select
+                    value={content}
+                    onChange={(e: any) => setContent(e.target.value)}
+                  >
+                    <option value="All Products">Page title</option>
+                    <option value="Itens to invoice">Title 1</option>
+                    <option value="Package 1">Title 2</option>
+                    <option value="Publish">Action</option>
+                    <option value="1.273">Display</option>
+                    <option value="Designed to take on anything mother-nature has to offer the Guide Jacket from Simms will shield you from all of the relentless elements she brings. Constructed of a Gore-Tex shell and two top-loading pockets for ultimate storage and fast access.">
+                      Body
+                    </option>
+                    <option value="Images must be in the JPEG, PNG or GIF format, with up to 5MB each">
+                      Detail
+                    </option>
+                  </Select>
+                </Cell>
+              </Row>
+              <Row>
+                <Cell>Space</Cell>
+                <Cell>
+                  <Select
+                    value={space}
+                    onChange={(e: any) => setSpace(e.target.value)}
+                  >
+                    <option value="8px">Wide</option>
+                    <option value="152px">Narrow</option>
+                  </Select>
+                </Cell>
+              </Row>
+              <Row>
                 <Cell>Font Size</Cell>
                 <Cell>
-                  <Range
+                  <Input
                     value={fontSize}
                     onChange={(e: any) => setFontSize(Number(e.target.value))}
-                    max={200}
+                    max={80}
                   />
                 </Cell>
               </Row>
               <Row>
                 <Cell>WGHT</Cell>
                 <Cell>
-                  <Range
+                  <Input
                     value={wght}
                     onChange={(e: any) => setWght(Number(e.target.value))}
                     max={1000}
+                    step={100}
                   />
                 </Cell>
               </Row>
               <Row>
                 <Cell>WDTH</Cell>
                 <Cell>
-                  <Range
+                  <Input
                     value={wdth}
                     onChange={(e: any) => setWdth(Number(e.target.value))}
                     max={1000}
+                    step={100}
                   />
                 </Cell>
               </Row>
               <Row>
                 <Cell>ITAL</Cell>
                 <Cell>
-                  <Range
+                  <Input
                     value={ital}
                     onChange={(e: any) => setItal(Number(e.target.value))}
                     step={0.1}
                     max={1}
+                  />
+                </Cell>
+              </Row>
+              <Row>
+                <Cell>Line Height</Cell>
+                <Cell>
+                  <Input
+                    value={lineHeight}
+                    onChange={(e: any) => setLineHeight(Number(e.target.value))}
+                    step={0.1}
+                    max={10}
+                  />
+                </Cell>
+              </Row>
+              <Row>
+                <Cell>Letter Spacing</Cell>
+                <Cell>
+                  <Input
+                    value={letterSpacing}
+                    onChange={(e: any) =>
+                      setLetterSpacing(Number(e.target.value))
+                    }
+                    step={0.001}
+                    max={10}
                   />
                 </Cell>
               </Row>
@@ -199,14 +200,16 @@ export function TypefaceShowcase() {
             codeString={`<tag.p
   csx={{
     fontSize: ${fontSize},
-    fontVariationSettings: "'WGHT' ${wght}, 'WDTH' ${wdth}, 'ITAL' ${ital}"
+    fontVariationSettings: "'WGHT' ${wght}, 'WDTH' ${wdth}, 'ITAL' ${ital}",
+    lineHeight: ${lineHeight},
+    letterSpacing: ${letterSpacing}em,
   }}
 >
-  VTEX Trust
+  ${content}
 </tag.p>`}
           />
         </Flex>
       </Flex>
-    </tag.div>
+    </Container>
   )
 }
