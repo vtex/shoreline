bc9c6649dc5ca0bb7e5af73e6fae207e63724f68
matheusps <cout.matheusps@gmail.com>
2022-02-25 10:49:47 -0300

    feat(admin-ui): make datepicker and i18nprovider as experimental

---
+++ packages/admin-ui/src/experimental.ts
@@ -0,0 +1,21 @@
+import {
+  DatePicker as experimental_DatePicker,
+  DatePickerProps as experimental_DatePickerProps,
+  DatePickerInitialState as experimental_DatePickerInitialState,
+  DatePickerStateReturn as experimental_DatePickerStateReturn,
+  useDatePickerState as experimental_useDatePickerState,
+} from './date-picker'
+import {
+  I18nProvider as experimental_I18nProvider,
+  useDateFormatter as experimental_useDateFormatter,
+} from './i18n'
+
+export {
+  experimental_DatePicker,
+  experimental_DatePickerProps,
+  experimental_DatePickerInitialState,
+  experimental_DatePickerStateReturn,
+  experimental_useDatePickerState,
+  experimental_I18nProvider,
+  experimental_useDateFormatter,
+}
