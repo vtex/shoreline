6ab6cc297fdbf9821e3f62f6d6e409fd2fab6f06
anitavincent <paes.anita@gmail.com>
2022-03-31 17:17:43 -0300

    fix(combobox): fix typing error on build

--- packages/admin-ui/src/combobox/combobox-item.ts
+++ packages/admin-ui/src/combobox/combobox-item.ts
@@ -1,12 +1,21 @@
 import { createComponent, useElement } from '@vtex/admin-ui-react'
-import { ComboboxItem as AriakitComboboxItem } from 'ariakit/combobox'
+import type { ComboboxItemOptions } from 'ariakit/combobox'
+import { useComboboxItem } from 'ariakit/combobox'
+import type { HTMLProps } from 'react'
 
 import * as style from './combobox.style'
 
-export const ComboboxItem = createComponent<typeof AriakitComboboxItem>(
+export const ComboboxItem = createComponent<'div', ComboboxItemOptions>(
   (props) => {
-    return useElement(AriakitComboboxItem, {
-      ...props,
+    // weird, the input of the hook should be exactly ComboboxItemOptions
+    const htmlProps = useComboboxItem(props as any) as Omit<
+      HTMLProps<'div'>,
+      never
+    >
+
+    return useElement('div', {
+      ...htmlProps,
+      children: props.children,
       baseStyle: style.item,
     })
   }
