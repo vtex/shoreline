0fbabc262545167107bdd91a9d7792f9b6f60775
Lenderson Morais <lenderson.morais@vtex.com.br>
2022-03-31 12:10:56 -0300

    refactor: update page with new header

--- packages/admin-ui-docs/src/components/PageHeader/index.tsx
+++ packages/admin-ui-docs/src/components/PageHeader/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { tag } from '@vtex/admin-ui-react'
+import { useSidebarBreadcrumbs } from '@docusaurus/theme-common'
 
 import styles from './styles'
 
@@ -7,24 +8,36 @@ interface PageHeaderProps {
   title: string
   componentPath: string
 }
+
 const GITHUB_BASE_URL =
   'https://github.com/vtex/admin-ui/tree/main/packages/admin-ui/src/'
 
+const FIGMA_URL =
+  'https://www.figma.com/file/sepBhrHViUHyrTnktkuHaW/Admin-UI-%E2%80%93-Components?node-id=586%3A243904'
+
 export function PageHeader(props: PageHeaderProps) {
-  const breadcrumbs = window.location.pathname.split('/').filter(Boolean)
-  const firstBreadcrumb = breadcrumbs[0] ?? 'introduction'
+  const breadcrumbs = useSidebarBreadcrumbs()
+  const firstBreadcrumb = breadcrumbs?.[0].label ?? ''
 
   return (
     <tag.header csx={styles.container}>
       <tag.div csx={styles.caption}>
         <tag.p csx={styles.breadcrumb}>{firstBreadcrumb}</tag.p>
         {props.componentPath && (
-          <tag.a
-            csx={styles.gitHubUrl}
-            href={`${GITHUB_BASE_URL}${props.componentPath}`}
-            target="_blank"
-            aria-label="GitHub repository"
-          />
+          <tag.span csx={styles.logosContainer}>
+            <tag.a
+              csx={styles.gitHubUrl}
+              href={`${GITHUB_BASE_URL}${props.componentPath}`}
+              target="_blank"
+              aria-label="Component url in the GitHub repository"
+            />
+            <tag.a
+              csx={styles.figma}
+              href={FIGMA_URL}
+              target="_blank"
+              aria-label="Component url in the Figma"
+            />
+          </tag.span>
         )}
       </tag.div>
       <tag.h1 csx={styles.title}>{props.title}</tag.h1>
