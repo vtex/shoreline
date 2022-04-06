89bbc2c2819859ec841873ee7a7c88716377788f
Lenderson Morais <lenderson.morais@vtex.com.br>
2022-04-06 20:30:29 -0300

    refactor(page): add table of content(toc) for each tab

--- packages/admin-ui-docs/src/components/Page/index.tsx
+++ packages/admin-ui-docs/src/components/Page/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 
 import { Tabs, Tab, TabList, useTabState, colors } from '@vtex/admin-ui'
+import TOC from '@theme/TOC'
 
 import '@site/src/components/Page/styles.scss'
 
-interface PageProps {
+export type TabTocType = {
+  overview: String[]
+  designReference: String[]
+  codeReference: String[]
+}
+
+export interface PageProps {
+  tabToc: TabTocType
   children: Node
 }
-const getTabColor = (selectedId: any, tabId: string) => {
+
+const getTabColor = (selectedId: string, tabId: string) => {
   const isTabSelected = selectedId === tabId
   const selectedTabColor = isTabSelected ? colors.pink40 : '$primary'
 
@@ -20,29 +29,31 @@ const getTabColor = (selectedId: any, tabId: string) => {
   }
 }
 
+const tabName = {
+  overview: 'Overview',
+  designReference: 'Design Reference',
+  codeReference: 'Code Reference',
+}
+
 export function Page(props: PageProps) {
-  const state = useTabState()
+  const state = useTabState({ selectedId: 'overview' })
 
   return (
     <Tabs state={state}>
       <TabList className="tab-list" aria-label="Component page tabs">
-        <Tab id="overview" csx={getTabColor(state.selectedId, 'overview')}>
-          Overview
-        </Tab>
-        <Tab
-          id="design-reference"
-          csx={getTabColor(state.selectedId, 'design-reference')}
-        >
-          Design Reference
-        </Tab>
-        <Tab
-          id="code-reference"
-          csx={getTabColor(state.selectedId, 'code-reference')}
-        >
-          Code Reference
-        </Tab>
+        {Object.keys(props.tabToc).map((key) => (
+          <Tab id={key} csx={getTabColor(state.selectedId as string, key)}>
+            {tabName[key as keyof Object]}
+          </Tab>
+        ))}
       </TabList>
-      {props.children}
+      <div className="tab-container">
+        {props.children}
+        <TOC
+          className="toc-container"
+          toc={props.tabToc[state.selectedId as keyof TabTocType]}
+        />
+      </div>
     </Tabs>
   )
 }
