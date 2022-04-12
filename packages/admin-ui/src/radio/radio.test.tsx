9ca00c27cd465129305758bdf57948c6d270cb5c
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-04-12 16:12:49 -0300

    feat(radio): add radio component review

---
+++ packages/admin-ui/src/radio/radio.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import { render, axe, withState } from '../test-utils'
+import { Radio, useRadioState } from './radio'
+
+const StatefulRadio = withState(Radio, useRadioState)
+
+describe('Radio', () => {
+  it('should match snapshot', () => {
+    const { asFragment } = render(
+      <>
+        <StatefulRadio value="unchecked" label="label" />
+        <StatefulRadio value="checked" label="label" checked />
+        <StatefulRadio value="unchecked disabled" label="label" disabled />
+        <StatefulRadio
+          value="checked disabled"
+          label="label"
+          disabled
+          checked
+        />
+      </>
+    )
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('should not have a11y violations', async () => {
+    const { container } = render(<StatefulRadio value="test" label="test" />)
+
+    const results = await axe(container)
+
+    expect(results).toHaveNoViolations()
+  })
+})
