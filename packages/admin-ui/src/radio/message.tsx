69d52378e7061123f2b408abc1179591f797e74c
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-04-13 18:08:36 -0300

    feat(radio): add message component

---
+++ packages/admin-ui/src/radio/message.tsx
@@ -0,0 +1,35 @@
+import type { ReactNode } from 'react'
+import React from 'react'
+
+import { Set } from '../components/Set'
+import { Text } from '../components/Text'
+
+export function Message(props: MessageProps) {
+  const { helperText, criticalMessage, tone } = props
+
+  const isCritical = tone === 'critical' && criticalMessage
+  const hasMessage = isCritical || helperText
+
+  return hasMessage ? (
+    <Set spacing="$xs" orientation="vertical">
+      {helperText ? (
+        <Text variant="detail" tone="secondary">
+          {helperText}
+        </Text>
+      ) : null}
+      {isCritical ? (
+        <Text variant="detail" tone={tone}>
+          {criticalMessage}
+        </Text>
+      ) : null}
+    </Set>
+  ) : (
+    <></>
+  )
+}
+
+export interface MessageProps {
+  helperText?: ReactNode
+  criticalMessage?: ReactNode
+  tone?: 'critical' | 'neutral'
+}
