3494ec1131d3a41fac17c82fcdcb84fd32b407b3
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-05-02 13:26:32 -0300

    refactor(radio): add i18n optional text to RadioGroup

---
+++ packages/admin-ui/src/radio/radio.i18n.ts
@@ -0,0 +1,14 @@
+export const messages = {
+  radioGroup: {
+    'en-US': { optional: 'Optional' },
+    'es-AR': { optional: 'Optional' },
+    'fr-FR': { optional: 'Optional' },
+    'pt-BR': { optional: 'Opcional' },
+    'ja-JP': { optional: 'Optional' },
+    'ko-KR': { optional: 'Optional' },
+    'it-IT': { optional: 'Optional' },
+    'nl-NL': { optional: 'Optional' },
+    'ro-RO': { optional: 'Optional' },
+    'bg-BG': { optional: 'Optional' },
+  },
+}
