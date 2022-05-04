0bd0f27d30a92bd60b8e9eb3cdd1585233b84aff
Lenderson Morais <lenderson.morais@vtex.com.br>
2022-05-04 19:03:20 -0300

    docs: update checkbox documentation

--- packages/admin-ui-core/src/tokens/fg.ts
+++ packages/admin-ui-core/src/tokens/fg.ts
@@ -50,7 +50,6 @@ export const fg = {
     neutral: colors.black,
     neutralChecked: colors.white,
 
-    control: 'transparent',
     controlNeutral: colors.black,
     controlChecked: colors.white,
 
