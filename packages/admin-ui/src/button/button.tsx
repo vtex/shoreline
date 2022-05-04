457b272f2195e4e28983699d4f8e43fcf39bfb74
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-05-04 17:35:28 -0300

    fix(search): fix rebase issues

--- packages/admin-ui/src/button/button.tsx
+++ packages/admin-ui/src/button/button.tsx
@@ -23,7 +23,7 @@ export const useButton = createHook<typeof ReakitButton, ButtonOptions>(
     icon,
     size = 'normal',
     variant = 'primary',
-    iconPosition = 'start',
+    iconPosition: defaultIconPosition = 'start',
     loading = false,
     disabled = false,
     bleedY = false,
@@ -31,6 +31,10 @@ export const useButton = createHook<typeof ReakitButton, ButtonOptions>(
     children,
     ...props
   }) => {
+    const iconOnly = icon && !children
+
+    const iconPosition = iconOnly ? 'center' : defaultIconPosition
+
     const bleedYStyle = bleedY
       ? style.bleedY({
           size,
