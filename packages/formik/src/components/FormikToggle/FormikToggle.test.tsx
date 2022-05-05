630641b72050b1f062e0b46fe9649adbcfbf6671
marcelovicentegc <marcelovicentegc@gmail.com>
2022-05-05 17:02:20 -0300

    chore(switch): remove floating ring and add tokens

--- packages/formik/src/components/FormikToggle/FormikToggle.test.tsx
+++ packages/formik/src/components/FormikToggle/FormikToggle.test.tsx
@@ -75,7 +75,10 @@ describe('Toggle tests', () => {
               data-testid="text-field"
               label="TextField label"
             />
-            <Button children="Change Value" onClick={() => setFieldValue('value', false)} />
+            <Button
+              children="Change Value"
+              onClick={() => setFieldValue('value', false)}
+            />
             <Button type="submit" children="Submit" />
           </Form>
         )}
@@ -253,7 +256,7 @@ describe('Toggle tests', () => {
             <Button type="submit" children="Submit" />
           </Form>
         </Formik>
-      );
+      )
     }
 
     render(
