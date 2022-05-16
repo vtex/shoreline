e1bb35aa0ffb4bbeb32a192c727ad1a9399b6642
anitavincent <paes.anita@gmail.com>
2022-05-16 15:35:32 -0300

    docs(textarea): add more storybook examples

--- packages/admin-ui/src/text-area/stories/text-area.stories.tsx
+++ packages/admin-ui/src/text-area/stories/text-area.stories.tsx
@@ -1,21 +1,19 @@
-import React, { useRef, useState } from 'react'
+import React, { useLayoutEffect, useRef, useState } from 'react'
 import type { Meta } from '@storybook/react'
 
 import { Stack } from '../../stack'
 import { TextArea } from '../text-area'
+import { Box } from '../../components/Box'
 
 export default {
   title: 'admin-ui-review/text-area',
 } as Meta
 
 export const Uncontrolled = () => {
-  const ref = useRef<HTMLTextAreaElement>(null)
-
   return (
-    <Stack space="$l" csx={{ padding: '$s' }} fluid>
-      <TextArea label="Uncontrolled" charLimit={120} ref={ref} />
-      <TextArea label="Error text" errorText="Error text" error />
-    </Stack>
+    <Box csx={{ padding: '$s', width: '300px' }}>
+      <TextArea label="Uncontrolled" />
+    </Box>
   )
 }
 
@@ -23,7 +21,7 @@ export const Controlled = () => {
   const [value, setValue] = useState<string>('')
 
   return (
-    <Stack space="$l" csx={{ padding: '$s' }} fluid>
+    <Stack space="$l" csx={{ padding: '$s' }}>
       <TextArea
         label="Controlled"
         value={value}
@@ -31,8 +29,54 @@ export const Controlled = () => {
         helpText="help"
         errorText="Error text"
         error
+      />
+    </Stack>
+  )
+}
+
+export const States = () => {
+  return (
+    <Stack space="$l" csx={{ padding: '$s' }}>
+      <TextArea label="Plain" />
+      <TextArea
+        label="With Error and help"
+        id="error-input"
+        errorText="Error text"
+        helpText="help text"
+        error
+      />
+      <TextArea
+        label="With Help text and counter"
+        id="help-input"
+        helpText="help text"
         charLimit={120}
       />
+      <TextArea
+        label="With Error, help and counter"
+        id="error-counter-input"
+        errorText="Error text"
+        helpText="help text"
+        charLimit={120}
+        error
+      />
+      <TextArea label="disabled" disabled />
+    </Stack>
+  )
+}
+
+export const ForwardRef = () => {
+  const ref = useRef<HTMLTextAreaElement>(null)
+
+  useLayoutEffect(() => {
+    if (ref.current) {
+      ref.current.value = 'Text'
+      ref.current.focus()
+    }
+  }, [])
+
+  return (
+    <Stack space="$l" csx={{ padding: '$s' }}>
+      <TextArea ref={ref} />
     </Stack>
   )
 }
