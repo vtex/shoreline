d32ad0bdb3d398386e42e04695da2b7d162ef2a3
anitavincent <paes.anita@gmail.com>
2022-05-16 13:48:27 -0300

    feat(textarea): add autoresize and charlimit counter

--- packages/admin-ui/src/text-area/text-area.style.ts
+++ packages/admin-ui/src/text-area/text-area.style.ts
@@ -1,9 +1,11 @@
 import { style } from '@vtex/admin-ui-core'
 
+export const MAX_HEIGHT = 440
+
 export const container = style({
   display: 'block',
   minHeight: '68px',
-  maxHeight: '120px',
+  maxHeight: `${MAX_HEIGHT}px`,
   resize: 'none',
   transition: 'snap',
 })
--- packages/admin-ui/src/text-area/text-area.tsx
+++ packages/admin-ui/src/text-area/text-area.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode, ComponentPropsWithoutRef, Ref } from 'react'
-import React, { useState, forwardRef } from 'react'
+import React, { useRef, useState, forwardRef } from 'react'
 import { useId } from '@vtex/admin-ui-hooks'
 
 import { TextAreaContainer } from './text-area-container'
@@ -12,6 +12,7 @@ import {
   FormControlMessage,
 } from '../form-control'
 import { Flex, FlexSpacer } from '..'
+import { MAX_HEIGHT } from './text-area.style'
 
 export const TextArea = forwardRef(
   (props: TextAreaProps, ref: Ref<HTMLTextAreaElement>) => {
@@ -28,13 +29,70 @@ export const TextArea = forwardRef(
     } = props
 
     const id = useId(defaultId)
+    const containerRef = useRef<any>(null)
 
     const [charCount, setCharCount] = useState(0)
+    const [lineHeight, setLineHeight] = useState<number>(0)
+    const [minScrollHeight, setMinScrollHeight] = useState<number>(0)
+
+    const getLineHeight = (elm: HTMLTextAreaElement) => {
+      const savedRows = elm.rows
+      const savedValue = elm.value
+
+      elm.value = ''
+      elm.rows = 1
+      const base1 = elm.scrollHeight
+
+      elm.rows = 2
+      const base2 = elm.scrollHeight
+
+      elm.rows = savedRows
+      elm.value = savedValue
+
+      const calculatedHeight = base2 - base1
+
+      setLineHeight(calculatedHeight)
+
+      return calculatedHeight
+    }
+
+    const getScrollHeight = (elm: HTMLTextAreaElement) => {
+      const savedValue = elm.value
+
+      elm.value = ''
+
+      const baseScrollHeight = elm.scrollHeight
+
+      elm.value = savedValue
+
+      setMinScrollHeight(baseScrollHeight)
+
+      return baseScrollHeight
+    }
+
+    const onExpandableTextareaInput = (
+      e: React.FormEvent<HTMLTextAreaElement>
+    ) => {
+      const elm = e.target as HTMLTextAreaElement
+      const baseRows = 2
+
+      lineHeight || getLineHeight(elm)
+      minScrollHeight || getScrollHeight(elm)
+
+      elm.rows = baseRows
+      const newHeight = Math.min(elm.scrollHeight, MAX_HEIGHT - lineHeight)
+
+      const extraRows: number = Math.ceil(
+        (newHeight - minScrollHeight) / lineHeight
+      )
+
+      elm.rows = baseRows + extraRows
+    }
 
     return (
       <FormControl error={error}>
         {label && <FormControlLabel htmlFor={id}>{label}</FormControlLabel>}
-        <TextAreaContainer error={error} disabled={disabled}>
+        <TextAreaContainer error={error} disabled={disabled} ref={containerRef}>
           <TextAreaElement
             ref={ref}
             disabled={disabled}
@@ -43,6 +101,7 @@ export const TextArea = forwardRef(
               setCharCount(e.currentTarget.value.toString().length)
               onChange?.(e)
             }}
+            onInput={onExpandableTextareaInput}
             maxLength={charLimit}
             {...inputProps}
           />
