170a5f26a081e9a7dc2e4496a407ea3a6a5c1dc2
anitavincent <paes.anita@gmail.com>
2022-05-18 11:04:19 -0300

    refactor(textarea): simplify auto resize logic

--- packages/admin-ui/src/text-area/useTextArea.ts
+++ packages/admin-ui/src/text-area/useTextArea.ts
@@ -1,64 +1,25 @@
 import { useState } from 'react'
-import { MAX_HEIGHT } from './text-area.style'
-
-const MIN_AREA_ROWS = 2
+import {
+  LINE_HEIGHT,
+  MIN_TEXT_AREA_HEIGHT,
+  MIN_AREA_ROWS,
+  MAX_TEXT_AREA_HEIGHT,
+} from './text-area-constants'
 
 export function useTextarea() {
   const [charCount, setCharCount] = useState(0)
-  const [lineHeight, setLineHeight] = useState<number>(0)
-  const [minScrollHeight, setMinScrollHeight] = useState<number>(0)
-
-  const getLineHeight = (elm: HTMLTextAreaElement) => {
-    if (lineHeight) {
-      return lineHeight
-    }
-
-    const savedRows = elm.rows
-    const savedValue = elm.value
-
-    elm.value = ''
-    elm.rows = 1
-    const base1 = elm.scrollHeight
-
-    elm.rows = 2
-    const base2 = elm.scrollHeight
-
-    elm.rows = savedRows
-    elm.value = savedValue
-
-    const calculatedHeight = base2 - base1
-
-    setLineHeight(calculatedHeight)
-
-    return calculatedHeight
-  }
-
-  const getMinScrollHeight = (elm: HTMLTextAreaElement) => {
-    if (minScrollHeight) {
-      return minScrollHeight
-    }
-
-    const savedValue = elm.value
-
-    elm.value = ''
-
-    const baseScrollHeight = elm.scrollHeight
-
-    elm.value = savedValue
-
-    setMinScrollHeight(baseScrollHeight)
-
-    return baseScrollHeight
-  }
 
   const resizeTextArea = (e: React.FormEvent<HTMLTextAreaElement>) => {
     const elm = e.target as HTMLTextAreaElement
 
-    const lineHeight = getLineHeight(elm)
-    const minScrollHeight = getMinScrollHeight(elm)
+    const lineHeight = LINE_HEIGHT
+    const minScrollHeight = MIN_TEXT_AREA_HEIGHT
 
     elm.rows = MIN_AREA_ROWS
-    const newHeight = Math.min(elm.scrollHeight, MAX_HEIGHT - lineHeight)
+    const newHeight = Math.min(
+      elm.scrollHeight,
+      MAX_TEXT_AREA_HEIGHT - lineHeight
+    )
 
     const extraRows: number = Math.ceil(
       (newHeight - minScrollHeight) / lineHeight
