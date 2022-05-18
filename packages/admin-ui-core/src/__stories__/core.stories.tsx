8613e56143a06180aec4ad4c62a06550e667625c
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-05-18 10:55:36 -0300

    fix(stitches): improve chained selector alias

--- packages/admin-ui-core/src/__stories__/core.stories.tsx
+++ packages/admin-ui-core/src/__stories__/core.stories.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import type { Meta } from '@storybook/react'
 
 import { createCsx } from '../index'
+import { theme } from '../theme'
 
 export default {
   title: 'admin-ui-core/core',
 } as Meta
 
 // join the parser w/ emotion.css
-const csx = createCsx()
+const csx = createCsx(theme)
 
 export function FrameworkdAgnostic() {
   return (
@@ -20,13 +21,19 @@ export function FrameworkdAgnostic() {
         ':hover': {
           bg: '$action.neutral.tertiaryHover',
         },
+        div: {
+          bg: '$secondary',
+        },
         padding: 1,
         marginY: 2,
         size: 150,
         borderRadius: 4,
       })}
     >
-      <div className="test">Framework Agnostic Box</div>
+      <div className={csx({ '+ button': { bg: '$action.critical.primary' } })}>
+        Framework Agnostic Box
+      </div>
+      <button>test button</button>
     </div>
   )
 }
