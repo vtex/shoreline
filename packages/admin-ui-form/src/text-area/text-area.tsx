be1b343ea876114af1fd5ca5aef792597f090baf
anitavincent <paes.anita@gmail.com>
2022-05-19 15:52:11 -0300

    feat(textarea): include textarea component to admin-ui-form

---
+++ packages/admin-ui-form/src/text-area/text-area.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import type { next_TextAreaProps as AdminUITextAreaProps } from '@vtex/admin-ui'
+import { next_TextArea as AdminUITextArea } from '@vtex/admin-ui'
+
+import type { FormState, RegisterOptions } from '../form'
+import { hasError, getErrorText, useFieldDx } from '../util'
+
+/**
+ * Admin UI TextArea controlled by useFormState
+ * @example
+ * const form = useFormState()
+ *
+ * <TextArea name="required-name" state={form} />
+ */
+export function TextArea(props: TextInputProps) {
+  const { state, name = '', validation, ...rest } = props
+  const { register } = state
+
+  useFieldDx(props)
+
+  return (
+    <AdminUITextArea
+      error={hasError(state, name)}
+      errorText={getErrorText(state, name)}
+      {...register(name, validation)}
+      {...rest}
+    />
+  )
+}
+
+type InputHiddenProps =
+  | 'name'
+  | 'error'
+  | 'errorText'
+  | 'required'
+  | 'onChange'
+  | 'onBlur'
+  | 'minLength'
+  | 'max'
+  | 'min'
+  | 'pattern'
+  | 'disabled'
+  | 'value'
+
+type InheritedProps = Omit<AdminUITextAreaProps, InputHiddenProps>
+
+export interface TextInputProps extends InheritedProps {
+  /**
+   * Input required state
+   */
+  name: string
+  /**
+   * Form state
+   */
+  state: FormState
+  /**
+   * Field validation
+   */
+  validation?: RegisterOptions
+}
