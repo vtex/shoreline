021a303d5ecf51af6134e836951af6c914f2d8c3
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-05-19 15:54:14 -0300

    feat(select-inline): add select-inline component review

--- packages/admin-ui/src/select/select.tsx
+++ packages/admin-ui/src/select/select.tsx
@@ -3,17 +3,13 @@ import React, { forwardRef } from 'react'
 import { useId } from '@vtex/admin-ui-hooks'
 import type { StyleProp } from '@vtex/admin-ui-core'
 
-import * as styles from './select.styles'
 import type { SelectInputOptions } from './select-input'
-import { SelectInput } from './select-input'
+import { SelectInput, SelectInputContainer } from './select-input'
 import {
   FormControl,
   FormControlLabel,
   FormControlMessage,
 } from '../form-control'
-import { Box } from '../box'
-import { Stack } from '../stack'
-import { IconCaretDown, IconCaretUp } from '@vtex/phosphor-icons'
 
 export const Select = forwardRef(
   (props: SelectProps, ref: Ref<HTMLSelectElement>) => {
@@ -36,15 +32,11 @@ export const Select = forwardRef(
         {label ? (
           <FormControlLabel htmlFor={id}>{label}</FormControlLabel>
         ) : null}
-        <Box csx={{ position: 'relative' }}>
+        <SelectInputContainer>
           <SelectInput id={id} ref={ref} error={error} {...selectProps}>
             {children}
           </SelectInput>
-          <Stack space="0" csx={styles.caret}>
-            <IconCaretUp width="12" height="12" />
-            <IconCaretDown width="12" height="12" />
-          </Stack>
-        </Box>
+        </SelectInputContainer>
         <FormControlMessage helpText={helpText} errorText={errorText} />
       </FormControl>
     )
@@ -53,12 +45,12 @@ export const Select = forwardRef(
 
 type JSXSelectProps = ComponentPropsWithoutRef<'select'>
 
-export interface SelectOptions extends SelectInputOptions {
+export interface SelectOptions extends Omit<SelectInputOptions, 'variant'> {
   label?: ReactNode
+  csx?: StyleProp
   helpText?: ReactNode
   errorText?: ReactNode
   optional?: boolean
-  csx?: StyleProp
 }
 
 export type SelectProps = SelectOptions &
