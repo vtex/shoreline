b7b7e6ff875083b11541d11dec4d3a85f7ba6999
Lenderson Morais <lenderson.morais@vtex.com.br>
2022-05-24 18:18:30 -0300

    refactor: add number input i18n file

---
+++ packages/admin-ui/src/number-input/number-input.i18n.ts
@@ -0,0 +1,14 @@
+export const messages = {
+  formControl: {
+    'en-US': { decrement: 'Decrement', increment: 'Increment' },
+    'es-AR': { decrement: 'Decrementar', increment: 'Incrementar' },
+    'fr-FR': { decrement: 'Décrémenter', increment: 'Incrémenter' },
+    'pt-BR': { decrement: 'Decrementar', increment: 'Incrementar' },
+    'ja-JP': { decrement: 'デクリメント', increment: 'インクリメント' },
+    'ko-KR': { decrement: '감소', increment: '증가' },
+    'it-IT': { decrement: 'Decremento', increment: 'Incremento' },
+    'nl-NL': { decrement: 'Verlagen', increment: 'Increment' },
+    'ro-RO': { decrement: 'Scăderea', increment: 'Creştere' },
+    'bg-BG': { decrement: 'Намаляване', increment: 'Увеличение' },
+  },
+}
