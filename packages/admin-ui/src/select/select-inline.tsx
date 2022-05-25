fa3af99bcb0a24d875bca59d29eb80034633e901
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-05-25 17:36:38 -0300

    refactor(select): add design requested changes

--- packages/admin-ui/src/select/select-inline.tsx
+++ packages/admin-ui/src/select/select-inline.tsx
@@ -1,35 +1,53 @@
-import type { ComponentPropsWithoutRef, Ref } from 'react'
+import type { Ref } from 'react'
 import React, { forwardRef } from 'react'
 import { useId } from '@vtex/admin-ui-hooks'
 import type { StyleProp } from '@vtex/admin-ui-core'
 
-import { SelectInput, SelectInputContainer } from './select-input'
+import * as styles from './select.styles'
+import type { JSXSelectProps } from './select-input'
+import { SelectInput, SelectContainer } from './select-input'
 import { VisuallyHidden } from 'ariakit'
 
 export const SelectInline = forwardRef(
   (props: SelectInlineProps, ref: Ref<HTMLSelectElement>) => {
-    const { children, label, id: defaultId, csx, ...selectProps } = props
+    const {
+      children,
+      label,
+      id: defaultId,
+      bleedY = false,
+      bleedX = false,
+      csx,
+      ...selectProps
+    } = props
 
     const id = useId(defaultId)
+    const variant = 'inline'
 
     return (
-      <SelectInputContainer csx={{ width: 'fit-content', ...csx }}>
+      <SelectContainer csx={csx} variant={variant}>
         <VisuallyHidden>
           <label htmlFor={id}>{label}</label>
         </VisuallyHidden>
-        <SelectInput id={id} ref={ref} variant="inline" {...selectProps}>
+        <SelectInput
+          id={id}
+          ref={ref}
+          variant={variant}
+          csx={styles.bleed({ bleedX, bleedY })}
+          {...selectProps}
+        >
           {children}
         </SelectInput>
-      </SelectInputContainer>
+      </SelectContainer>
     )
   }
 )
 
-type JSXSelectProps = ComponentPropsWithoutRef<'select'>
-
 export interface SelectInlineOptions {
   label: string
   csx?: StyleProp
+  bleedY: boolean
+  bleedX: boolean
 }
+
 export type SelectInlineProps = SelectInlineOptions &
   Omit<JSXSelectProps, keyof SelectInlineOptions>
