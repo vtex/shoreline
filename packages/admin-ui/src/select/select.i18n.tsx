2e1215b23d27cd79186cba17f70663f4da69aba1
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-05-25 17:36:38 -0300

    refactor(select): add design requested changes

---
+++ packages/admin-ui/src/select/select.i18n.tsx
@@ -0,0 +1,14 @@
+export const messages = {
+  select: {
+    'en-US': { placeholder: 'Select...' },
+    'es-AR': { placeholder: 'Select...' },
+    'fr-FR': { placeholder: 'Select...' },
+    'pt-BR': { placeholder: 'Selecione...' },
+    'ja-JP': { placeholder: 'Select...' },
+    'ko-KR': { placeholder: 'Select...' },
+    'it-IT': { placeholder: 'Select...' },
+    'nl-NL': { placeholder: 'Select...' },
+    'ro-RO': { placeholder: 'Select...' },
+    'bg-BG': { placeholder: 'Select...' },
+  },
+}
--- packages/admin-ui/src/select/select.styles.ts
+++ packages/admin-ui/src/select/select.styles.ts
@@ -1,6 +1,6 @@
-import { styles, styleVariants, focusVisible } from '@vtex/admin-ui-core'
+import { style, styleVariants, focusVisible } from '@vtex/admin-ui-core'
 
-export const baseline = styles({
+export const baseline = style({
   borderRadius: 'default',
   appearance: 'none',
   cursor: 'pointer',
@@ -8,22 +8,19 @@ export const baseline = styles({
   paddingRight: '2rem',
   bg: '$form.neutral',
   width: '100%',
+  text: '$body',
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
 })
 
 export const variants = styleVariants({
-  error: {
-    false: {},
-    true: {
-      ...focusVisible('critical'),
-      border: '$form.critical',
-    },
-  },
-  selected: {
-    true: { color: '$form.neutral', text: '$detail' },
-    false: { color: '$secondary', text: '$body' },
-  },
   variant: {
     default: {
+      boxSizing: 'border-box',
+      height: '2.75rem',
+      padding: '$m',
+      paddingRight: '2.5rem',
       border: '$form.neutral',
       ...focusVisible('neutral'),
 
@@ -32,32 +29,89 @@ export const variants = styleVariants({
         color: '$disabled',
         border: '$disabled',
         cursor: 'not-allowed',
+        '+ * > svg': {
+          color: '$disabled',
+        },
       },
     },
     inline: {
+      padding: '$s',
+      paddingRight: '2rem',
       ...focusVisible('neutral', {
         focus: { bg: '$action.neutral.tertiaryHover' },
       }),
 
-      ':hover': {
-        bg: '$action.neutral.tertiaryHover',
-      },
-
       ':disabled': {
-        bg: '$disabled',
         color: '$disabled',
         cursor: 'not-allowed',
+        '+ * > svg': {
+          color: '$disabled',
+        },
       },
+
+      ':hover:not(:disabled)': {
+        bg: '$action.neutral.tertiaryHover',
+      },
+    },
+  },
+
+  error: {
+    false: {},
+    true: {
+      ...focusVisible('critical'),
+      border: '$form.critical',
+    },
+  },
+
+  selected: {
+    false: { color: '$secondary' },
+    true: { color: '$form.neutral' },
+  },
+})
+
+export const caret = style({
+  position: 'absolute',
+  top: 0,
+  bottom: 0,
+  margin: 'auto',
+  pointerEvents: 'none',
+})
+
+export const caretVariants = styleVariants({
+  variant: {
+    default: {
+      right: '1rem',
+      height: '1.25rem',
+    },
+    inline: {
+      right: '0.75rem',
+      height: '1rem',
     },
   },
 })
 
-export const caret = styles({
-  height: '1.5rem',
-  position: 'absolute',
-  right: 16,
-  top: 0,
-  bottom: 0,
-  margin: 'auto',
-  color: '$secondary',
+export const container = styleVariants({
+  variant: {
+    default: {
+      position: 'relative',
+      width: '100%',
+    },
+    inline: {
+      position: 'relative',
+      width: 'fit-content',
+    },
+  },
+})
+
+export const bleed = styleVariants({
+  bleedY: {
+    true: {
+      marginY: '$-m',
+    },
+    false: {},
+  },
+  bleedX: {
+    true: { marginX: '$-m' },
+    false: {},
+  },
 })
