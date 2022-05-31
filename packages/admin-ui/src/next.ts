d6fcbfd1773897b3248eb023d6d6801b52959782
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-05-31 10:34:18 -0300

    refactor(select): export select as a next feature

--- packages/admin-ui/src/next.ts
+++ packages/admin-ui/src/next.ts
@@ -2,8 +2,12 @@ import type { TextInputProps } from './text-input'
 import { TextInput } from './text-input'
 import type { TextAreaProps } from './text-area'
 import { TextArea } from './text-area'
+import type { SelectProps } from './select'
+import { Select } from './select'
 
 export type { TextInputProps as next_TextInputProps }
 export { TextInput as next_TextInput }
 export type { TextAreaProps as next_TextAreaProps }
 export { TextArea as next_TextArea }
+export type { SelectProps as next_SelectProps }
+export { Select as next_Select }
