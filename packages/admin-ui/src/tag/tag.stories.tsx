89320292aa00543f67fb8a55b5da970b4ecfd541
marcelovicentegc <marcelovicentegc@gmail.com>
2022-06-01 12:35:48 -0300

    feat(tag): update tokens

--- packages/admin-ui/src/tag/tag.stories.tsx
+++ packages/admin-ui/src/tag/tag.stories.tsx
@@ -1,11 +1,15 @@
 /* eslint-disable no-console */
+import type { ReactNode } from 'react'
 import React from 'react'
 import type { Meta, Story } from '@storybook/react'
 
 import type { TagProps } from './tag'
 import { Tag } from './tag'
 import { Stack } from '../stack'
-import { Inline } from '../inline'
+import { Inline as AdminUIInline } from '../inline'
+import { Text } from '../components/Text'
+
+const LABEL = 'Short text'
 
 export default {
   title: 'admin-ui-review/tag',
@@ -23,23 +27,60 @@ export const Playground: Story<TagProps> = (args) => {
 }
 
 Playground.args = {
-  label: 'Rio de Janeiro',
+  label: 'Short text',
+}
+
+function Inline(props: { children: ReactNode }) {
+  return (
+    <AdminUIInline
+      csx={{
+        minWidth: '15rem',
+        justifyContent: 'space-between',
+      }}
+    >
+      {props.children}
+    </AdminUIInline>
+  )
 }
 
 export function Variants() {
   return (
     <Stack>
       <Inline>
-        <Tag label="Rio de Janeiro" />
-        <Tag label="Rio de Janeiro" palette="cyan" />
-        <Tag label="Rio de Janeiro" palette="purple" />
-        <Tag label="Rio de Janeiro" palette="green" />
+        <Text>gray</Text>
+        <Tag label={LABEL} palette="gray" />
       </Inline>
       <Inline>
-        <Tag label="Rio de Janeiro" palette="red" />
-        <Tag label="Rio de Janeiro" palette="orange" />
-        <Tag label="Rio de Janeiro" palette="teal" />
-        <Tag label="Rio de Janeiro" palette="gray" />
+        <Text>red (critical)</Text>
+        <Tag label={LABEL} palette="red" />
+      </Inline>
+      <Inline>
+        <Text>orange (warning)</Text>
+        <Tag label={LABEL} palette="orange" />
+      </Inline>
+      <Inline>
+        <Text>green (positive)</Text>
+        <Tag label={LABEL} palette="green" />
+      </Inline>
+      <Inline>
+        <Text>lightBlue (default, info)</Text>
+        <Tag label={LABEL} palette="lightBlue" />
+      </Inline>
+      <Inline>
+        <Text>cyan</Text>
+        <Tag label={LABEL} palette="cyan" />
+      </Inline>
+      <Inline>
+        <Text>purple</Text>
+        <Tag label={LABEL} palette="purple" />
+      </Inline>
+      <Inline>
+        <Text>teal</Text>
+        <Tag label={LABEL} palette="teal" />
+      </Inline>
+      <Inline>
+        <Text>pink</Text>
+        <Tag label={LABEL} palette="pink" />
       </Inline>
     </Stack>
   )
