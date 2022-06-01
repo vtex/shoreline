61ddbbdc7fd2ea0cc1076e14393d604f1b0019a3
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-06-01 17:15:32 -0300

    Fix stitches config to allow SSR

--- packages/admin-ui-core/src/styles.ts
+++ packages/admin-ui-core/src/styles.ts
@@ -1,7 +1,6 @@
-import type { CSS as CSSObject } from '@stitches/core'
-import { css } from '@stitches/core'
+import type { CSS as CSSObject } from '@stitches/react'
+import { css } from './stitches.config'
 import { isFunction } from '@vtex/admin-ui-util'
-
 import { alias } from './aliases'
 import { resolveRule } from './rules'
 import { canSplit, split } from './splits'
