1a7058019afd2c0dad201dbb072c5a58dafe2311
marcelovicentegc <marcelovicentegc@gmail.com>
2022-06-01 16:30:40 -0300

    feat(tag): codemod

---
+++ packages/admin-ui-codemod/src/tag-review.js
@@ -0,0 +1,22 @@
+function remove(source, j, componentName, props) {
+  return j(source)
+    .find(j.JSXOpeningElement, { name: { name: componentName } })
+    .forEach((p) => {
+      const { name, attributes, selfClosing } = p.value
+
+      const newAttributes = attributes.filter((attribute) => {
+        return !props.includes(attribute.name.name)
+      })
+
+      j(p).replaceWith(j.jsxOpeningElement(name, newAttributes, selfClosing))
+    })
+    .toSource()
+}
+
+module.exports = function (file, { jscodeshift: j }) {
+  let { source } = file
+
+  source = remove(source, j, 'Tag', ['size', 'handleDelete'])
+
+  return source
+}
