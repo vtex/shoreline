519cbe3f7b686980e9e91e9cb22967f47a60cfe6
matheusps <cout.matheusps@gmail.com>
2022-06-07 16:45:26 -0300

    feat(form): integrate switch

---
+++ packages/admin-ui-form/src/switch/stories/switch.stories.tsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import type { Meta } from '@storybook/react'
+import { Box, Button, Stack } from '@vtex/admin-ui'
+
+import { useFormState, Form } from '../../form'
+import { Switch } from '../index'
+
+export default {
+  title: 'admin-ui-form/switch',
+} as Meta
+
+export const Basic = () => {
+  const form = useFormState()
+
+  return (
+    <Box>
+      <Form onSubmit={(data) => console.log(data)} state={form}>
+        <Stack space="$l">
+          <Switch
+            state={form}
+            name="terms-of-service"
+            label="I agree to the Terms of Service"
+            helpText="You must select this option to use our services"
+          />
+          <Button csx={{ marginLeft: '$s' }} type="submit">
+            Submit
+          </Button>
+        </Stack>
+      </Form>
+    </Box>
+  )
+}
+
+export const DefaultValue = () => {
+  const form = useFormState({
+    defaultValues: {
+      enabled: true,
+    },
+  })
+
+  return (
+    <Box>
+      <Form onSubmit={(data) => console.log(data)} state={form}>
+        <Stack space="$l">
+          <Switch
+            state={form}
+            name="enabled"
+            label="Search enabled"
+            helpText="Enables the search feature on the system"
+          />
+          <Button csx={{ marginLeft: '$s' }} type="submit">
+            Submit
+          </Button>
+        </Stack>
+      </Form>
+    </Box>
+  )
+}
+
+export const Validation = () => {
+  const form = useFormState()
+
+  return (
+    <Box>
+      <Form onSubmit={(data) => console.log(data)} state={form}>
+        <Stack space="$l">
+          <Switch
+            state={form}
+            name="terms-of-service"
+            label="I agree to the Terms of Service"
+            helpText="You must select this option to use our services"
+            validation={{
+              required: 'This is required',
+            }}
+          />
+          <Button csx={{ marginLeft: '$s' }} type="submit">
+            Submit
+          </Button>
+        </Stack>
+      </Form>
+    </Box>
+  )
+}
