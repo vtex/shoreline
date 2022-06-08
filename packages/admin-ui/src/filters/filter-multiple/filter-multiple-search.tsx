d327fd338fa59bdd93978c7387f3c394c2da6d6f
anitavincent <paes.anita@gmail.com>
2022-06-08 16:21:09 -0300

    feat(filter): add filter multiple search component

---
+++ packages/admin-ui/src/filters/filter-multiple/filter-multiple-search.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import { BaseFilter } from '../filter-base'
+import { ComboboxItem } from '../../combobox/combobox-item'
+import { FilterSeachbox } from '../filter-searchbox'
+import { MultipleItemsLabel } from './filter-multiple-items-label'
+import { Box, Checkbox } from '../..'
+import { useMessageFormatter } from '../../i18n'
+import { messages } from '../filter.i18n'
+import type { FilterMultipleProps } from '..'
+
+import * as style from '../filter.style'
+
+export function FilterMultipleSearch<T>(props: FilterMultipleProps<T>) {
+  const formatMessage = useMessageFormatter(messages.searchBox)
+  const {
+    state: { appliedItems, combobox, baseId, getOptionId, getOptionLabel },
+    state,
+  } = props
+
+  const isEmpty = !combobox.matches.length
+
+  return (
+    <BaseFilter
+      state={state}
+      appliedValuesLabel={
+        <MultipleItemsLabel appliedItems={appliedItems} state={state} />
+      }
+    >
+      <FilterSeachbox state={combobox} id={`${baseId ?? ''}-search`} />
+      {isEmpty && <Box>{formatMessage('noResultsTitle')}</Box>}
+      {combobox.matches.map((item) => (
+        <ComboboxItem
+          aria-selected={combobox.isSelected(item)}
+          key={getOptionId(item)}
+          onClick={() => combobox.onChange(item)}
+          style={style.option}
+          id={`${baseId ?? ''}-item-${getOptionId(item)}`}
+        >
+          <Checkbox
+            checked={combobox.isSelected(item)}
+            aria-checked={undefined}
+            csx={{ marginRight: '$s' }}
+            label={getOptionLabel(item)}
+            readOnly
+          />
+        </ComboboxItem>
+      ))}
+    </BaseFilter>
+  )
+}
