efdbc80945f2e98b5b443e3b47909b0d263a0e02
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-06-13 17:59:53 -0300

    docs(tabs): separate doc tabs into mdx files to improve doc writing UX

---
+++ packages/admin-ui-docs/content/button/toc.js
@@ -0,0 +1,109 @@
+export const overview = [
+  {
+    id: 'what-is-this',
+    value: 'What is this?',
+    level: 2,
+  },
+  {
+    id: 'variants',
+    value: 'Variants',
+    level: 2,
+  },
+  {
+    id: 'how-does-each-variant-work',
+    value: 'How does each variant work?',
+    level: 3,
+  },
+  {
+    id: 'button-sizes',
+    value: 'Button sizes',
+    level: 2,
+  },
+  {
+    id: 'button-with-icon',
+    value: 'Button with icon',
+    level: 2,
+  },
+  {
+    id: 'disabled',
+    value: 'Disabled',
+    level: 2,
+  },
+  {
+    id: 'loading',
+    value: 'Loading',
+    level: 2,
+  },
+]
+
+export const codeReference = [
+  {
+    id: 'usage',
+    value: 'Usage',
+    level: 2,
+  },
+  {
+    id: 'props',
+    value: 'Props',
+    level: 2,
+  },
+  {
+    id: 'accessibility',
+    value: 'Accessibility',
+    level: 2,
+  },
+]
+
+export const designReference = [
+  {
+    id: 'best-practices',
+    value: `Best Practices`,
+    level: 2,
+  },
+  {
+    id: 'writing-labels',
+    value: 'Writing Labels',
+    level: 2,
+  },
+  {
+    id: 'combining',
+    value: 'Combining button styles',
+    level: 2,
+  },
+  {
+    id: 'recommendations',
+    value: 'Recommendations',
+    level: 2,
+  },
+  {
+    id: 'components',
+    value: 'Components',
+    level: 3,
+  },
+  {
+    id: 'variants',
+    value: 'Variants',
+    level: 3,
+  },
+  {
+    id: 'position',
+    value: 'Position',
+    level: 3,
+  },
+  {
+    id: 'behavior',
+    value: 'Behavior',
+    level: 3,
+  },
+  {
+    id: 'content',
+    value: 'Content',
+    level: 3,
+  },
+]
+
+export const tabToc = {
+  overview,
+  codeReference,
+  designReference,
+}
