efe8f81d79dcd33821b7eab662b691c4e5e2ace9
matheusps <cout.matheusps@gmail.com>
2022-06-20 18:43:37 -0300

    docs(text-input): add docs page

---
+++ packages/admin-ui-docs/content/text-input/toc.js
@@ -0,0 +1,9 @@
+import { toc as overview } from './overview.mdx'
+import { toc as designReference } from './design.mdx'
+import { toc as codeReference } from './code.mdx'
+
+export const tabToc = {
+  overview,
+  codeReference,
+  designReference,
+}
