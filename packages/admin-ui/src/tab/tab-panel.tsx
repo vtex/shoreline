85a033a22eddbc5177eeeeb3d874cbbf3153255d
matheusps <cout.matheusps@gmail.com>
2022-06-22 11:26:17 -0300

    feat(tab): review component

---
+++ packages/admin-ui/src/tab/tab-panel.tsx
@@ -0,0 +1,13 @@
+import { TabPanel as AriakitTabPanel } from 'ariakit/Tab'
+import { createComponent, useElement } from '@vtex/admin-ui-react'
+
+import * as style from './tabs.style'
+
+export const TabPanel = createComponent<typeof AriakitTabPanel>((props) => {
+  return useElement(AriakitTabPanel, {
+    ...props,
+    baseStyle: style.tabPanel,
+  })
+})
+
+export type TabPanelProps = React.ComponentPropsWithRef<typeof TabPanel>
