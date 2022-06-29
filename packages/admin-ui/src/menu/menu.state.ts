2193b921e28f08e6420220b0a231f5f033675228
Lenderson Morais <lenderson.morais@vtex.com.br>
2022-06-29 12:00:17 -0300

    refactor: improve menu state wrapper and its typing

--- packages/admin-ui/src/menu/menu.state.ts
+++ packages/admin-ui/src/menu/menu.state.ts
@@ -1,6 +1,10 @@
 import { useMenuState as useAriakitMenuState } from 'ariakit/menu'
 
-export const useMenuState = () =>
+export const useMenuState: MenuStateReturn = (props?) =>
   useAriakitMenuState({
     placement: 'bottom-end',
+    ...props,
   })
+
+export type MenuStateReturn = typeof useAriakitMenuState
+export type { MenuState } from 'ariakit/menu'
