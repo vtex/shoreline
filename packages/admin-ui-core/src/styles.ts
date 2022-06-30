997f96fad6663103cc8764bced20890240d0ceab
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-06-30 17:31:06 -0300

    fix(responsive-value): remove deprecated responsive value approach

--- packages/admin-ui-core/src/styles.ts
+++ packages/admin-ui-core/src/styles.ts
@@ -6,76 +6,20 @@ import { alias } from './aliases'
 import { resolveRule } from './rules'
 import { callUtil, isUtil } from './utils'
 import { createTransform } from './transforms'
-import type { StyleObject, StyleProp, ThemeDerivedStyles } from './types'
+import type { StyleObject, StyleProp } from './types'
 import { theme as defaultTheme } from './theme'
 
 const TOKEN_PREFIX = '$'
-
-/**
- * Available media queries mapped from the breakpoints
- */
-const mediaQueries = [
-  null,
-  ...defaultTheme.breakpoints.map((n) => `@media screen and (min-width: ${n})`),
-]
-
-/**
- * This function allows the responsive arrays
- * @example
- * csx({
- *  width: [20, 30, 40]
- * })
- */
-function resolveResponsiveArray(
-  styles: Exclude<StyleProp, ThemeDerivedStyles>
-): Exclude<StyleProp, ThemeDerivedStyles> {
-  const next: any = {}
-
-  for (const k in styles) {
-    const key = k as keyof typeof styles
-    const value = styles[key]
-
-    if (value === null) {
-      continue
-    }
-
-    if (!Array.isArray(value)) {
-      next[key] = value
-      continue
-    }
-
-    for (let i = 0; i < value.slice(0, mediaQueries.length).length; i++) {
-      const media = mediaQueries[i]
-
-      if (!media) {
-        next[key] = value[i]
-        continue
-      }
-
-      next[media] = next[media] ?? {}
-
-      if (value[i] == null) {
-        continue
-      }
-
-      next[media][key] = value[i]
-    }
-  }
-
-  return next
-}
 
 /**
  * Parses a style object
  */
 export function styles(csxObject: StyleProp = {}, theme: any = defaultTheme) {
   const cssObject: CSSObject = {}
-  const responsive = resolveResponsiveArray(csxObject as any)
 
-  for (const key in responsive) {
+  for (const key in csxObject) {
     const cssProperty = alias(key)
-    const mqValue = responsive[key as keyof typeof responsive]
-    const token = isFunction(mqValue) ? (mqValue as Function)(theme) : mqValue
+    const token = isFunction(key) ? (key as Function)(theme) : key
 
     if (token && typeof token === 'object') {
       cssObject[cssProperty] = styles(token as StyleObject, theme)
