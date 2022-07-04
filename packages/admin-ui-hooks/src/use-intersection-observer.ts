1501e5cc5222457241b0dd5910ddf1bf3dbec1af
marcelovicentegc <marcelovicentegc@gmail.com>
2022-07-03 23:57:01 -0300

    feat(page-header): use intersection observer api

---
+++ packages/admin-ui-hooks/src/use-intersection-observer.ts
@@ -0,0 +1,47 @@
+import { useEffect, useRef, useState } from 'react'
+
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+ */
+export function useIntersectionObserver({
+  // Defaults to the browser viewport
+  root = null,
+  rootMargin = undefined,
+  threshold = 0,
+  callback,
+}: IntersectionObserverInit & {
+  callback: (entry: IntersectionObserverEntry) => void
+}) {
+  const [node, setNode] = useState<HTMLDivElement>()
+  const observer = useRef<IntersectionObserver>()
+
+  useEffect(() => {
+    observer?.current && observer.current.disconnect()
+
+    observer.current = new IntersectionObserver(
+      ([entry]) => {
+        callback(entry)
+      },
+      {
+        root,
+        rootMargin,
+        threshold,
+      }
+    )
+
+    const { current: currentObserver } = observer
+
+    // We need to disconnect in case the component using
+    // this hook changes the node that the observer is
+    // observing, otherwise we'll polute the DOM with
+    // unused observables. This ensures that the observer
+    // is acting only over the node we care about.
+    currentObserver.disconnect()
+
+    node && currentObserver.observe(node)
+
+    return () => currentObserver.disconnect()
+  }, [node, root, rootMargin, threshold, callback])
+
+  return { setNode }
+}
