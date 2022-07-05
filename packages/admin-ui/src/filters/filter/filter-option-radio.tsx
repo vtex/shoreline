86a512d120f2fe7a2dc02e9ae0ad3b3b45e783e0
anitavincent <paes.anita@gmail.com>
2022-07-05 18:53:02 -0300

    feat(filters): implement suggested composable API

--- packages/admin-ui/src/filters/filter/filter-option-radio.tsx
+++ packages/admin-ui/src/filters/filter/filter-option-radio.tsx
@@ -1,43 +1,43 @@
 import React from 'react'
 
-import type { AnyObject } from 'packages/admin-ui/dist/declarations-util/src'
-
 import { ComboboxItem } from 'ariakit/combobox'
 
 import * as style from '../filter.style'
+
 import { useSystem } from '../..'
 import type { UseFilterStateReturn } from './filter.state'
 import { FilterRadio } from './filter-radio'
 
 export const FilterOptionRadio = (props: {
   state: UseFilterStateReturn<any>
-  id?: string
-  item: AnyObject
+  id: string
+  children: string
+  data?: any
 }) => {
   const {
     state: {
       combobox: { selectedItem, setSelectedItem },
-      getOptionId,
-      getOptionLabel,
     },
     id,
-    item,
+    children,
+    data,
   } = props
 
   const { cn } = useSystem()
 
-  const isSelected =
-    selectedItem && getOptionId(selectedItem) === getOptionId(item)
+  const isSelected = selectedItem?.id === id || false
+
+  const item = { id, label: children, data }
 
   return (
     <ComboboxItem
       aria-selected={isSelected}
       onClick={() => setSelectedItem(item)}
-      id={id || getOptionId(item)}
+      id={id}
       className={cn(style.option)}
     >
       <FilterRadio checked={isSelected} />
-      {getOptionLabel(item)}
+      {children}
     </ComboboxItem>
   )
 }
