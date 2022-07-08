e9ec72e753dbe17e2fb92656702df24f6bb35894
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-07-08 17:23:46 -0300

    fix(formik): remove deprecated admin-ui-formik library

--- packages/deprecated-formik/src/components/DeprecatedBase/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './Field'
-export * from './Input'
-export * from './InputPassword'
-export * from './Label'
-export * from './NumericStepper'
-export * from './Select'
-export * from './TextArea'
--- packages/deprecated-formik/src/components/FormikRadio/index.tsx
+++
@@ -1,2 +0,0 @@
-export * from './FormikRadio'
-export * from './FormikRadioGroup'
