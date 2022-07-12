997524d7bff3d2fac2feeb9b400b6858730f894b
anitavincent <paes.anita@gmail.com>
2022-07-12 15:20:20 -0300

    feat(filters): enable csx on all composable filter components

--- packages/admin-ui/src/filters/filter-listbox.tsx
+++ packages/admin-ui/src/filters/filter-listbox.tsx
@@ -1,43 +1,51 @@
+import type { HTMLProps } from 'react'
 import React, { useEffect, useRef } from 'react'
 
 import { ComboboxList } from 'ariakit/combobox'
 
-import { Box, tag } from '..'
+import { Box, createComponent, useElement } from '..'
 
 import * as style from './filter.style'
 import { usePopoverContext } from './filter-popover-context'
+import type { Role } from 'ariakit'
 
-export const FilterListbox = (props: FilterListboxProps) => {
-  const { children, id } = props
-  const optionsContainerRef = useRef<HTMLDivElement>(null)
+export const FilterListbox = createComponent<typeof Role, FilterListboxProps>(
+  (props) => {
+    const { children, id, ...restProps } = props
+    const optionsContainerRef = useRef<HTMLDivElement>(null)
 
-  const {
-    setIsScrollableLayout,
-    state: { combobox },
-  } = usePopoverContext()
+    const {
+      setIsScrollableLayout,
+      state: { combobox },
+    } = usePopoverContext()
 
-  const scrollHeight = optionsContainerRef?.current?.scrollHeight ?? 0
-  const containerHeight = optionsContainerRef?.current?.clientHeight ?? 0
+    const scrollHeight = optionsContainerRef?.current?.scrollHeight ?? 0
+    const containerHeight = optionsContainerRef?.current?.clientHeight ?? 0
 
-  useEffect(() => {
-    setIsScrollableLayout(scrollHeight > containerHeight)
-  }, [scrollHeight, containerHeight])
+    useEffect(() => {
+      setIsScrollableLayout(scrollHeight > containerHeight)
+    }, [scrollHeight, containerHeight])
 
-  const ariakitcomboboxState = { ...combobox, matches: [] }
+    const ariakitcomboboxState = { ...combobox, matches: [] }
 
-  return (
-    <tag.span ref={optionsContainerRef} csx={style.scrollableContainer}>
-      <Box
-        as={ComboboxList as any}
-        state={{ ...ariakitcomboboxState, visible: true }}
-        id={id}
-        csx={style.list}
-      >
-        {children}
-      </Box>
-    </tag.span>
-  )
-}
+    return useElement('span', {
+      baseStyle: style.scrollableContainer,
+      children: (
+        <Box
+          as={ComboboxList as any}
+          state={{ ...ariakitcomboboxState, visible: true }}
+          id={id}
+          csx={style.list}
+        >
+          {children}
+        </Box>
+      ),
+      ref: optionsContainerRef as any,
+      id,
+      ...(restProps as Omit<HTMLProps<'span'>, never>),
+    })
+  }
+)
 
 interface FilterListboxProps {
   id?: string
