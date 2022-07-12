8bdc9d256dfba86b846cf04c088fdddbabf8d770
anitavincent <paes.anita@gmail.com>
2022-07-12 10:11:26 -0300

    refactor(filters): code improvements
    improve usage of admin ui internal features on filter component

--- packages/admin-ui/src/filters/filter-footer.tsx
+++ packages/admin-ui/src/filters/filter-footer.tsx
@@ -5,6 +5,7 @@ import { Button } from '../button'
 import { useMessageFormatter } from '../i18n'
 import { messages } from './filter.i18n'
 import { usePopoverContext } from './filter-popover-context'
+import { Stack } from '../stack'
 
 export const FilterFooter = createComponent<typeof Role>(() => {
   const { state } = usePopoverContext()
@@ -21,17 +22,14 @@ export const FilterFooter = createComponent<typeof Role>(() => {
       paddingTop: isScrollableLayout ? undefined : 0,
       display: 'flex',
       justifyContent: 'end',
-      'button:not(:first-child)': {
-        marginLeft: '$m',
-      },
     },
     children: (
-      <>
+      <Stack direction="row" space="$m">
         <Button variant="tertiary" onClick={onClear}>
           {formatMessage('clear')}
         </Button>
         <Button onClick={onChange}>{formatMessage('apply')}</Button>
-      </>
+      </Stack>
     ),
   })
 })
