933a7da55cc8443a78261edcd275caf66236e530
marcelovicentegc <marcelovicentegc@gmail.com>
2022-07-15 16:43:26 -0300

    test(page): add more tests

--- packages/admin-ui/src/page/page-header.test.tsx
+++ packages/admin-ui/src/page/page-header.test.tsx
@@ -1,14 +1,51 @@
+import type { ReactNode } from 'react'
 import React from 'react'
 import { render, axe } from '../test-utils'
-import { PageHeader, PageHeaderTop, PageHeaderTitle } from './index'
+import {
+  PageHeader,
+  PageHeaderTop,
+  PageHeaderTitle,
+  PageHeaderTags,
+  PageHeaderTag,
+  PageHeaderActions,
+  PageHeaderButton,
+} from './index'
 
 import 'intersection-observer'
 
-function Basic() {
+interface Props {
+  tags?: ReactNode
+  actions?: ReactNode
+  onPopNavigation?: boolean
+}
+
+const tags = (
+  <PageHeaderTags>
+    <PageHeaderTag label="Short text" />
+  </PageHeaderTags>
+)
+
+const actions = (
+  <PageHeaderActions>
+    <PageHeaderButton variant="secondary">Edit</PageHeaderButton>
+    <PageHeaderButton>Create</PageHeaderButton>
+  </PageHeaderActions>
+)
+
+function Basic(props: Props) {
+  const {
+    tags = null,
+    actions = null,
+    onPopNavigation: onPopNavProp = false,
+  } = props
+
+  const onPopNavigation = onPopNavProp ? () => null : undefined
+
   return (
-    <PageHeader>
+    <PageHeader onPopNavigation={onPopNavigation}>
       <PageHeaderTop>
-        <PageHeaderTitle>Product #123</PageHeaderTitle>
+        <PageHeaderTitle>Product #123 {tags}</PageHeaderTitle>
+        {actions}
       </PageHeaderTop>
     </PageHeader>
   )
@@ -16,11 +53,21 @@ function Basic() {
 
 describe('page-header', () => {
   it('should not have a11y violations', async () => {
-    const { container } = render(<Basic />)
+    const { container } = render(
+      <Basic tags={tags} actions={actions} onPopNavigation />
+    )
 
     const results = await axe(container)
 
     expect(results).toHaveNoViolations()
+  })
+
+  it('page-header onPopNavigation should be visible', () => {
+    const { queryByRole } = render(<Basic onPopNavigation />)
+
+    const results = queryByRole('button')
+
+    expect(results).toBeTruthy()
   })
 
   it('page-header title should be visible', async () => {
@@ -29,5 +76,27 @@ describe('page-header', () => {
     const results = await findByText('Product #123')
 
     expect(results).toBeVisible()
+  })
+
+  it("page-header's tag should be visible", async () => {
+    const { findByText } = render(<Basic tags={tags} />)
+
+    const results = await findByText('Short text')
+
+    expect(results).toBeVisible()
+  })
+
+  it("page-header's actions should be visible", async () => {
+    const { queryAllByRole } = render(<Basic actions={actions} />)
+
+    const results = queryAllByRole('button')
+
+    expect(results).toHaveLength(2)
+
+    const expects = ['Edit', 'Create']
+
+    results.forEach((result, index) =>
+      expect(result).toHaveTextContent(expects[index])
+    )
   })
 })
