b2bab40fa57072343f32559394aa7967e464e27a
Lenderson Morais <lenderson.morais@vtex.com.br>
2022-07-19 09:51:20 -0300

    feat(table): enable columns to be fixed on horizontal scroll

--- packages/admin-ui/src/table/components/table-cell.tsx
+++ packages/admin-ui/src/table/components/table-cell.tsx
@@ -2,13 +2,24 @@ import { createComponent, useElement } from '@vtex/admin-ui-react'
 import type { VariantProps } from '@vtex/admin-ui-core'
 import * as styles from '../styles/table-cell.styles'
 import type { TableColumn } from '../types'
+import { useRef, useEffect } from 'react'
 
 export const TableCell = createComponent<'div', CellOptions>((props) => {
-  const { column, onClick, role = 'cell', ...cellProps } = props
+  const { column, onClick, role = 'cell', ref, ...cellProps } = props
 
   const clickable = !!onClick
 
+  const cellRef = useRef<'div'>(null)
+
+  useEffect(() => {
+    if (cellRef.current && column.position) {
+      cellRef.current.style.position = column.position
+      cellRef.current.style.left = `${cellRef.current.offsetLeft}px`
+    }
+  }, [])
+
   return useElement('div', {
+    ref: cellRef,
     baseStyle: {
       ...styles.baseline,
       ...styles.variants({ clickable }),
@@ -22,3 +33,5 @@ export const TableCell = createComponent<'div', CellOptions>((props) => {
 export interface CellOptions extends VariantProps<typeof styles.variants> {
   column?: TableColumn<any>
 }
+
+export type CellProps = React.ComponentPropsWithRef<typeof TableCell>
