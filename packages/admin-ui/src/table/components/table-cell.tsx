ff0141ff6cee7b459962cdbdfdc9e7c3d1d70b32
Lenderson Morais <lenderson.morais@vtex.com.br>
2022-07-20 09:38:51 -0300

    docs(table): add table scroll stories

--- packages/admin-ui/src/table/components/table-cell.tsx
+++ packages/admin-ui/src/table/components/table-cell.tsx
@@ -1,47 +1,58 @@
 import { createComponent, useElement } from '@vtex/admin-ui-react'
 import type { VariantProps } from '@vtex/admin-ui-core'
+import { cx } from '@vtex/admin-ui-core'
 import * as styles from '../styles/table-cell.styles'
 import type { TableColumn } from '../types'
 import React, { useRef, useEffect } from 'react'
 import { useStateContext } from '../context'
 import { Box } from '../../box'
+import { useForkRef } from '@vtex/admin-ui-hooks'
 
 export const TableCell = createComponent<'div', CellOptions>((props) => {
-  const { column, onClick, role = 'cell', ref, children, ...cellProps } = props
+  const {
+    column,
+    onClick,
+    role = 'cell',
+    ref: htmlRef,
+    children,
+    className = '',
+    ...cellProps
+  } = props
+
   const { columns } = useStateContext()
 
   const clickable = !!onClick
-  const isFixed = !!column?.fixed
 
-  const belongsToLastFixedColumn =
-    isFixed &&
-    columns.filter((currentColumn) => !!currentColumn?.fixed).slice(-1)[0] ===
-      column
+  const isFixed = Boolean(column?.fixed)
 
-  const cellRef = useRef<HTMLDivElement>(null)
+  const [lastFixedColumn] = columns.filter((col) => !!col?.fixed).slice(-1)
+
+  const isLastFixedColumn = isFixed && lastFixedColumn?.id === column?.id
+
+  const ref = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    if (cellRef.current && isFixed) {
-      cellRef.current.style.left = `${cellRef.current.offsetLeft}px`
+    if (ref.current && isFixed) {
+      ref.current.style.left = `${ref.current.offsetLeft}px`
     }
   }, [])
 
   return useElement('div', {
-    ref: cellRef as any,
-    className: isFixed ? 'fixed-cell' : '',
+    ref: useForkRef(ref, htmlRef as any),
+    className: isFixed ? cx('__admin-ui-fixed-cell', className) : className,
     baseStyle: {
       ...styles.baseline,
       ...styles.variants({
         clickable,
         fixed: isFixed,
-        lastFixed: belongsToLastFixedColumn,
+        lastFixed: isLastFixedColumn,
       }),
     },
     role,
     onClick,
     ...cellProps,
     children: isFixed ? (
-      <Box csx={{ ...styles.fixedInnerContainer }}>{children}</Box>
+      <Box csx={styles.fixedInnerContainer}>{children}</Box>
     ) : (
       children
     ),
