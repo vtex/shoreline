4e6a7f41de99eac9e1eb5c52968480a9be4c53f9
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-07-20 11:37:42 -0300

    feat(table): add menu resolver

---
+++ packages/admin-ui/src/table/stories/menu.stories.tsx
@@ -0,0 +1,101 @@
+import React, { useState } from 'react'
+import type { Meta } from '@storybook/react'
+import faker from 'faker'
+
+import {
+  DataView,
+  DataViewControls,
+  useDataViewState,
+} from '../../components/DataView'
+import { Table, useTableState } from '../index'
+import { Button } from '../../button'
+import { createColumns } from '../create-columns'
+import { IconTrash, IconPencil } from '@vtex/phosphor-icons'
+
+export default {
+  title: 'admin-ui-review/table',
+  component: Table,
+} as Meta
+
+interface Item {
+  id: string
+  name: string
+  lastSale: string
+  price: string
+  brand: string
+}
+
+const initialItems: Item[] = [...Array(20).keys()].map((id) => {
+  return {
+    id: `${id}`,
+    name: faker.commerce.productName(),
+    lastSale: faker.date.past().toDateString(),
+    price: faker.commerce.price(),
+    brand: faker.random.arrayElement(['mistery_id', 'cool_id']),
+  }
+})
+
+export function WithMenu() {
+  const [items, setItems] = useState(initialItems)
+  const view = useDataViewState()
+
+  const columns = createColumns<Item>([
+    {
+      id: 'name',
+      header: 'Product Name',
+    },
+    {
+      id: 'lastSale',
+      header: 'Last Sale',
+    },
+    {
+      id: 'price',
+      header: 'Price',
+      resolver: {
+        type: 'currency',
+        locale: 'en-US',
+        currency: 'USD',
+      },
+    },
+    {
+      id: 'menu',
+      resolver: {
+        type: 'menu',
+        actions: [
+          {
+            label: 'Edit',
+            icon: <IconPencil />,
+            onClick: (item) => {
+              console.log(item)
+            },
+          },
+          {
+            label: 'Delete',
+            icon: <IconTrash />,
+            onClick: (item) => {
+              setItems(items.filter((i) => i.id !== item.id))
+            },
+          },
+        ],
+      },
+    },
+  ])
+
+  const grid = useTableState<Item>({
+    view,
+    columns,
+    items,
+  })
+
+  return (
+    <DataView csx={{ width: 500 }} state={view}>
+      <DataViewControls>
+        <Button onClick={() => view.setStatus({ type: 'ready' })}>Ready</Button>
+        <Button onClick={() => view.setStatus({ type: 'loading' })}>
+          Loading
+        </Button>
+      </DataViewControls>
+      <Table state={grid} />
+    </DataView>
+  )
+}
