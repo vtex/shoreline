6d448d763ed97ea05c9ee3988ddd80f81f26bbe8
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-07-20 17:49:31 -0300

    docs(card): add design tab

--- packages/admin-ui-docs/content/card/toc.js
+++ packages/admin-ui-docs/content/card/toc.js
@@ -1,7 +1,9 @@
 import { toc as codeReference } from './code.mdx'
 import { toc as overview } from './overview.mdx'
+import { toc as designReference } from './design.mdx'
 
 export const tabToc = {
   overview,
   codeReference,
+  designReference,
 }
