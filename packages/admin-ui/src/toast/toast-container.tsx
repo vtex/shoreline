b692fd144733019f09e5499675ab7a2cda8e2ebe
matheusps <cout.matheusps@gmail.com>
2022-07-27 19:17:48 -0300

    feat(toast): review design specs

--- packages/admin-ui/src/toast/toast-container.tsx
+++ packages/admin-ui/src/toast/toast-container.tsx
@@ -1,54 +1,22 @@
 import type { VariantProps } from '@vtex/admin-ui-core'
-import { styleVariants } from '@vtex/admin-ui-core'
 import { createComponent, useElement } from '@vtex/admin-ui-react'
+
+import * as style from './toast.style'
 
 export const ToastContainer = createComponent<'div', ToastContainerOptions>(
   (props) => {
-    const { tone = 'info', ...restProps } = props
+    const { variant = 'info', ...restProps } = props
 
     return useElement('div', {
       ...restProps,
       baseStyle: {
-        position: 'relative',
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        minWidth: '16.125rem',
-        width: 'auto',
-        minHeight: '4.5rem',
-        height: 'auto',
-        maxHeight: '4.5rem',
-        padding: '1rem',
-        boxShadow: 'subtle',
-        border: 'default',
-        pointerEvents: 'all',
-        overflow: 'hidden',
-        paddingY: '4',
-        color: '$primary',
-        borderRadius: 'default',
-        ...variants({ tone }),
+        ...style.toastContainer,
+        ...style.toastContainerVariants({ variant }),
       },
     })
   }
 )
 
-const variants = styleVariants({
-  tone: {
-    critical: {
-      bg: '$critical',
-      border: '$critical',
-    },
-    positive: {
-      bg: '$positive',
-      border: '$positive',
-    },
-    warning: {
-      bg: '$warning',
-      border: '$warning',
-    },
-    info: { bg: '$info', border: '$info' },
-  },
-})
-
-export type ToastContainerOptions = VariantProps<typeof variants>
+export type ToastContainerOptions = VariantProps<
+  typeof style.toastContainerVariants
+>
