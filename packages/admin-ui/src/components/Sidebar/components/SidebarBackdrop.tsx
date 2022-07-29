76b89286cd59dd0dbb2d7156196590d48cf66f6a
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-07-29 13:51:06 -0300

    fix(sidebar): change sidebar item skeleton render condition

--- packages/admin-ui/src/components/Sidebar/components/SidebarBackdrop.tsx
+++ packages/admin-ui/src/components/Sidebar/components/SidebarBackdrop.tsx
@@ -34,6 +34,10 @@ export const SidebarBackdrop = forwardRef(function SidebarBackdrop(
     loading = false,
   } = props
 
+  const showSidebarItemSkeleton = selectedItem
+    ? loading && selectedItem.expandable
+    : loading
+
   return (
     <Fragment>
       <tag.div
@@ -50,7 +54,7 @@ export const SidebarBackdrop = forwardRef(function SidebarBackdrop(
           borderRight: selectedItem?.expandable ? '$neutral' : 'none',
         }}
       >
-        {loading && <SidebarItemSkeleton />}
+        {showSidebarItemSkeleton && <SidebarItemSkeleton />}
       </tag.div>
 
       {loading ? null : (
