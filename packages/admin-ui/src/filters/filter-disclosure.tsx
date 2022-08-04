6f1ff84cf47adc5debc4257426051e8dffbb55bf
anitavincent <paes.anita@gmail.com>
2022-08-04 18:01:20 -0300

    feat(hidden filters): auto open popover on show

--- packages/admin-ui/src/filters/filter-disclosure.tsx
+++ packages/admin-ui/src/filters/filter-disclosure.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from 'react'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { IconCaretUp } from '@vtex/phosphor-icons'
 import { MenuButton } from 'ariakit/menu'
@@ -9,6 +9,7 @@ import { AppliedItemsLabel } from './filter-applied-items-label'
 import type { UseFilterMultipleReturn } from './filter-multiple/filter-multiple.state'
 import type { UseFilterStateReturn } from './filter/filter.state'
 import { createComponent, useElement } from '@vtex/admin-ui-react'
+import { useFilterOptionalContext } from './filter-toggler/filter-optional-context'
 
 const asMulti = (state: any) => state as UseFilterMultipleReturn<any>
 
@@ -25,11 +26,16 @@ export const FilterDisclosure = createComponent<
   FilterDisclosureProps
 >((props: FilterDisclosureProps) => {
   const { state, children, id, ...restProps } = props
+  const { setMenuState } = useFilterOptionalContext()
 
   const { menu } = state
 
   const appliedList =
     asMulti(state)?.appliedItems ?? convertAppliedToArray(state)
+
+  useEffect(() => {
+    setMenuState?.(state.menu)
+  }, [])
 
   return useElement(MenuButton, {
     baseStyle: style.disclosure,
