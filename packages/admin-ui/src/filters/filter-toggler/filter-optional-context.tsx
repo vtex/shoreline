fca60d3d51135187681d1bc1f350d76233a72a7d
anitavincent <paes.anita@gmail.com>
2022-08-04 18:01:20 -0300

    feat(hidden filters): auto open popover on show

---
+++ packages/admin-ui/src/filters/filter-toggler/filter-optional-context.tsx
@@ -0,0 +1,16 @@
+import type { MenuState } from 'ariakit'
+import React from 'react'
+
+const FilterOptionalContext = React.createContext<{
+  setMenuState?: (menu: MenuState<any>) => void
+} | null>(null)
+
+export function useFilterOptionalContext() {
+  const context = React.useContext(FilterOptionalContext)
+
+  return context || {}
+}
+
+const { Provider } = FilterOptionalContext
+
+export { Provider as FilterOptionalProvider }
--- packages/admin-ui/src/filters/filter-toggler/filter-optional.tsx
+++ packages/admin-ui/src/filters/filter-toggler/filter-optional.tsx
@@ -1,11 +1,34 @@
+import type { MenuState } from 'ariakit'
+import React, { useEffect, useState } from 'react'
+import { FilterOptionalProvider } from './filter-optional-context'
 import type { FilterVisibilityStateReturn } from './filter-visibility-state'
 
 export const FilterOptional = (props: FilterOptionalProps) => {
   const { state, id, children } = props
 
-  const isVisible = state.visible.find((i) => i.id === id)
+  const [childMenuState, setChildMenuState] = useState<MenuState<any> | null>()
 
-  return isVisible && children ? (children as React.ReactElement) : null
+  const isVisible = state.visible.find((i) => i.id === id)
+  const childMounted = !!childMenuState
+
+  useEffect(() => {
+    // clearing child state when unmounting
+    if (!isVisible && childMenuState) {
+      setChildMenuState(null)
+    }
+  }, [isVisible])
+
+  useEffect(() => {
+    if (isVisible && childMenuState && state.shouldOpenOnMount(id)) {
+      childMenuState.show()
+    }
+  }, [isVisible, childMounted])
+
+  return isVisible && children ? (
+    <FilterOptionalProvider value={{ setMenuState: setChildMenuState }}>
+      {children}
+    </FilterOptionalProvider>
+  ) : null
 }
 
 interface FilterOptionalProps {
