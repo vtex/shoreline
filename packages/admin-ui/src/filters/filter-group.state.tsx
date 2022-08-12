10fece77fea3f671ff5a06a359153d0a993cc897
anitavincent <paes.anita@gmail.com>
2022-08-12 14:32:15 -0300

    style(filter): improve code readability

--- packages/admin-ui/src/filters/filter-group.state.tsx
+++ packages/admin-ui/src/filters/filter-group.state.tsx
@@ -1,11 +1,11 @@
 import type { GenericFilterStateReturn } from './filter/filter.state'
 
 export function useFilterGroupState(props: UseFilterGroupStateReturn) {
-  const { filterStates, onClear: onClearCb = () => {} } = props
+  const { filterStates, onClear: onClearCb } = props
 
   const onClear = () => {
     filterStates.forEach((state) => state.onClear())
-    onClearCb()
+    onClearCb?.()
   }
 
   return { onClear }
