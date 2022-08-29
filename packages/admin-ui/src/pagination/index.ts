53756d76ff4f4cffc7dd1930b98b108e70d047c0
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-08-29 18:30:15 -0300

    feat(pagination): add Pagination component review

---
+++ packages/admin-ui/src/pagination/index.ts
@@ -0,0 +1,3 @@
+export * from './pagination'
+export * from './hooks/use-pagination-state'
+export * from './hooks/use-query-pagination-state'
---
+++ packages/admin-ui/src/pagination/pagination.i18n.ts
@@ -0,0 +1,52 @@
+export const messages = {
+  'en-US': {
+    preposition: 'of',
+    prevLabel: 'Return to previous page',
+    nextLabel: 'Advance to next page',
+  },
+  'es-AR': {
+    preposition: 'of',
+    prevLabel: 'Return to previous page',
+    nextLabel: 'Advance to next page',
+  },
+  'fr-FR': {
+    preposition: 'of',
+    prevLabel: 'Return to previous page',
+    nextLabel: 'Advance to next page',
+  },
+  'pt-BR': {
+    preposition: 'de',
+    prevLabel: 'Retorne para página anterior',
+    nextLabel: 'Avance para página seguinte',
+  },
+  'ja-JP': {
+    preposition: 'of',
+    prevLabel: 'Return to previous page',
+    nextLabel: 'Advance to next page',
+  },
+  'ko-KR': {
+    preposition: 'of',
+    prevLabel: 'Return to previous page',
+    nextLabel: 'Advance to next page',
+  },
+  'it-IT': {
+    preposition: 'of',
+    prevLabel: 'Return to previous page',
+    nextLabel: 'Advance to next page',
+  },
+  'nl-NL': {
+    preposition: 'of',
+    prevLabel: 'Return to previous page',
+    nextLabel: 'Advance to next page',
+  },
+  'ro-RO': {
+    preposition: 'of',
+    prevLabel: 'Return to previous page',
+    nextLabel: 'Advance to next page',
+  },
+  'bg-BG': {
+    preposition: 'of',
+    prevLabel: 'Return to previous page',
+    nextLabel: 'Advance to next page',
+  },
+}
