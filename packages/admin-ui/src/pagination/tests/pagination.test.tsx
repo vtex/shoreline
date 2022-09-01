47579f0584fce11c4658fa972cce5fbd1207f544
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-09-01 11:06:14 -0300

    fix(pagination): i18n wrong message

--- packages/admin-ui/src/pagination/tests/pagination.test.tsx
+++ packages/admin-ui/src/pagination/tests/pagination.test.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-import { Pagination, useQueryPaginationState } from '../index'
-import { jestMatchMedia, render } from '../../test-utils'
+import {
+  Pagination,
+  usePaginationState,
+  useQueryPaginationState,
+} from '../index'
+import { jestMatchMedia, withState, render } from '../../test-utils'
 import { screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { useQueryStateContext, QueryStateProvider } from '@vtex/admin-ui-hooks'
@@ -22,6 +26,14 @@ const setQuery = (query: Record<string, any> = {}): boolean => {
   return true
 }
 
+const PaginationWithInitialValue = withState(Pagination, () =>
+  usePaginationState({
+    pageSize: 5,
+    total: 50,
+    initialPage: 2,
+  })
+)
+
 const PersistedPaginationWithInitialValue = () => {
   setQuery({ page: 3 })
 
@@ -33,17 +45,26 @@ const PersistedPaginationWithInitialValue = () => {
   return <Pagination state={state} data-testid="pagination" />
 }
 
-describe('Pagination with query string', () => {
+describe('Pagination', () => {
   beforeEach(jestMatchMedia)
+
+  it('should starts in a specific page', () => {
+    const { getByTestId } = render(
+      <PaginationWithInitialValue data-testid="pagination" />
+    )
+
+    expect(getByTestId('pagination')).toHaveTextContent('6 — 10 of 50')
+  })
 
   // window.history.back() not work (test pass because there is not await before waitFor)
   it('should starts in a specific page and persisted state', async () => {
-    const { rerender } = render(
+    const { getByTestId, rerender } = render(
       <QueryStateProvider>
         <PersistedPaginationWithInitialValue />
       </QueryStateProvider>
     )
 
+    expect(getByTestId('pagination')).toHaveTextContent('11 — 15 of 50')
     expect(window.location.href).toContain(`page=3`)
 
     const nextButton = screen
@@ -57,6 +78,9 @@ describe('Pagination with query string', () => {
       </QueryStateProvider>
     )
 
+    await waitFor(() =>
+      expect(getByTestId('pagination')).toHaveTextContent('16 — 20 of 50')
+    )
     waitFor(() => expect(window.location.href).toContain('?page=4'))
   })
 })
