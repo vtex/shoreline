fde02a39b1d62041e9fae045c2d597266d497660
Lenderson Morais <lenderson.morais@vtex.com.br>
2022-09-02 12:47:22 -0300

    build: create custom rollup config for each package based on a default config

--- packages/admin-ui-form/rollup.config.js
+++ packages/admin-ui-form/rollup.config.js
@@ -1,43 +1,11 @@
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import typescript from '@rollup/plugin-typescript'
-import external from 'rollup-plugin-peer-deps-external'
-import { terser } from 'rollup-plugin-terser'
-import dts from 'rollup-plugin-dts'
+import rollupConfig from '../../rollup.config'
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
-const packageJson = require('./package.json')
+const [mainConfig, typesConfig] = rollupConfig
 
 export default [
   {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: 'esm',
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      external(),
-      resolve(),
-      commonjs(),
-      typescript({
-        tsconfig: './tsconfig.json',
-        exclude: ['**/*.test.*', '**/*.stories.*'],
-      }),
-      terser(),
-    ],
-    external: ['react', 'react-dom', '@vtex/admin-ui'],
+    ...mainConfig,
+    external: [...mainConfig.external, '@vtex/admin-ui'],
   },
-  {
-    input: 'src/index.ts',
-    output: [{ file: packageJson.types, format: 'esm' }],
-    plugins: [dts()],
-  },
+  typesConfig,
 ]
---
+++ rollup.config.js
@@ -0,0 +1,50 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import external from 'rollup-plugin-peer-deps-external'
+import { terser } from 'rollup-plugin-terser'
+import babel from '@rollup/plugin-babel'
+import dts from 'rollup-plugin-dts'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
+const packageJson = require('./package.json')
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled',
+        compact: true,
+        configFile: '../../babel.config.js',
+      }),
+      typescript({
+        tsconfig: './tsconfig.json',
+        exclude: ['**/*.test.*', '**/*.stories.*'],
+      }),
+      terser(),
+    ],
+    external: ['react', 'react-dom'],
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: packageJson.types, format: 'esm' }],
+    plugins: [dts()],
+  },
+]
