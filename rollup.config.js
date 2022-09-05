f186706ab5a0eac42e49540c3006adcb3557b303
Lenderson Morais <lenderson.morais@vtex.com.br>
2022-09-05 18:05:11 -0300

    build: fix @rollup/plugin-typescript version to use declarationDir option (tsconfig)

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import external from 'rollup-plugin-peer-deps-external'
-// import babel from '@rollup/plugin-babel'
+import babel from '@rollup/plugin-babel'
 import dts from 'rollup-plugin-dts'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
@@ -27,22 +27,21 @@ export default [
       external(),
       resolve(),
       commonjs(),
-      // babel({
-      //   exclude: 'node_modules/**',
-      //   babelHelpers: 'bundled',
-      //   compact: true,
-      //   configFile: '../../babel.config.js',
-      // }),
+      babel({
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled',
+        compact: true,
+        configFile: '../../babel.config.js',
+      }),
       typescript({
         tsconfig: './tsconfig.json',
-        exclude: ['**/*.test.*', '**/*.stories.*'],
       }),
     ],
     external: ['react', 'react-dom'],
   },
   {
-    input: 'dist/index.d.ts',
-    output: [{ file: packageJson.types }],
+    input: 'src/index.ts',
+    output: [{ file: packageJson.types, format: 'esm' }],
     plugins: [dts()],
   },
 ]
