6fbe3597e24a64c7fc2aafc7d19640ed83447bee
Lenderson Morais <lenderson.morais@vtex.com.br>
2022-09-06 18:42:36 -0300

    build: fix inner imports of ariakit and reakit

--- packages/admin-ui-react/rollup.config.js
+++ packages/admin-ui-react/rollup.config.js
@@ -1,15 +1,3 @@
 import rollupConfig from '../../rollup.config'
 
-const [mainConfig, typesConfig] = rollupConfig
-
-export default [
-  {
-    ...mainConfig,
-    external: [
-      ...mainConfig.external,
-      '@vtex/admin-ui-core',
-      '@vtex/phosphor-icons',
-    ],
-  },
-  typesConfig,
-]
+export default rollupConfig
