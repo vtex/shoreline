792f16de4ecf3f51410a03234f6f3a7f336749c7
anitavincent <paes.anita@gmail.com>
2023-01-27 15:09:51 -0300

    style(divider style): remove comments

--- packages/admin-ui/src/components/Divider/divider.css.ts
+++ packages/admin-ui/src/components/Divider/divider.css.ts
@@ -1,23 +1,6 @@
 import { csx } from '@vtex/admin-ui-core'
 import { variant } from '../../modal/util'
 
-// export const variants = styleVariants({
-//   orientation: {
-//     horizontal: {
-//       borderBottom: 0,
-//     },
-//     vertical: {
-//       borderLeft: 0,
-//       height: 'auto',
-//     },
-//   },
-// })
-
-// export const baseline = style({
-//   border: '$neutral',
-//   margin: '$space-0',
-// })
-
 export const dividerTheme = csx({
   border: '$neutral',
   margin: '$space-0',
