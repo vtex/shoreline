92059c6c3755520ae9a425047696d6b35e1ce06a
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2023-03-07 10:57:07 -0300

    feat(checkbox): migrate to the new component model

--- packages/admin-ui/src/checkbox/checkbox.tsx
+++ packages/admin-ui/src/checkbox/checkbox.tsx
@@ -1,54 +1,56 @@
-import type { ReactNode } from 'react'
-import React from 'react'
+import type { ReactNode, Ref } from 'react'
+import React, { forwardRef } from 'react'
 
-import { createComponent } from '@vtex/admin-ui-react'
 import { unstable_useId as useId } from 'reakit/Id'
 
 import { Label } from '../label'
 import { Stack } from '../stack'
 import { Inline } from '../inline'
 import { CheckboxInput } from './checkbox-input'
-import type { CheckboxInputOptions } from './checkbox-input'
+import type { CheckboxInputProps } from './checkbox-input'
 import { FormControl, FormControlMessage } from '../form-control'
+import { labelTheme } from './checkbox.css'
 
-import * as style from './checkbox.style'
+export const Checkbox = forwardRef(function Checkbox(
+  props: CheckboxProps,
+  ref: Ref<HTMLInputElement>
+) {
+  const {
+    id,
+    label,
+    helpText,
+    error = false,
+    errorText,
+    ...checkboxInputProps
+  } = props
 
-export const Checkbox = createComponent<typeof CheckboxInput, CheckboxOptions>(
-  (props) => {
-    const {
-      id,
-      label,
-      helpText,
-      error = false,
-      errorText,
-      ...checkboxInputProps
-    } = props
+  const { id: baseId } = useId({ id })
 
-    const { id: baseId } = useId({ id })
+  return (
+    <FormControl>
+      <Inline hSpace="$space-2" vSpace="" spaceInside>
+        <CheckboxInput
+          ref={ref}
+          id={baseId}
+          error={error}
+          {...checkboxInputProps}
+        />
+        <Stack space="$space-05">
+          <Label htmlFor={baseId} className={labelTheme}>
+            {label}
+          </Label>
+          <FormControlMessage
+            error={error}
+            helpText={helpText}
+            errorText={errorText}
+          />
+        </Stack>
+      </Inline>
+    </FormControl>
+  )
+})
 
-    return (
-      <FormControl>
-        <Inline hSpace="$space-2" vSpace="" spaceInside>
-          <CheckboxInput id={baseId} error={error} {...checkboxInputProps} />
-          <Stack space="$space-05">
-            <Label htmlFor={baseId} className={style.labelTheme}>
-              {label}
-            </Label>
-            <FormControlMessage
-              error={error}
-              helpText={helpText}
-              errorText={errorText}
-            />
-          </Stack>
-        </Inline>
-      </FormControl>
-    )
-  }
-)
-
-Checkbox.displayName = 'Checkbox'
-
-export interface CheckboxOptions extends CheckboxInputOptions {
+export type CheckboxProps = CheckboxInputProps & {
   /**
    * Checkbox error text. It appears when error property is set to true.
    */
@@ -64,7 +66,5 @@ export interface CheckboxOptions extends CheckboxInputOptions {
   label?: ReactNode
 }
 
-export type CheckboxProps = React.ComponentPropsWithRef<typeof Checkbox>
-
-export { useCheckboxState } from './state'
-export type { CheckboxState, CheckboxStateReturn } from './state'
+export { useCheckboxState } from './use-checkbox-state'
+export type { CheckboxState, CheckboxStateReturn } from './use-checkbox-state'
