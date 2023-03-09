7552857696f322638457ca1a7212aa1969b96489
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2023-03-09 17:11:53 -0300

    feat(text-input): migrate to the new component model

---
+++ packages/admin-ui/src/text-input/text-input-container.tsx
@@ -0,0 +1,35 @@
+import type { ComponentPropsWithoutRef, Ref } from 'react'
+import React, { forwardRef } from 'react'
+import { cx } from '@vtex/admin-ui-core'
+
+import { containerTheme } from './text-input.css'
+
+export const TextInputContainer = forwardRef(function TextInputContainer(
+  props: TextInputContainerProps,
+  ref: Ref<HTMLDivElement>
+) {
+  const {
+    error = false,
+    disabled = false,
+    className = '',
+    children,
+    ...htmlProps
+  } = props
+
+  return (
+    <div
+      ref={ref}
+      data-error={error}
+      data-disabled={disabled}
+      className={cx(containerTheme, className)}
+      {...htmlProps}
+    >
+      {children}
+    </div>
+  )
+})
+
+export type TextInputContainerProps = ComponentPropsWithoutRef<'div'> & {
+  error?: boolean
+  disabled?: boolean
+}
---
+++ packages/admin-ui/src/text-input/text-input-element.tsx
@@ -0,0 +1,17 @@
+import { cx } from '@vtex/admin-ui-core'
+import type { ComponentPropsWithoutRef, Ref } from 'react'
+import React, { forwardRef } from 'react'
+import { inputTheme } from './text-input.css'
+
+export const TextInputElement = forwardRef(function TextInputElement(
+  props: TextInputElementProps,
+  ref: Ref<HTMLInputElement>
+) {
+  const { className = '', ...htmlProps } = props
+
+  return (
+    <input ref={ref} className={cx(inputTheme, className)} {...htmlProps} />
+  )
+})
+
+export type TextInputElementProps = ComponentPropsWithoutRef<'input'>
