d1082a501745398674c2257f7389b667ec9338e2
matheusps <cout.matheusps@gmail.com>
2023-04-12 19:04:38 -0300

    feat(admin-ui-gatsby): drop support to gatsby

--- packages/gatsby-plugin-admin-ui/src/provider.js
+++
@@ -1,8 +0,0 @@
-import { createElement } from 'react'
-import { ThemeProvider } from '@vtex/admin-ui'
-
-export function wrapRootElement(args) {
-  const { element } = args
-
-  return createElement(ThemeProvider, {}, element)
-}
