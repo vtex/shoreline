59d4745dba8e1e95e52e601daae219c332a2e23c
matheusps <cout.matheusps@gmail.com>
2023-06-29 10:40:09 -0300

    docs(raccoon): add documentation

--- packages/next-docs/theme.config.tsx
+++ packages/next-docs/theme.config.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import type { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>🏝️ Beachfront</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/vtex/beachfront',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/vtex/beachfront/next-docs',
   footer: {
     text: 'Nextra Docs Template',
   },
