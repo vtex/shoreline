904032518c5e073ee944f46d130026d8b8b110b9
matheusps <cout.matheusps@gmail.com>
2023-07-24 13:59:25 -0300

    refactor(csx): new docs organization

--- packages/csx/src/math.ts
+++ packages/csx/src/math.ts
@@ -1,6 +1,16 @@
 import { COMMA, SPACE } from './constants'
+import { cssVar } from './css-var'
+import { isString } from './token-utils'
 
 const MATH_SEPARATOR = `${COMMA}${SPACE}`
+
+function parseValue(value: string | number) {
+  if (!isString(value)) return value
+
+  return cssVar({
+    token: value,
+  })
+}
 
 /**
  * Sets the smallest (most negative) value from a list of comma-separated expressions as the value of a CSS property value
@@ -9,7 +19,9 @@ const MATH_SEPARATOR = `${COMMA}${SPACE}`
  * minx('20rem', '100%') // returns min(20rem, 100%)
  */
 export function min(...values: Array<string | number>): string {
-  const joinedString = values.join(MATH_SEPARATOR)
+  const parsedValues = values.map((value) => parseValue(value))
+
+  const joinedString = parsedValues.join(MATH_SEPARATOR)
 
   return `min(${joinedString})`
 }
@@ -21,7 +33,9 @@ export function min(...values: Array<string | number>): string {
  * max('20rem', '100%') // returns max(20rem, 100%)
  */
 export function max(...values: Array<string | number>): string {
-  const joinedString = values.join(MATH_SEPARATOR)
+  const parsedValues = values.map((value) => parseValue(value))
+
+  const joinedString = parsedValues.join(MATH_SEPARATOR)
 
   return `max(${joinedString})`
 }
@@ -36,7 +50,13 @@ export function max(...values: Array<string | number>): string {
  * clamp('200px', '40%', '400px') // returns clamp(200px, 40%, 400px)
  */
 export function clamp(min: string, val: string, max: string): string {
-  return `clamp(${min}${MATH_SEPARATOR}${val}${MATH_SEPARATOR}${max})`
+  const parsedMin = parseValue(min)
+
+  const parsedVal = parseValue(val)
+
+  const parsedMax = parseValue(max)
+
+  return `clamp(${parsedMin}${MATH_SEPARATOR}${parsedVal}${MATH_SEPARATOR}${parsedMax})`
 }
 
 /**
@@ -57,7 +77,10 @@ export const calc = {
    * @returns {String} "calc(value1 + value2)"
    */
   add(value1: string | number, value2: string | number): string {
-    return `calc(${value1} + ${value2})`
+    const parsedValue1 = parseValue(value1)
+    const parsedValue2 = parseValue(value2)
+
+    return `calc(${parsedValue1} + ${parsedValue2})`
   },
   /**
    * Performs the subtract (-) operation using calc()
@@ -67,7 +90,10 @@ export const calc = {
    * @returns {String} "calc(value1 - value2)"
    */
   subtract(value1: string | number, value2: string | number): string {
-    return `calc(${value1} - ${value2})`
+    const parsedValue1 = parseValue(value1)
+    const parsedValue2 = parseValue(value2)
+
+    return `calc(${parsedValue1} - ${parsedValue2})`
   },
   /**
    * Performs the multiply (*) operation using calc()
@@ -77,7 +103,10 @@ export const calc = {
    * @returns {String} "calc(value1 * value2)"
    */
   multiply(value1: string | number, value2: string | number): string {
-    return `calc(${value1} * ${value2})`
+    const parsedValue1 = parseValue(value1)
+    const parsedValue2 = parseValue(value2)
+
+    return `calc(${parsedValue1} * ${parsedValue2})`
   },
   /**
    * Performs the division (/) operation using calc()
@@ -87,7 +116,10 @@ export const calc = {
    * @returns {String} "calc(value1 / value2)"
    */
   divide(value1: string | number, value2: string | number): string {
-    return `calc(${value1} / ${value2})`
+    const parsedValue1 = parseValue(value1)
+    const parsedValue2 = parseValue(value2)
+
+    return `calc(${parsedValue1} / ${parsedValue2})`
   },
   /**
    * Negate a value using calc()
@@ -96,6 +128,8 @@ export const calc = {
    * @returns {String} "calc(-value)"
    */
   negate(value: string | number): string {
-    return `calc(-${value})`
+    const parsedValue = parseValue(value)
+
+    return `calc(-${parsedValue})`
   },
 }
