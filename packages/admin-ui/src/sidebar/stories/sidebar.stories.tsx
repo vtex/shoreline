91c203ecc392a0cdecb668093188b26965d3dc85
anitavincent <paes.anita@gmail.com>
2023-07-27 14:58:13 -0300

    feat(sidebar): add story for mobile style

--- packages/admin-ui/src/sidebar/stories/sidebar.stories.tsx
+++ packages/admin-ui/src/sidebar/stories/sidebar.stories.tsx
@@ -9,6 +9,8 @@ import {
 } from '../index'
 import { getShellIcon } from '../shell-icons'
 import { csx } from '@vtex/admin-ui-core'
+import { Divider } from '@vtex/admin-ui'
+import * as styles from '../sidebar.css'
 
 export default {
   title: 'admin-ui-review/sidebar',
@@ -60,6 +62,61 @@ export const Example = () => {
               </SidebarLinkGroup>
             ))}
           </SidebarSection>
+        ))}
+      </Sidebar>
+    </div>
+  )
+}
+
+export const Mobile = () => {
+  function useData() {
+    // fetch pro admin-grahql que te retorna um data
+    const aaa = [...mockedData.top, ...mockedData.bottom]
+
+    const optimalData = aaa.map((section) => ({
+      ...section,
+      groups: section.sections,
+    })) // data transformado
+
+    return optimalData
+  }
+
+  const [selectedPage, setSelectedPage] = useState()
+
+  const data = useData()
+
+  return (
+    <div
+      className={csx({
+        display: 'grid',
+        gridTemplateColumns: '320px 1fr',
+        gap: '1rem',
+        height: '100vh',
+      })}
+    >
+      <Sidebar>
+        {data.map((section, index) => (
+          <>
+            <SidebarSection
+              icon={getShellIcon(section.icon)}
+              title={section.label}
+            >
+              {section.groups.map((group) => (
+                <SidebarLinkGroup title={group.title}>
+                  {group.subItems.map((pageLink) => (
+                    <SidebarLink
+                      href={pageLink.path}
+                      onClick={() => setSelectedPage(pageLink.englishLabel)}
+                      active={pageLink.englishLabel === selectedPage}
+                    >
+                      {pageLink.label}
+                    </SidebarLink>
+                  ))}
+                </SidebarLinkGroup>
+              ))}
+            </SidebarSection>
+            <Divider className={styles.sidebarSectionDivider} />
+          </>
         ))}
       </Sidebar>
     </div>
