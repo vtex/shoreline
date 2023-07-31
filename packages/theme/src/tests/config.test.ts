28206e33397d4d5db73b3d8df429bf4bd8e41162
matheusps <cout.matheusps@gmail.com>
2023-07-31 18:26:50 -0300

    feat(shoreline-theme): create a theme package containing configuration utilities

---
+++ packages/theme/src/tests/config.test.ts
@@ -0,0 +1,65 @@
+import { join } from 'path'
+import { defineConfig, isShorelineConfig, loadConfig } from '../config'
+
+describe('defineConfig', () => {
+  it('should allow empty configs', () => {
+    const result = defineConfig({})
+    const expectation = {}
+
+    expect(result).toStrictEqual(expectation)
+  })
+
+  it('should allow filled configs', () => {
+    const result = defineConfig({
+      prefix: 'sl',
+      tokens: {
+        radii: '8px',
+      },
+    })
+
+    const expectation = {
+      prefix: 'sl',
+      tokens: {
+        radii: '8px',
+      },
+    }
+
+    expect(result).toStrictEqual(expectation)
+  })
+})
+
+describe('isShorelineConfig', () => {
+  it('should reject', () => {
+    expect(isShorelineConfig('')).toBeFalsy()
+    expect(isShorelineConfig('shoreline.config')).toBeFalsy()
+    expect(isShorelineConfig('shoreline.config.txt')).toBeFalsy()
+    expect(isShorelineConfig('shoreline.js')).toBeFalsy()
+    expect(isShorelineConfig('config.js')).toBeFalsy()
+  })
+
+  it('accepts js', () => {
+    expect(isShorelineConfig('shoreline.config.js')).toBeTruthy()
+  })
+
+  it('accepts ts', () => {
+    expect(isShorelineConfig('shoreline.config.ts')).toBeTruthy()
+  })
+})
+
+describe('loadConfig', () => {
+  it('should load a js config', () => {
+    const result = loadConfig({
+      cwd: join(__dirname, 'fixtures/js-config'),
+    })
+
+    expect(result.config.prefix).toBe('js')
+  })
+
+  it('should load a ts config', () => {
+    const result = loadConfig({
+      cwd: join(__dirname, 'fixtures/ts-config'),
+    })
+
+    expect(result.config.prefix).toBe('ts')
+  })
+})
---
+++ packages/theme/src/tests/fixtures/ts-config/shoreline.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from '../../../config'
+
+export default defineConfig({
+  prefix: 'ts',
+})
