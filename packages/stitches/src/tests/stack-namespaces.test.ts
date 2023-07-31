c048fe0eeb18af54ef34c679590d471e1eebe1e3
matheusps <cout.matheusps@gmail.com>
2023-07-31 13:59:48 -0300

    feat(shoreline-csx): create stitches integration

---
+++ packages/stitches/src/tests/stack-namespaces.test.ts
@@ -0,0 +1,46 @@
+import { stackNamespaces } from '../stack-namespaces'
+
+test('should return an empty object', () => {
+  const result = stackNamespaces({})
+  const expectation = {}
+
+  expect(result).toStrictEqual(expectation)
+})
+
+test('should stack namespaces', () => {
+  const result = stackNamespaces({
+    background: 'white',
+    '@layer': {
+      'sl-tokens': {
+        '--sl-color-blue-100': 'blue',
+      },
+      'sl-base': {
+        body: {
+          margin: 0,
+        },
+      },
+    },
+    '@media': {
+      '(prefers-color-scheme: dark)': {
+        background: 'black',
+      },
+    },
+  })
+
+  const expectation = {
+    background: 'white',
+    '@layer sl-tokens': {
+      '--sl-color-blue-100': 'blue',
+    },
+    '@layer sl-base': {
+      body: {
+        margin: 0,
+      },
+    },
+    '@media (prefers-color-scheme: dark)': {
+      background: 'black',
+    },
+  }
+
+  expect(result).toStrictEqual(expectation)
+})
