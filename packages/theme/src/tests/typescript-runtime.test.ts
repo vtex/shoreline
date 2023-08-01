e5030dfe867493129d0bd6b42aacf9df9e922a51
matheusps <cout.matheusps@gmail.com>
2023-08-01 07:19:51 -0300

    test(theme): remove runtime testing
    The jest env does not supports this testing on CI

--- packages/theme/src/tests/typescript-runtime.test.ts
+++
@@ -1,9 +0,0 @@
-import { join } from 'path'
-import { lazyRuntime } from '../typescript-runtime'
-
-test('should eval code from path', () => {
-  const path = join(__dirname, 'fixtures/ts-config/shoreline.config.ts')
-  const code = lazyRuntime()(path)
-
-  expect(code.default.prefix).toStrictEqual('ts')
-})
