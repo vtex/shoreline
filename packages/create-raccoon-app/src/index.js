da93397df189aaeb0b842d906ebabf520248ee6c
matheusps <cout.matheusps@gmail.com>
2023-08-10 21:03:46 -0300

    refactor(rc-scripts): change the name of the package

--- packages/create-raccoon-app/src/index.js
+++ packages/create-raccoon-app/src/index.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 
-import path from 'node:path'
+import path, { dirname } from 'path'
 import minimist from 'minimist'
 import { Plop, run } from 'plop'
-import { dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { fileURLToPath } from 'url'
 
 const args = process.argv.slice(2)
 const argv = minimist(args)
 
-function init() {
+function main() {
   const __dirname = dirname(fileURLToPath(import.meta.url))
 
   Plop.prepare(
@@ -25,9 +24,10 @@ function init() {
           ...env,
           dest: process.cwd(),
         }
+
         return run(options, undefined, true)
       })
   )
 }
 
-init()
+main()
