e422c9ba9014af7be20c163b1ed4bf77eb838772
anitavincent <paes.anita@gmail.com>
2023-09-04 18:16:19 -0300

    feat(theme): handle responsive values on a preset

--- packages/theme/src/tokens-to-css-variables.ts
+++ packages/theme/src/tokens-to-css-variables.ts
@@ -1,13 +1,47 @@
 import cssesc from 'cssesc'
 
+const breakpointValues = {
+  small: '48rem',
+  medium: '64rem',
+  large: '90rem',
+}
+
 export function tokensToCssVariables(object: Record<string, any>) {
   let cssString = ''
+  const responsiveStyles = {
+    small: '',
+    medium: '',
+    large: '',
+  }
 
   for (const key in object) {
     const value = object[key]
-    const variable = `${cssesc(key)}: ${cssesc(value)};\n`
 
-    cssString += variable
+    if (key.match(/-@media-/g)) {
+      const baseVariable = key.replace(/-@media-[^-]*/g, '')
+
+      if (key.match(/-@media-small/g)) {
+        const variable = `${cssesc(baseVariable)}: ${cssesc(value)};\n`
+
+        responsiveStyles.small += variable
+      }
+
+      if (key.match(/-@media-medium/g)) {
+        const variable = `${cssesc(baseVariable)}: ${cssesc(value)};\n`
+
+        responsiveStyles.medium += variable
+      }
+
+      if (key.match(/-@media-large/g)) {
+        const variable = `${cssesc(baseVariable)}: ${cssesc(value)};\n`
+
+        responsiveStyles.large += variable
+      }
+    } else {
+      const variable = `${cssesc(key)}: ${cssesc(value)};\n`
+
+      cssString += variable
+    }
   }
 
   return `
@@ -15,6 +49,23 @@ export function tokensToCssVariables(object: Record<string, any>) {
     :root {
       ${cssString}
     }
+
+    ${Object.keys(responsiveStyles)
+      .map((breakpoint) =>
+        responsiveStyles[breakpoint as keyof typeof responsiveStyles]
+          ? `
+        @media (min-width: ${
+          breakpointValues[breakpoint as keyof typeof breakpointValues]
+        }) {
+          :root {
+            ${responsiveStyles[breakpoint as keyof typeof responsiveStyles]}
+          }
+        }
+      `
+          : ''
+      )
+      .join('')}
+
   }
   `
 }
