04e4375749b2c962f68f33c7edf26da862781c85
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2023-09-05 11:58:52 -0300

    docs(tokens-grid): fix build error

--- packages/next-docs/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import '../shoreline/theme.css'
-
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
