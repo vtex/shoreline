6118ff4086e30b57de3623c3cb22afee7d5ba31c
anitavincent <paes.anita@gmail.com>
2023-10-06 15:58:02 -0300

    build: untrack shoreline styles

--- .storybook/themeDecorator.jsx
+++ .storybook/themeDecorator.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import { ThemeProvider } from '../packages/admin-ui/dist/vtex-admin-ui.esm'
 
 export function themeDecorator(storyFn) {
-  return <ThemeProvider>{storyFn()}</ThemeProvider>
+  return <ThemeProvider experimentalDisabledGlobalStyles>{storyFn()}</ThemeProvider>
 }
