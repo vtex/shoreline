8089d684cef21f84e8b7b016fc1edfb4badff8ac
matheusps <cout.matheusps@gmail.com>
2023-10-09 10:51:22 -0300

    docs(button): improve dx with code docs

--- packages/components/src/compose/compose-element.tsx
+++ packages/components/src/compose/compose-element.tsx
@@ -1,12 +1,21 @@
-import type {
-  AnyObject,
-  ElementWithRef,
-  RenderProps,
-} from '@vtex/shoreline-utils'
+import type { AnyObject, ElementWithRef } from '@vtex/shoreline-utils'
 import { mergeProps, mergeRefs } from '@vtex/shoreline-utils'
 import type { ForwardedRef, ReactNode } from 'react'
 import { cloneElement } from 'react'
 
+/**
+ * Clone element merging its props and refs with passed props and refs.
+ * @example
+ * const element: React.ReactNode = ...
+ * const props = {}
+ * cosnt ref = useRef()
+ *
+ * composeElement({
+ *  element,
+ *  props,
+ *  ref,
+ * })
+ */
 export function composeElement({ element, props, ref }: ComposeElementArgs) {
   const {
     props: { children, ...ownProps },
@@ -24,8 +33,16 @@ export function composeElement({ element, props, ref }: ComposeElementArgs) {
 }
 
 export interface ComposeElementArgs {
+  /**
+   * Element to clone
+   */
   element: ReactNode
+  /**
+   * Props to merge with element props
+   */
   props: AnyObject
+  /**
+   * Ref to merge with element ref
+   */
   ref: ForwardedRef<any>
-  wrap?: RenderProps
 }
