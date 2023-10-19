971bf14a13ee3790d310bf6d5b7b0d62858bf2fd
marcelovicentegc <marcelovicentegc@gmail.com>
2023-10-19 17:16:55 -0300

    tests(raccoon-next): test utils and some core functions

---
+++ packages/raccoon-next/src/tests/router.vitest.test.ts
@@ -0,0 +1,28 @@
+import { useNavigation } from '../router'
+import { vi } from 'vitest'
+import * as message from '../message'
+
+test('useNavigation should return navigate function', () => {
+  const { navigate } = useNavigation()
+  expect(typeof navigate).toBe('function')
+})
+
+test('navigate should publish message with path', async () => {
+  vi.spyOn(message, 'publishMessage')
+  const { navigate } = useNavigation()
+  navigate('/test')
+
+  expect(message.publishMessage).toHaveBeenCalledWith({
+    type: 'navigation',
+    data: {
+      path: '/test',
+    },
+  })
+})
+
+test('navigate should log warning if path does not start with /', () => {
+  const mockConsoleWarn = vi.spyOn(console, 'warn').mockImplementation(() => {})
+  const { navigate } = useNavigation()
+  navigate('test')
+  expect(mockConsoleWarn).toHaveBeenCalledWith('The path must start with /')
+})
---
+++ packages/raccoon-next/src/utils/tests/store.vitest.test.ts
@@ -0,0 +1,24 @@
+import { test, expect } from 'vitest'
+import { createStore } from '../store'
+
+test('createStore returns a store object with the expected shape', () => {
+  const store = createStore()
+
+  expect(store !== undefined)
+  expect(typeof store === 'object')
+  expect(typeof store.subscribe === 'function')
+  expect(typeof store.set === 'function')
+  expect(typeof store.read === 'function')
+})
+
+test('store.read returns the current value of the store', () => {
+  const store = createStore<{
+    status: string
+    payload: number
+  }>()
+
+  store.set({ status: 'fulfilled', payload: 42 })
+
+  expect(store.read().status === 'fulfilled')
+  expect(store.read().payload === 42)
+})
