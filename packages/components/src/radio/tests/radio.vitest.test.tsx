1d345b36b958bb435b25d5f186b5ba140911d7ae
anitavincent <paes.anita@gmail.com>
2023-10-24 14:31:13 -0300

    feat(radio): add basic tests

--- packages/components/src/radio/tests/radio.vitest.test.tsx
+++ packages/components/src/radio/tests/radio.vitest.test.tsx
@@ -6,8 +6,14 @@ import { Radio } from '../radio'
 
 describe('radio', () => {
   test('renders', () => {
-    const { container } = render(<Radio />)
+    const { container } = render(<Radio label="label" value="id" />)
 
     expect(container.querySelector('[data-sl-radio]')).toBeInTheDocument()
+  })
+
+  test('renders field label', () => {
+    const { getByText } = render(<Radio label="label" value="id" />)
+
+    expect(getByText('label')).toBeInTheDocument()
   })
 })
