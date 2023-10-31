94eb2430a1ce69e4d1ae5487a5c8620add7af987
matheusps <cout.matheusps@gmail.com>
2023-10-31 14:58:40 -0300

    fix(select): styles

--- packages/components/src/select/select-option.tsx
+++ packages/components/src/select/select-option.tsx
@@ -1,17 +1,11 @@
 import type { ComponentPropsWithoutRef } from 'react'
 import React, { forwardRef } from 'react'
-import { SelectItem, useSelectContext } from '@ariakit/react'
+import { SelectItem } from '@ariakit/react'
 import { SelectOptionCheck } from './select-option-check'
 
 export const SelectOption = forwardRef<HTMLDivElement, SelectOptionProps>(
   function SelectOption(props, ref) {
     const { asChild = false, children, ...otherProps } = props
-    const context = useSelectContext()
-
-    console.log({
-      context,
-      state: context?.getState(),
-    })
 
     return (
       <SelectItem
