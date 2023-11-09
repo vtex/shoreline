58e155e1857c25dcfa947e3f10532ce8d5857ed4
anitavincent <paes.anita@gmail.com>
2023-11-09 18:32:05 -0300

    fix(textarea): style and layout

--- packages/components/src/textarea/textarea.tsx
+++ packages/components/src/textarea/textarea.tsx
@@ -3,6 +3,9 @@ import React, { forwardRef } from 'react'
 import { useId } from '@vtex/shoreline-utils'
 
 import { Field, FieldLabel, FieldMessage } from '../field'
+import { Stack } from '../stack'
+import { Flex } from '../flex'
+import { Grid } from '../grid'
 
 export const Textarea = forwardRef<HTMLDivElement, TextareaProps>(
   function Textarea(props, ref) {
@@ -25,7 +28,7 @@ export const Textarea = forwardRef<HTMLDivElement, TextareaProps>(
     const id = baseId ?? useId()
 
     return (
-      <Field ref={ref} className={className}>
+      <Field ref={ref} className={className} data-sl-textarea-container>
         {label && <FieldLabel htmlFor={id}>{label}</FieldLabel>}
         <div data-sl-textarea-container>
           <textarea
@@ -36,15 +39,25 @@ export const Textarea = forwardRef<HTMLDivElement, TextareaProps>(
             disabled={disabled}
             maxLength={maxLength}
             aria-invalid={error}
+            value={value}
             {...htmlProps}
           />
-          {maxLength && (
-            <p data-sl-textarea-char-counter>
-              {charCount} / {maxLength}
-            </p>
-          )}
         </div>
-        <FieldMessage error={error} errorText={errorText} helpText={helpText} />
+        <Grid templateColumns="1fr auto" data-sl-field-message>
+          <Stack space="$space-0">
+            {helpText && <p data-sl-field-message-text>{helpText}</p>}
+            {error && (
+              <p data-sl-field-message-text role="alert">
+                {errorText}
+              </p>
+            )}
+          </Stack>
+          {maxLength && (
+            <div data-sl-textarea-char-counter>
+              {charCount} / {maxLength}
+            </div>
+          )}
+        </Grid>
       </Field>
     )
   }
