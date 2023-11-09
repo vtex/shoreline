f130e75ed29ef24263f6a6a56fb00f677a75c6ac
matheusps <cout.matheusps@gmail.com>
2023-11-09 09:56:05 -0300

    feat(tab): create component

---
+++ packages/components/src/tab/tab.tsx
@@ -0,0 +1,44 @@
+import type { ComponentPropsWithoutRef } from 'react'
+import React, { forwardRef } from 'react'
+import { Tab as BaseTab } from '@ariakit/react'
+
+/**
+ * @example
+ * <TabProvider>
+ *  <TabList>
+ *    <Tab>Tab 1</Tab>
+ *    <Tab>Tab 2</Tab>
+ *  </TabList>
+ *  <TabPanel>
+ *    Tab 1
+ *  </TabPanel>
+ *  <TabPanel>
+ *    Tab 2
+ *  </TabPanel>
+ * </TabProvider>
+ */
+export const Tab = forwardRef<HTMLButtonElement, TabProps>(function Tab(
+  props,
+  ref
+) {
+  const { asChild = false, children, ...otherProps } = props
+
+  return (
+    <BaseTab
+      data-sl-tab
+      ref={ref}
+      render={asChild ? (children as JSX.Element) : undefined}
+      {...otherProps}
+    >
+      {children}
+    </BaseTab>
+  )
+})
+
+export interface TabProps extends ComponentPropsWithoutRef<'button'> {
+  /**
+   * Enable children composition
+   * @default false
+   */
+  asChild?: boolean
+}
