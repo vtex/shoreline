c56d0ba447b10fc9658defe9c5c717f55a4a73db
matheusps <cout.matheusps@gmail.com>
2023-11-13 16:15:51 -0300

    build(shoreline): replace rollup for tsup as bundler for components and utils

--- packages/utils/src/forward-ref.ts
+++ packages/utils/src/forward-ref.ts
@@ -4,9 +4,9 @@ import * as React from 'react'
  * Augmented forwardRef function to support generics
  */
 export function forwardRef<T, P = {}>(
-  render: (props: P, ref: React.Ref<T>) => React.ReactNode | null
-): (props: P & React.RefAttributes<T>) => React.ReactNode | null {
-  return React.forwardRef(render) as unknown as (
+  render: (props: P, ref: React.Ref<T>) => JSX.Element | null
+): (props: P & React.RefAttributes<T>) => JSX.Element | null {
+  return React.forwardRef(render as any) as unknown as (
     props: P & React.RefAttributes<T>
-  ) => React.ReactNode | null
+  ) => JSX.Element | null
 }
---
+++ packages/utils/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  external: ['react'],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  dts: true,
+  legacyOutput: true,
+})
