0216214b154542135da59f6a717885a8c5791f00
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2023-11-13 15:19:27 -0300

    docs: add icons documentation

---
+++ packages/next-docs/components/icons-grid/icons.ts
@@ -0,0 +1,565 @@
+// Generated from Icons Description spreadsheet:
+// https://docs.google.com/spreadsheets/d/1XapTcKjZIGKkz-8PtYoSC5IIvB4rkPzuaP8ymBTKTyM/edit?usp=sharing
+const iconsInfo = [
+  {
+    name: 'Alarm',
+    description: 'clock; time',
+  },
+  {
+    name: 'Archive',
+    description: '–',
+  },
+  {
+    name: 'ArrowCounterClockwise',
+    description: 'undo',
+  },
+  {
+    name: 'ArrowDown',
+    description: 'move',
+  },
+  {
+    name: 'ArrowDownSmall',
+    description: 'move',
+  },
+  {
+    name: 'ArrowLeft',
+    description: 'back; move',
+  },
+  {
+    name: 'ArrowLineDown',
+    description: 'download; export',
+  },
+  {
+    name: 'ArrowLineUp',
+    description: 'upload; import',
+  },
+  {
+    name: 'ArrowRight',
+    description: 'forward; move',
+  },
+  {
+    name: 'ArrowsClockwise',
+    description: 'refresh; update',
+  },
+  {
+    name: 'ArrowUp',
+    description: 'move',
+  },
+  {
+    name: 'ArrowUpRight',
+    description: 'external; link; open',
+  },
+  {
+    name: 'ArrowUpRightSmall',
+    description: 'external; link; open',
+  },
+  {
+    name: 'ArrowUpSmall',
+    description: 'move',
+  },
+  {
+    name: 'ArrowUUpLeft',
+    description: 'return',
+  },
+  {
+    name: 'Barcode',
+    description: 'scan; read; capture',
+  },
+  {
+    name: 'Bell',
+    description: 'notifications; alerts; messages; center',
+  },
+  {
+    name: 'CalendarBlank',
+    description: 'date; month',
+  },
+  {
+    name: 'CameraFill',
+    description: 'photo; image',
+  },
+  {
+    name: 'CaretDown',
+    description: 'chevron; expand; collapse; accordion',
+  },
+  {
+    name: 'CaretDownSmall',
+    description: 'chevron; expand; collapse; accordion',
+  },
+  {
+    name: 'CaretLeft',
+    description: 'chevron; previous; expand; collapse',
+  },
+  {
+    name: 'CaretLeftSmall',
+    description: 'chevron; previous; expand; collapse',
+  },
+  {
+    name: 'CaretRight',
+    description: 'chevron; next; expand; collapse',
+  },
+  {
+    name: 'CaretRightSmall',
+    description: 'chevron; next; expand; collapse',
+  },
+  {
+    name: 'CaretUp',
+    description: 'chevron; expand; collapse; accordion',
+  },
+  {
+    name: 'CaretUpSmall',
+    description: 'chevron; expand; collapse; accordion',
+  },
+  {
+    name: 'ChartBar',
+    description: 'graph; performance; metrics; data; stats; report',
+  },
+  {
+    name: 'ChartLineUp',
+    description: 'graph; performance; metrics; data; stats; report; growth',
+  },
+  {
+    name: 'ChatText',
+    description: 'message; comment',
+  },
+  {
+    name: 'Check',
+    description: 'success; mark; complete',
+  },
+  {
+    name: 'CheckCircle',
+    description: 'success; completed; positive',
+  },
+  {
+    name: 'CheckCircleFill',
+    description: 'success; completed; positive',
+  },
+  {
+    name: 'CheckSmall',
+    description: 'success; mark; complete',
+  },
+  {
+    name: 'CirclesThreePlusFill',
+    description: 'add',
+  },
+  {
+    name: 'Clock',
+    description: 'time; hour; minute; duration',
+  },
+  {
+    name: 'ClockClockwise',
+    description: 'time; duration',
+  },
+  {
+    name: 'CloudArrowUp',
+    description: 'publish; upload; send',
+  },
+  {
+    name: 'Code',
+    description: 'technical; development; developer; center',
+  },
+  {
+    name: 'CopySimple',
+    description: 'duplicate; url',
+  },
+  {
+    name: 'CornersIn',
+    description: 'screen; minimize; reduce',
+  },
+  {
+    name: 'CornersOut',
+    description: 'full; screen; maximize',
+  },
+  {
+    name: 'CreditCard',
+    description: 'payment; method; network',
+  },
+  {
+    name: 'Cube',
+    description:
+      'delivery; shipping; shipment; order; fulfilment; package; box',
+  },
+  {
+    name: 'CurrencyCircleDollar',
+    description: 'money; value; cost',
+  },
+  {
+    name: 'DotsSixVertical',
+    description: 'move; drag',
+  },
+  {
+    name: 'DotsSixVerticalSmall',
+    description: 'move; drag',
+  },
+  {
+    name: 'DotsThreeVertical',
+    description: 'more; actions; menu',
+  },
+  {
+    name: 'EnvelopeFill',
+    description: 'email; message; share; send',
+  },
+  {
+    name: 'EnvelopeSimple',
+    description: 'email; message; share; send',
+  },
+  {
+    name: 'Eye',
+    description: 'view; see; show',
+  },
+  {
+    name: 'EyeSlash',
+    description: 'hide',
+  },
+  {
+    name: 'Faders',
+    description: 'filter; preferences;',
+  },
+  {
+    name: 'FileText',
+    description: 'document; text',
+  },
+  {
+    name: 'Flag',
+    description: 'report; mark as important',
+  },
+  {
+    name: 'GearSix',
+    description: 'settings; configurations; preferences',
+  },
+  {
+    name: 'Gift',
+    description: 'send; reward',
+  },
+  {
+    name: 'GlobeHemisphereWest',
+    description: 'localize; localization; language; translate; world; globe',
+  },
+  {
+    name: 'HandbagSimple',
+    description: 'cart; products; items',
+  },
+  {
+    name: 'HandPointingFill',
+    description: '–',
+  },
+  {
+    name: 'Headset',
+    description: 'support; help; contact; sac',
+  },
+  {
+    name: 'Heart',
+    description: 'favorite; like',
+  },
+  {
+    name: 'House',
+    description: 'home',
+  },
+  {
+    name: 'IdentificationCard',
+    description: 'account; user; settings; roles',
+  },
+  {
+    name: 'ImageSquare',
+    description: 'picture; asset; media; placeholder',
+  },
+  {
+    name: 'ImageSquareFill',
+    description: 'picture; asset; media; placeholder',
+  },
+  {
+    name: 'Info',
+    description: 'information; view; details',
+  },
+  {
+    name: 'InfoFill',
+    description: '–',
+  },
+  {
+    name: 'Key',
+    description: 'account; settings; code; password; sensitive',
+  },
+  {
+    name: 'Layout',
+    description: 'storefront; cms; pages',
+  },
+  {
+    name: 'LinkBreak',
+    description: 'unlink; remove; url',
+  },
+  {
+    name: 'LinkFill',
+    description: 'url; copy; share; send',
+  },
+  {
+    name: 'LinkSimple',
+    description: 'url; copy; share; send',
+  },
+  {
+    name: 'List',
+    description: 'menu; navigation',
+  },
+  {
+    name: 'ListBullets',
+    description: 'points; bulleted',
+  },
+  {
+    name: 'ListNumbers',
+    description: 'numbered',
+  },
+  {
+    name: 'LockKey',
+    description: 'password; secret; sensitive',
+  },
+  {
+    name: 'LockKeyOpen',
+    description: 'unlock',
+  },
+  {
+    name: 'MagnifyingGlass',
+    description: 'search; find',
+  },
+  {
+    name: 'MagnifyingGlassPlus',
+    description: 'zoom; in; enlarge',
+  },
+  {
+    name: 'MagnifyingGlassSmall',
+    description: 'search; find',
+  },
+  {
+    name: 'MapPin',
+    description: 'location; address; coordinate; gps',
+  },
+  {
+    name: 'Megaphone',
+    description: 'promotions',
+  },
+  {
+    name: 'MegaphoneFill',
+    description: 'promotions',
+  },
+  {
+    name: 'Minus',
+    description: 'remove; subtract',
+  },
+  {
+    name: 'MinusCircleFill',
+    description: 'pending',
+  },
+  {
+    name: 'MinusSmall',
+    description: 'remove; subtract',
+  },
+  {
+    name: 'Newspaper',
+    description: 'blog',
+  },
+  {
+    name: 'Notebook',
+    description: 'help; center; recipes; documentation; docs; support',
+  },
+  {
+    name: 'Paperclip',
+    description: 'attachment; file; asset; bond; connect; combine',
+  },
+  {
+    name: 'PaperPlaneTilt',
+    description: 'send; share; email',
+  },
+  {
+    name: 'PauseCircle',
+    description: '–',
+  },
+  {
+    name: 'Pencil',
+    description: 'edit; write; compose; change',
+  },
+  {
+    name: 'PlayCircle',
+    description: 'run',
+  },
+  {
+    name: 'PlayCircleFill',
+    description: 'run',
+  },
+  {
+    name: 'Plus',
+    description: 'add; new; +',
+  },
+  {
+    name: 'Printer',
+    description: '–',
+  },
+  {
+    name: 'Pulse',
+    description: 'help; health; check; status; platform; support; activity',
+  },
+  {
+    name: 'Question',
+    description: 'help; contact; support; center; documentation; docs',
+  },
+  {
+    name: 'Receipt',
+    description: 'billing',
+  },
+  {
+    name: 'RocketLaunch',
+    description: 'launch; success; new; performance',
+  },
+  {
+    name: 'Rows',
+    description: 'table; list',
+  },
+  {
+    name: 'ShareNetwork',
+    description: 'marketplace; network; sellers; tree',
+  },
+  {
+    name: 'ShareNetworkFill',
+    description: 'marketplace; network; sellers; tree',
+  },
+  {
+    name: 'ShoppingCartSimple',
+    description: 'cart; products; order; orders; items',
+  },
+  {
+    name: 'SignOut',
+    description: 'log; out; logout; leave',
+  },
+  {
+    name: 'SlidersFill',
+    description: 'preferences',
+  },
+  {
+    name: 'Sparkle',
+    description: 'AI; intelligence',
+  },
+  {
+    name: 'SparkleFill',
+    description: 'intelligence; AI',
+  },
+  {
+    name: 'SquaresFour',
+    description: 'gallery; grid',
+  },
+  {
+    name: 'Stack',
+    description: 'apps; store; new; install',
+  },
+  {
+    name: 'Star',
+    description: 'favorite; rate; rating',
+  },
+  {
+    name: 'StarFill',
+    description: 'favorite; rate; rating',
+  },
+  {
+    name: 'Storefront',
+    description: '–',
+  },
+  {
+    name: 'Tag',
+    description: 'product; products; item; sku',
+  },
+  {
+    name: 'TagFill',
+    description: 'product; products; item; sku',
+  },
+  {
+    name: 'TextAlignCenter',
+    description: 'centered',
+  },
+  {
+    name: 'TextAlignLeft',
+    description: '–',
+  },
+  {
+    name: 'TextAlignRight',
+    description: '–',
+  },
+  {
+    name: 'TextB',
+    description: 'bold',
+  },
+  {
+    name: 'TextItalic',
+    description: '–',
+  },
+  {
+    name: 'TextUnderline',
+    description: '–',
+  },
+  {
+    name: 'Ticket',
+    description: 'gift; card; promo; code; coupon',
+  },
+  {
+    name: 'Trash',
+    description: 'remove; delete; move',
+  },
+  {
+    name: 'TreeStructureFill',
+    description: 'category; taxonomy',
+  },
+  {
+    name: 'TrendDownFill',
+    description: 'reduce; decrease',
+  },
+  {
+    name: 'TrendUpFill',
+    description: 'grow',
+  },
+  {
+    name: 'Truck',
+    description: 'delivery; shipping; package',
+  },
+  {
+    name: 'UniteFill',
+    description: 'join; combine; match',
+  },
+  {
+    name: 'User',
+    description: 'name; profile; account',
+  },
+  {
+    name: 'Users',
+    description: 'organization; people; contacts; crm; customers',
+  },
+  {
+    name: 'VideoCameraFill',
+    description: '–',
+  },
+  {
+    name: 'WarningCircle',
+    description: 'important; urgent; alert',
+  },
+  {
+    name: 'WarningCircleFill',
+    description: 'important; urgent; alert; important; urgent; alert',
+  },
+  {
+    name: 'X',
+    description: 'close; remove',
+  },
+  {
+    name: 'XCircle',
+    description: 'cancel; stop; error; critical; failed',
+  },
+  {
+    name: 'XCircleFill',
+    description: 'cancel; stop; error; critical; failed',
+  },
+  {
+    name: 'XSmall',
+    description: 'close; remove',
+  },
+]
+
+export const descriptions = iconsInfo.reduce((acc, info) => {
+  return { ...acc, [`Icon${info.name}`]: info.description }
+}, {})
+
+export * as icons from '@vtex/shoreline-icons'
+
+export { names } from '@vtex/shoreline-icons'
