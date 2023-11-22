f123dfc5bd993caccb87f7b369bd766d3024983d
marcelovicentegc <marcelovicentegc@gmail.com>
2023-11-22 12:53:15 -0300

    docs: auto generate _meta.json

--- scripts/typedoc-to-nextra.js
+++ scripts/typedoc-to-nextra.js
@@ -1,32 +1,32 @@
+// This could be packaged as a separate module and published to npm to benefit
+// the community, but for now it's just a script that we run manually
 const fs = require('fs')
 const prettier = require('prettier')
-var path = require('path')
 
-function copyFileSync(source, target) {
-  var targetFile = target
+const prettierConfigPath = require.resolve('../.prettierrc')
+const prettierConfig = require(prettierConfigPath)
+const path = require('path')
 
-  // If target is a directory, a new file with the same name will be created
-  if (fs.existsSync(target)) {
-    if (fs.lstatSync(target).isDirectory()) {
-      targetFile = path.join(target, path.basename(source))
-    }
-  }
-
-  fs.writeFileSync(targetFile, fs.readFileSync(source))
-}
-
+/**
+ * Copy a folder recursively
+ *
+ * @param {string} source
+ * @param {string} target
+ */
 function copyFolderRecursiveSync(source, target) {
-  // Check if folder needs to be created or integrated
   const targetFolder = path.join(target, path.basename(source))
+
   if (!fs.existsSync(targetFolder)) {
     fs.mkdirSync(targetFolder)
   }
 
-  // Copy
+  // Copy while preserving the content from the target folder
   if (fs.lstatSync(source).isDirectory()) {
     const files = fs.readdirSync(source)
+
     files.forEach(function (file) {
       const curSource = path.join(source, file)
+
       if (fs.lstatSync(curSource).isDirectory()) {
         copyFolderRecursiveSync(curSource, targetFolder)
       } else {
@@ -36,12 +36,19 @@ function copyFolderRecursiveSync(source, target) {
   }
 }
 
+/**
+ * Split the generated docs into multiple files.
+ * This is the typedoc-to-nextra.js entrypoint.
+ */
 function splitIntoMultipleFiles() {
   const cwd = process.cwd()
 
   const rootFile = fs.readFileSync(`${cwd}/__tmpDocs/modules.md`, 'utf8')
   // typedoc-plugin-markdown generates a single file with all the docs separated by "___"
   const files = rootFile.split('___')
+
+  const components = []
+  const hooks = []
 
   for (const file of files) {
     try {
@@ -64,13 +71,18 @@ function splitIntoMultipleFiles() {
           .toLowerCase()
 
         const folderPath = `${cwd}/__tmpDocs/hooks/${kebabCaseName}`
+
         fs.mkdirSync(folderPath, {
           recursive: true,
         })
 
         const filePath = `${folderPath}/code.mdx`
+
         fs.writeFileSync(filePath, file)
+
         prettify(filePath)
+
+        hooks.push(componentName)
 
         continue
       }
@@ -81,14 +93,18 @@ function splitIntoMultipleFiles() {
           .toLowerCase()
 
         const folderPath = `${cwd}/__tmpDocs/components/${kebabCaseName}`
+
         fs.mkdirSync(folderPath, {
           recursive: true,
         })
 
         const filePath = `${folderPath}/code.mdx`
+
         fs.writeFileSync(filePath, file)
 
         prettify(filePath)
+
+        components.push(componentName)
 
         continue
       }
@@ -105,14 +121,41 @@ function splitIntoMultipleFiles() {
   // Move everything under ${cwd}/__tmpDocs to packages/next-docs/pages
   const tmpDocsPath = `${cwd}/__tmpDocs`
   const pagesPath = `${cwd}/packages/next-docs/pages`
+  const componentsMetaJson = `${cwd}/packages/next-docs/pages/components/_meta.json`
+  const hooksMetaJson = `${cwd}/packages/next-docs/pages/hooks/_meta.json`
 
   copyFolderRecursiveSync(`${tmpDocsPath}/components`, pagesPath)
   copyFolderRecursiveSync(`${tmpDocsPath}/hooks`, pagesPath)
+
+  // Update componentsMetaJson with the new paths
+  // Adds the components as kebab-case on keys and PascalCase on values
+  const componentsMetaUpdated = components.map((component) => ({
+    [component.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase()]: component,
+  }))
+
+  fs.writeFileSync(
+    componentsMetaJson,
+    JSON.stringify(componentsMetaUpdated, null, 2)
+  )
+
+  // Update hooksMetaJson with the new paths
+  // Adds the components as kebab-case on keys and camelCase on values
+  const hooksMetaUpdated = hooks.map((hook) => ({
+    [hook.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase()]: hook,
+  }))
+
+  // Write the updated componentsMetaJson
+  fs.writeFileSync(hooksMetaJson, JSON.stringify(hooksMetaUpdated, null, 2))
+
+  // TODO: Copy interfaces to their corresponding components folder
 }
 
+/**
+ * Prettify a file using the .prettierrc config
+ *
+ * @param {string} filePath
+ */
 function prettify(filePath) {
-  const prettierConfigPath = require.resolve('../.prettierrc')
-  const prettierConfig = require(prettierConfigPath)
   const prettierOptions = {
     ...prettierConfig,
     parser: 'markdown',
@@ -120,6 +163,7 @@ function prettify(filePath) {
 
   const file = fs.readFileSync(filePath, 'utf8')
   const formatted = prettier.format(file, prettierOptions)
+
   fs.writeFileSync(filePath, formatted)
 }
 
