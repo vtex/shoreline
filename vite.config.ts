149b4f478c1397c7bb97cb0f30a546097c1ce64a
matheusps <cout.matheusps@gmail.com>
2023-11-29 13:44:36 -0300

    refactor(vitest): move to vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
+  test: {
+    globals: false,
+    watch: false,
+    include: ['**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    setupFiles: ['./packages/test-utils/dist/setup.mjs'],
+    environment: 'jsdom',
+    css: false,
+  },
 })
