ca3fd976a2ac77e77a2412ca75e7ded4eaa72759
matheusps <cout.matheusps@gmail.com>
2023-12-01 19:21:30 -0300

    chore(csx-vanilla-extract): deprecate csx and vanilla-extract packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [react()],
   test: {
     globals: false,
     watch: false,
