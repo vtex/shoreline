4a336adea8160c3045ec2dac0095e96b5078341e
matheusps <cout.matheusps@gmail.com>
2023-12-01 19:10:59 -0300

    feat(locale): add react-aria i18n-provider

--- packages/date/src/calendar/calendar.tsx
+++ packages/date/src/calendar/calendar.tsx
@@ -3,7 +3,6 @@ import { useCalendar } from '@react-aria/calendar'
 import { createCalendar } from '@internationalized/date'
 import { useLocale, IconButton } from '@vtex/shoreline-components'
 import { IconCaretLeft, IconCaretRight } from '@vtex/shoreline-icons'
-import { I18nProvider } from '@react-aria/i18n'
 
 import { CalendarGrid } from './calendar-grid'
 import { CalendarProvider } from './calendar-provider'
@@ -24,34 +23,32 @@ export function Calendar(props: any) {
 
   return (
     <CalendarProvider store={store}>
-      <I18nProvider locale={locale}>
-        <div data-sl-calendar {...calendarProps}>
-          <div data-sl-calendar-header>
-            <IconButton
-              label="Previous month"
-              variant="tertiary"
-              disabled={prevButtonProps.isDisabled}
-              aria-label={prevButtonProps['aria-label']}
-              onClick={prevButtonProps.onPress as any}
-              onFocus={prevButtonProps.onFocusChange as any}
-            >
-              <IconCaretLeft />
-            </IconButton>
-            <h2 data-sl-calendar-title>{title}</h2>
-            <IconButton
-              label="Next month"
-              variant="tertiary"
-              disabled={nextButtonProps.isDisabled}
-              aria-label={nextButtonProps['aria-label']}
-              onClick={nextButtonProps.onPress as any}
-              onFocus={nextButtonProps.onFocusChange as any}
-            >
-              <IconCaretRight />
-            </IconButton>
-          </div>
-          <CalendarGrid />
+      <div data-sl-calendar {...calendarProps}>
+        <div data-sl-calendar-header>
+          <IconButton
+            label="Previous month"
+            variant="tertiary"
+            disabled={prevButtonProps.isDisabled}
+            aria-label={prevButtonProps['aria-label']}
+            onClick={prevButtonProps.onPress as any}
+            onFocus={prevButtonProps.onFocusChange as any}
+          >
+            <IconCaretLeft />
+          </IconButton>
+          <h2 data-sl-calendar-title>{title}</h2>
+          <IconButton
+            label="Next month"
+            variant="tertiary"
+            disabled={nextButtonProps.isDisabled}
+            aria-label={nextButtonProps['aria-label']}
+            onClick={nextButtonProps.onPress as any}
+            onFocus={nextButtonProps.onFocusChange as any}
+          >
+            <IconCaretRight />
+          </IconButton>
         </div>
-      </I18nProvider>
+        <CalendarGrid />
+      </div>
     </CalendarProvider>
   )
 }
