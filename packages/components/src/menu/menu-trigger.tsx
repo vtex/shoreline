c32884ee6690b916dc1908222d6dd51c0e176bb9
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2023-12-04 14:04:50 -0300

    feat(menu): add design qa requested changes

--- packages/components/src/menu/menu-trigger.tsx
+++ packages/components/src/menu/menu-trigger.tsx
@@ -1,16 +1,26 @@
 import type { ComponentPropsWithoutRef } from 'react'
 import React, { forwardRef } from 'react'
-import { MenuButton } from '@ariakit/react'
+import { MenuButton, useMenuContext } from '@ariakit/react'
+import invariant from 'tiny-invariant'
+
+import './menu-trigger.css'
 
 export const MenuTrigger = forwardRef<HTMLButtonElement, MenuTriggerProps>(
   function MenuTrigger(props, ref) {
     const { children, asChild = false, ...otherProps } = props
+
+    const store = useMenuContext()
+
+    invariant(store, 'You must use MenuTrigger under the MenuProvider context.')
+
+    const open = store.useState('open')
 
     return (
       <MenuButton
         ref={ref}
         data-sl-menu-trigger
         render={asChild && (children as any)}
+        data-active={open}
         {...otherProps}
       >
         {children}
