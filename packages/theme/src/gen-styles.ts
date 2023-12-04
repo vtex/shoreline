f64e8807c695c351f293f3224f3f9cc0c63af224
matheusps <cout.matheusps@gmail.com>
2023-12-04 15:51:02 -0300

    refactor(theme): replace getPreflight by preflight.css

--- packages/theme/src/gen-styles.ts
+++ packages/theme/src/gen-styles.ts
@@ -1,11 +1,11 @@
 import type { ShorelineConfig } from '@vtex/shoreline-utils'
 import { parseTokens } from '@vtex/shoreline-utils'
-import autoprefixer from 'autoprefixer'
-import nested from 'postcss-nested'
+import presetEnv from 'postcss-preset-env'
 import postcss from 'postcss'
 import { format } from 'prettier'
+import fse from 'fs-extra'
+import path from 'path'
 
-import { getPreflight } from './get-preflight'
 import { tokensToCssVariables } from './tokens-to-css-variables'
 
 /**
@@ -22,11 +22,21 @@ export async function genStyles(config: ShorelineConfig): Promise<Buffer> {
   })
 
   const cssVariablesCode = tokensToCssVariables(parsedTokens)
-  const baseCode = getPreflight(prefix) + cssVariablesCode
 
-  const { css: unformatedCss } = await postcss([autoprefixer, nested]).process(
-    baseCode
-  )
+  const preflightCode = fse
+    .readFileSync(path.resolve(__dirname, 'preflight.css'))
+    .toString()
+
+  const { css: unformatedCss } = await postcss([
+    presetEnv({
+      stage: 4,
+      features: {
+        'custom-media-queries': true,
+        'system-ui-font-family': true,
+        'nesting-rules': true,
+      },
+    }),
+  ]).process(cssVariablesCode + preflightCode)
 
   const css = await format(unformatedCss, {
     parser: 'css',
