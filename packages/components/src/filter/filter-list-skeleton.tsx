da1e49fbabfaf05e88650e35ac86dfbe2d170255
anitavincent <paes.anita@gmail.com>
2023-12-19 17:23:30 -0300

    feat(filter): add loading content for filter

---
+++ packages/components/src/filter/filter-list-skeleton.tsx
@@ -0,0 +1,46 @@
+import type { ComponentPropsWithoutRef } from 'react'
+import React, { forwardRef } from 'react'
+
+import './filter-list.css'
+import { Skeleton } from '../skeleton'
+import { Stack } from '../stack'
+
+/**
+ * Filter listbox
+ * @example
+ * <FilterProvider>
+ *  <FilterPopover>
+ *    <FilterList>
+ *      <FilterOption value="option">Option</FilterOption>
+ *    </FilterList>
+ *  </FilterPopover>
+ * </FilterProvider>
+ */
+export const FilterListSkeleton = forwardRef<
+  HTMLDivElement,
+  FilterListSkeletonProps
+>(function FilterListSkeleton(props, ref) {
+  return (
+    <Stack space="$space-4" ref={ref} {...props}>
+      <Skeleton
+        style={{
+          width: '8.75rem',
+          height: '1.25rem',
+          marginTop: 'var(--sl-space-2)',
+        }}
+      />
+      <Skeleton style={{ width: '10rem', height: '1.25rem' }} />
+      <Skeleton style={{ width: '7.5rem', height: '1.25rem' }} />
+      <Skeleton style={{ width: '8.75rem', height: '1.25rem' }} />
+      <Skeleton
+        style={{
+          width: '10rem',
+          height: '1.25rem',
+          marginBottom: 'var(--sl-space-2)',
+        }}
+      />
+    </Stack>
+  )
+})
+
+export type FilterListSkeletonProps = ComponentPropsWithoutRef<'div'>
