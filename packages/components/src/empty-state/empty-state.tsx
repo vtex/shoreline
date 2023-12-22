4a8e7250f6e86bfbd9f8234659f12782cb981af8
anitavincent <paes.anita@gmail.com>
2023-12-22 17:35:22 -0300

    feat(emptystate): make it composable

--- packages/components/src/empty-state/empty-state.tsx
+++ packages/components/src/empty-state/empty-state.tsx
@@ -1,39 +1,18 @@
-import type { ComponentPropsWithoutRef, ReactNode } from 'react'
+import type { ComponentPropsWithoutRef } from 'react'
 import React, { forwardRef } from 'react'
-import { useId } from '@vtex/shoreline-utils'
 
 export const EmptyState = forwardRef<HTMLDivElement, EmptyStateProps>(
   function EmptyState(props, ref) {
-    const { src, title, children, description, ...otherProps } = props
-    const titleId = useId()
+    const { size = 'medium', children, ...otherProps } = props
 
     return (
-      <div data-sl-empty-state ref={ref} {...otherProps}>
-        <div data-sl-empty-state-container>
-          {src ? (
-            <img
-              data-sl-empty-state-image
-              src={src}
-              alt=""
-              aria-describedby={titleId}
-            />
-          ) : null}
-          <div data-sl-empty-state-title id={titleId}>
-            {title}
-          </div>
-          {description ? (
-            <div data-sl-empty-state-description>{description}</div>
-          ) : null}
-          {children ? <div data-sl-empty-state-actions>{children}</div> : null}
-        </div>
+      <div data-sl-empty-state data-sl-size={size} ref={ref} {...otherProps}>
+        <div data-sl-empty-state-container>{children}</div>
       </div>
     )
   }
 )
 
-export interface EmptyStateProps
-  extends Omit<ComponentPropsWithoutRef<'div'>, 'title'> {
-  title: ReactNode
-  description?: ReactNode
-  src?: string
+export interface EmptyStateProps extends ComponentPropsWithoutRef<'div'> {
+  size?: 'small' | 'medium' | 'large'
 }
--- packages/components/src/empty-state/stories/empty-state.stories.tsx
+++ packages/components/src/empty-state/stories/empty-state.stories.tsx
@@ -1,39 +1,101 @@
 import '../empty-state.css'
 import './styles.css'
 import React from 'react'
-import illustration from './Illustration.png'
 
 import { EmptyState } from '../index'
 import { Button } from '../../button'
+import { Heading } from '../../heading'
+import { Slot } from '../../slot'
+import { Text } from '../../text'
+import { IconMagnifyingGlass, IconWarningCircle } from '../../../../icons/src'
+import {
+  Filter,
+  FilterPopover,
+  FilterProvider,
+  FilterTrigger,
+} from '../../filter'
+import { Popover } from '../../popover'
 
 export default {
   title: 'shoreline-components/empty-state',
+  argTypes: {
+    size: {
+      description: 'Size',
+      default: 'medium',
+      options: ['small', 'medium', 'large'],
+      control: { type: 'radio' },
+    },
+  },
 }
 
-export function Default() {
+export function Default(props) {
+  const { size } = props
+
   return (
     <div className="screen-container">
-      <EmptyState
-        src={illustration}
-        title="Title goes here"
-        description="Before you write the description here please visit the Shoreline documentation website to learn more about this componente and its usage."
-      >
-        <Button>Label</Button>
-        <Button variant="primary">Label</Button>
+      <EmptyState size={size}>
+        <Slot name="illustration">
+          <IconWarningCircle />
+        </Slot>
+        <Heading>Title goes here</Heading>
+        <Text>
+          Before you write the description here please visit the Shoreline
+          documentation website to learn more about this componente and its
+          usage.
+        </Text>
+        <Slot name="actions">
+          <Button>Label</Button>
+          <Button variant="primary">Label</Button>
+        </Slot>
       </EmptyState>
     </div>
   )
 }
 
-export function Table() {
+export function Simple(props) {
+  const { size } = props
+
   return (
     <div className="screen-container">
-      <EmptyState
-        title="No products created"
-        description="Create products to start selling in your store"
-      >
-        <Button variant="primary">Create product</Button>
+      <EmptyState size={size}>
+        <Heading>Title goes here</Heading>
+        <Slot name="actions">
+          <Button variant="primary">label</Button>
+        </Slot>
       </EmptyState>
     </div>
+  )
+}
+
+export function FilterError() {
+  return (
+    <FilterProvider>
+      <FilterTrigger>Status</FilterTrigger>
+      <Popover data-sl-filter-popover>
+        <EmptyState size="small" style={{ height: '18.25rem' }}>
+          <Slot name="illustration">
+            <IconWarningCircle color="red" />
+          </Slot>
+          <Heading>Something went wrong</Heading>
+          <Slot name="actions">
+            <Button>Try again</Button>
+          </Slot>
+        </EmptyState>
+      </Popover>
+    </FilterProvider>
+  )
+}
+
+export function FilterNotFound() {
+  return (
+    <Filter label="Country" defaultValue={[]} setSearchValue={(value) => {}}>
+      <EmptyState size="small" style={{ height: '15rem' }}>
+        <Slot name="illustration">
+          <IconMagnifyingGlass />
+        </Slot>
+        <Heading>No results found</Heading>
+        <Text>Try using different terms.</Text>
+      </EmptyState>
+    </Filter>
   )
 }
