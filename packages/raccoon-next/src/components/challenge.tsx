dda1181753bf65ce1e9a43a93de309a18df56c5d
marcelovicentegc <marcelovicentegc@gmail.com>
2024-03-19 18:42:45 -0300

    feat(raccoon-next): detect app base route
    the ability to detect the app base route enables developers to create
    one application that contains many entry points on different base routes

--- packages/raccoon-next/src/components/challenge.tsx
+++ packages/raccoon-next/src/components/challenge.tsx
@@ -51,23 +51,66 @@ function Router({ children }: PropsWithChildren) {
 
   // Routes that aren't declared on the routes property shouldn't be available here.
   useEffect(() => {
+    const { production, devUrl, prodUrl } = ctx
+
+    // Get pathname from devUrl and prodUrl
+    const [devPathname, prodPathname] = [devUrl, prodUrl].map((url) => {
+      if (url) {
+        const urlObj = new URL(url)
+        return urlObj.pathname
+      }
+
+      return '/'
+    })
+
+    // Compare them to make sure they match
+    if (devPathname !== prodPathname) {
+      // We shouldn't necessarily throw an error here, since the app might be in development
+      console.warn(
+        `The devUrl and prodUrl pathnames are different. devUrl: ${devPathname}, prodUrl: ${prodPathname}. Ensure that the pathnames for both environments are the same to guarantee consistency`
+      )
+    }
+
+    // Get the app base path based on the environment
+    const appBasePath = production ? prodPathname : devPathname
+
     // If the route is not internal (it's the base path), we can mount the app.
     // This is also a backward compatible check, since the path is only available
     // apps using the most recent version of the @vtex/raccoon-next package.
     if (!ctx.path) {
+      console.warn(
+        `${prodUrl} is using an outdated version of @vtex/raccoon-next. Please update the package to the latest version.`
+      )
       setIsMounted(true)
 
       return
     }
 
-    const internalPath = ctx.path?.replace(ADMIN_BASE_PATH, '') || '/'
+    /**
+     * Remaining path is the path that comes after the base path
+     *
+     * @example
+     * base path relative to the admin -> `/admin/rocket`
+     * current path relative to the admin -> `/admin/rocket/nextjs-internal-route`
+     * remaining path which should map back to the next.js app -> `/nextjs-internal-route`
+     */
+    const remainingPath = ctx.path?.replace(ctx.basePath, '')
+    /**
+     * The internal path is the path that the app should be mounted on
+     */
+    const internalPath =
+      (appBasePath === '/' ? '' : appBasePath) + remainingPath || appBasePath
+
     const ioAndNextjsRoutingMatch = internalPath === router.asPath
 
     if (!ioAndNextjsRoutingMatch) {
+      // If the internal path doesn't match the current path, we navigate to the internal path
+      // to sync the admin shell with the app's internal state
       router.push(internalPath)
     }
 
     if (ioAndNextjsRoutingMatch) {
+      // If the internal path matches the current path, we can mount the app
       setIsMounted(true)
     }
   }, [ctx.path, router.asPath])
