e87928e28cbd3636baf3a8a2e0371e3fbe624c97
marcelovicentegc <marcelovicentegc@gmail.com>
2024-03-27 22:56:23 +1300

    chore: migrate raccoon to vtex/admin-platform

--- packages/raccoon-next/src/admin-store.ts
+++
@@ -1,7 +0,0 @@
-import type { AdminContext } from './types'
-import { createStore } from './utils/store'
-
-/**
- * internal store
- */
-export const adminStore = createStore<AdminContext>()
--- packages/raccoon-next/src/tests/bootstrap.vitest.test.ts
+++
@@ -1,9 +0,0 @@
-import { vi, test, expect } from '@vtex/shoreline-test-utils'
-import { connect } from '../boostrap'
-
-test('connect function should listen for admin shell events', () => {
-  const addEventListener = vi.spyOn(window, 'addEventListener')
-
-  connect()
-  expect(addEventListener).toHaveBeenCalled()
-})
