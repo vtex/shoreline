1c0fa3e202b372918ad063989e23e4f9c16139e9
matheusps <cout.matheusps@gmail.com>
2024-05-28 10:33:00 -0300

    feat(cli): remove shoreline cli

--- packages/shoreline/src/scripts/dev-css.ts
+++ packages/shoreline/src/scripts/dev-css.ts
@@ -1,27 +1,34 @@
 import { bundle } from '@vtex/shoreline-css'
 import { watch } from 'chokidar'
 
+function build() {
+  bundle({
+    inputFile: 'src/themes/sunrise/styles.css',
+    outputFile: 'dist/themes/sunrise/styles.css',
+    banner: '@layer sl-reset, sl-base, sl-tokens, sl-components;',
+    browserslistQuery: 'last 1 versions',
+  })
+
+  bundle({
+    inputFile: 'src/themes/sunrise/styles-unlayered.css',
+    outputFile: 'dist/themes/sunrise/styles-unlayered.css',
+    browserslistQuery: 'last 1 versions',
+  })
+}
+
 function main() {
   console.log('Watching CSS files')
+
+  build()
 
   watch('**/*.css', {
     ignored: [
       'dist/themes/sunrise/styles.css',
       'dist/themes/sunrise/styles-unlayered.css',
     ],
+    ignoreInitial: false,
   }).on('change', (path) => {
-    bundle({
-      inputFile: 'src/themes/sunrise/styles.css',
-      outputFile: 'dist/themes/sunrise/styles.css',
-      banner: '@layer sl-reset, sl-base, sl-tokens, sl-components;',
-      browserslistQuery: 'last 1 versions',
-    })
-
-    bundle({
-      inputFile: 'src/themes/sunrise/styles-unlayered.css',
-      outputFile: 'dist/themes/sunrise/styles-unlayered.css',
-      browserslistQuery: 'last 1 versions',
-    })
+    build()
 
     console.log(`File ${path} has been changed`)
   })
