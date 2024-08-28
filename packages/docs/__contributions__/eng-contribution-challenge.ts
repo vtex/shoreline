4dc700682d59e8cd22fc50fb516ab9d5857f5cf0
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2024-08-28 11:08:26 -0300

    docs(eng-onsite): update challenge results

--- packages/docs/__contributions__/eng-contribution-challenge.ts
+++ packages/docs/__contributions__/eng-contribution-challenge.ts
@@ -22,27 +22,44 @@ export interface ChallengeStats {
 export const stats: ChallengeStats = {
   participantsStats: [
     {
+      username: 'lafray',
+      image:
+        'https://avatars.githubusercontent.com/u/36760180?u=66a13423e65c88a3fc82201a20ba4282a1f5224a&v=4',
+      stats: { pulls: 9, merged: 6, issues: 3, comments: 12, rate: 54 },
+    },
+    {
+      username: 'riccoutinho',
+      image:
+        'https://avatars.githubusercontent.com/u/44474987?u=6eb25971ea6e45c93be67934739d9a6b2e6ddf91&v=4',
+      stats: { pulls: 0, merged: 0, issues: 1, comments: 0, rate: 2 },
+    },
+    {
+      username: 'lucasaarcoverde',
+      image:
+        'https://avatars.githubusercontent.com/u/20579226?u=22c79f6ca441ee1d933d5b59326348295b6a9844&v=4',
+      stats: { pulls: 3, merged: 1, issues: 0, comments: 6, rate: 15 },
+    },
+    {
+      username: 'caineblood',
+      image: 'https://avatars.githubusercontent.com/u/79236147?v=4',
+      stats: { pulls: 0, merged: 0, issues: 0, comments: 1, rate: 1 },
+    },
+    {
       username: 'vaporwavie',
       image:
         'https://avatars.githubusercontent.com/u/11232965?u=f6a8f2a77e73a92ee944e06f34d4fe70c7c4e65a&v=4',
       stats: { pulls: 0, merged: 0, issues: 3, comments: 2, rate: 8 },
     },
     {
-      username: 'lucasaarcoverde',
-      image:
-        'https://avatars.githubusercontent.com/u/20579226?u=22c79f6ca441ee1d933d5b59326348295b6a9844&v=4',
-      stats: { pulls: 3, merged: 1, issues: 0, comments: 5, rate: 14 },
-    },
-    {
       username: 'viniciuslagedo',
       image:
         'https://avatars.githubusercontent.com/u/3369680?u=d926ebade88220c24869b425ecd0c5fb064c0c0d&v=4',
-      stats: { pulls: 6, merged: 4, issues: 3, comments: 2, rate: 32 },
+      stats: { pulls: 6, merged: 4, issues: 3, comments: 3, rate: 33 },
     },
     {
       username: 'beatrizmilhomem',
       image: 'https://avatars.githubusercontent.com/u/84085258?v=4',
-      stats: { pulls: 0, merged: 0, issues: 2, comments: 2, rate: 6 },
+      stats: { pulls: 0, merged: 0, issues: 2, comments: 13, rate: 17 },
     },
     {
       username: 'davicostalf',
@@ -66,7 +83,7 @@ export const stats: ChallengeStats = {
       username: 'kevinch',
       image:
         'https://avatars.githubusercontent.com/u/2573602?u=7ce617df54906dc3d4dab85291c390f26a1c5950&v=4',
-      stats: { pulls: 1, merged: 0, issues: 2, comments: 4, rate: 10 },
+      stats: { pulls: 1, merged: 1, issues: 2, comments: 4, rate: 13 },
     },
     {
       username: 'matheusps',
@@ -75,10 +92,10 @@ export const stats: ChallengeStats = {
       stats: { pulls: 0, merged: 0, issues: 0, comments: 3, rate: 3 },
     },
     {
-      username: 'lafray',
+      username: 'BeatrizAlbino',
       image:
-        'https://avatars.githubusercontent.com/u/36760180?u=66a13423e65c88a3fc82201a20ba4282a1f5224a&v=4',
-      stats: { pulls: 6, merged: 6, issues: 0, comments: 8, rate: 38 },
+        'https://avatars.githubusercontent.com/u/64042309?u=5081e29d8ee28a25d1e259d079e883622ffb590b&v=4',
+      stats: { pulls: 0, merged: 0, issues: 0, comments: 1, rate: 1 },
     },
     {
       username: 'felipepowlist',
@@ -87,11 +104,11 @@ export const stats: ChallengeStats = {
       stats: { pulls: 0, merged: 0, issues: 0, comments: 1, rate: 1 },
     },
   ],
-  participants: 11,
-  issues: 16,
-  pulls: 16,
-  merged: 11,
-  comments: 30,
+  participants: 14,
+  issues: 20,
+  pulls: 19,
+  merged: 12,
+  comments: 49,
 }
 
 export function getWinners() {
