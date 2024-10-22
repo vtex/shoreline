992c8cfb369e71fde9d36e870f2e9617d41cc718
Matheus P. Silva <cout.matheusps@gmail.com>
2024-10-22 18:29:41 -0300

    chore(next): remove the next package

--- packages/next/src/tabs/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './tab-panel'
-export * from './tab'
-export * from './tab-provider'
-export * from './tab-list'
--- packages/next/src/tabs/tab-list.tsx
+++
@@ -1,5 +0,0 @@
-import { TabList as NextTabList } from '@vtex/shoreline'
-import type { TabListProps as NextTabListProps } from '@vtex/shoreline'
-
-export { NextTabList }
-export type { NextTabListProps }
