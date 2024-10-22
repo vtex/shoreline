6eaba0fa703e75b0071312110f7b62e7223ba396
Matheus P. Silva <cout.matheusps@gmail.com>
2024-10-22 16:56:43 -0300

    style(biome): update biome to lint css code

--- packages/shoreline/src/components/label/stories/show.stories.tsx
+++ packages/shoreline/src/components/label/stories/show.stories.tsx
@@ -10,9 +10,11 @@ export function Show() {
     <Stack>
       <Label>Label</Label>
       <Label optional>Label</Label>
-      <Stack role="group" space="$space-1">
-        <Label htmlFor="example">Label for input</Label>
-        <input id="example" />
+      <Stack asChild space="$space-1">
+        <fieldset>
+          <Label htmlFor="example">Label for input</Label>
+          <input id="example" />
+        </fieldset>
       </Stack>
     </Stack>
   )
