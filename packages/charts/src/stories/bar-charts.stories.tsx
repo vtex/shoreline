cb6cc26e4ca5ae30bf50742228e5d64c1285ab2c
theo-rodrigues <theo.evangelista.rodrigues@ccc.ufcg.edu.br>
2025-04-25 11:55:40 -0300

    ci(chromatic): add CI file config for chromatic
    add CI file for visual tests

--- packages/charts/src/stories/bar-charts.stories.tsx
+++ packages/charts/src/stories/bar-charts.stories.tsx
@@ -74,6 +74,7 @@ export const MultiSeriesMidi: Story = {
       series: [
         { data: [3, 2, 3, 4, 1, 9, 7], name: 'Series 1' },
         { data: [1, 4, 2, 3, 1, 5, 9], name: 'Series 2' },
+        { data: [23, 43, 1, 23, 5, 2], name: 'teste' },
       ],
     },
     chartConfig: { type: 'bar', gap: 2 },
