694ee11852a9f3995c9e5f51c74be745ad3fe830
Jose Ramon Severo Alves <jose.ramon@splab.ufcg.edu.br>
2025-05-07 11:36:07 -0300

    test(charts): adds a new example in general for use in handbook

--- packages/charts/src/stories/general-charts.stories.tsx
+++ packages/charts/src/stories/general-charts.stories.tsx
@@ -170,3 +170,45 @@ export const TextBetweenSync: Story = {
     )
   },
 }
+
+export const Handbook: Story = {
+  render: () => {
+    const data1: number[] = []
+    for (let i = 0; i < 12; i++) {
+      data1.push(random(12, false))
+    }
+    console.log(data1.toString())
+    return (
+      <>
+        <ChartCompositor
+          charts={[
+            {
+              series: { data: [3, 4, 3, 11, 5, 8, 6, 6, 10, 6, 8, 6] },
+              chartConfig: { type: 'bar' },
+            },
+            {
+              series: { data: [8, 1, 7, 2, 6, 1, 10, 1, 1, 7, 8, 1] },
+              chartConfig: { type: 'bar' },
+            },
+            {
+              series: { data: [1, 8, 7, 11, 7, 3, 6, 7, 0, 11, 6, 8] },
+              chartConfig: { type: 'line' },
+            },
+          ]}
+          tooltip={{ type: 'bar' }}
+          group="sync"
+        />
+        <Chart
+          option={{
+            series: [
+              { data: [5, 6, 1, -12, 2, -7, 10, 7, 10, 9, 3, 5] },
+              { data: [10, -4, 6, 11, 9, 10, -6, 2, -8, -4, 3, 4] },
+            ],
+          }}
+          chartConfig={{ type: 'bar', gap: 2 }}
+          group="sync"
+        />
+      </>
+    )
+  },
+}
