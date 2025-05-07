56c322e922bfe67f78c3521cc1088412302cf86e
Jose Ramon Severo Alves <jose.ramon@splab.ufcg.edu.br>
2025-05-07 13:42:32 -0300

    fix(charts): fixing example on storybook

--- packages/charts/src/stories/general-charts.stories.tsx
+++ packages/charts/src/stories/general-charts.stories.tsx
@@ -173,37 +173,53 @@ export const TextBetweenSync: Story = {
 
 export const Handbook: Story = {
   render: () => {
-    const data1: number[] = []
-    for (let i = 0; i < 12; i++) {
-      data1.push(random(12, false))
-    }
-    console.log(data1.toString())
     return (
       <>
         <ChartCompositor
           charts={[
             {
-              series: { data: [3, 4, 3, 11, 5, 8, 6, 6, 10, 6, 8, 6] },
+              series: {
+                data: [3, 4, 3, 11, 5, 8, 6, 6, 10, 6, 8, 6],
+                name: 'Data 1',
+              },
               chartConfig: { type: 'bar' },
             },
             {
-              series: { data: [8, 1, 7, 2, 6, 1, 10, 1, 1, 7, 8, 1] },
+              series: {
+                data: [8, 1, 7, 2, 6, 1, 10, 1, 1, 7, 8, 1],
+                name: 'Data 2',
+              },
               chartConfig: { type: 'bar' },
             },
             {
-              series: { data: [1, 8, 7, 11, 7, 3, 6, 7, 0, 11, 6, 8] },
+              series: {
+                data: [1, 8, 7, 11, 7, 3, 6, 7, 0, 11, 6, 8],
+                name: 'Data 3',
+              },
               chartConfig: { type: 'line' },
             },
           ]}
           tooltip={{ type: 'bar' }}
+          xAxis={{
+            data: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I', 'J', 'K', 'L'],
+          }}
           group="sync"
         />
         <Chart
           option={{
             series: [
-              { data: [5, 6, 1, -12, 2, -7, 10, 7, 10, 9, 3, 5] },
-              { data: [10, -4, 6, 11, 9, 10, -6, 2, -8, -4, 3, 4] },
+              {
+                data: [5, 6, 1, -12, 2, -7, 10, 7, 10, 9, 3, 5],
+                name: 'Data 4',
+              },
+              {
+                data: [10, -4, 6, 11, 9, 10, -6, 2, -8, -4, 3, 4],
+                name: 'Data 5',
+              },
             ],
+            xAxis: {
+              data: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I', 'J', 'K', 'L'],
+            },
           }}
           chartConfig={{ type: 'bar', gap: 2 }}
           group="sync"
