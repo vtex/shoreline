860ed12c8db12bb87419e8ae324da4564526ec8a
Jose Ramon Severo Alves <jose.ramon@splab.ufcg.edu.br>
2025-06-03 11:15:09 -0300

    docs(charts): remake the area chart stories

--- packages/charts/src/stories/line-charts.stories.tsx
+++ packages/charts/src/stories/line-charts.stories.tsx
@@ -142,7 +142,10 @@ export const MultipleGradientArea: Story = {
     return (
       <Chart
         style={{ height: 550 }}
-        series={[{ data: [1, 2, 3, 2, 1] }, { data: [3, 2, 5, 2, 3] }]}
+        series={[
+          { data: [6, 5, 2, 7, 1, 10, 6, 5, 1, 5, 6, 3, 8, 10, 4] },
+          { data: [4, 7, 5, 2, 6, 2, 6, 2, 6, 8, 4, 5, 9, 1, 5] },
+        ]}
         chartConfig={{ type: 'line', variant: 'area' }}
       />
     )
@@ -155,8 +158,14 @@ export const StackedArea: Story = {
       <Chart
         style={{ height: 550 }}
         series={[
-          { data: [1, 2, 3, 2, 1], stack: 'stack' },
-          { data: [3, 2, 5, 2, 3], stack: 'stack' },
+          {
+            data: [3, 4, 8, 8, 5, 4, 10, 9, 2, 7, 6, 3, 9, 2, 3],
+            stack: 'stack',
+          },
+          {
+            data: [5, 4, 5, 9, 6, 8, 7, 2, 8, 5, 3, 9, 4, 9, 5],
+            stack: 'stack',
+          },
         ]}
         chartConfig={{ type: 'line', variant: 'area' }}
       />
