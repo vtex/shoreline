c4d24d5aeae096e5a0075d1cfc1b2ca59917ee41
Jose Ramon Severo Alves <jose.ramon@splab.ufcg.edu.br>
2025-06-10 14:27:49 -0300

    fix(charts): make stories using the args and not using render

--- packages/charts/src/stories/area-charts.stories.tsx
+++ packages/charts/src/stories/area-charts.stories.tsx
@@ -17,34 +17,28 @@ export const Basic: Story = {
 }
 
 export const MultipleGradientArea: Story = {
-  render: () => {
-    return (
-      <Chart
-        style={{ height: 550 }}
-        series={[
-          { data: [6, 5, 2, 7, 1, 10, 6, 5, 1, 5, 6, 3, 8, 10, 4] },
-          { data: [4, 7, 5, 2, 6, 2, 6, 2, 6, 8, 4, 5, 9, 1, 5] },
-        ]}
-        chartConfig={{ type: 'area' }}
-      />
-    )
+  args: {
+    style: { height: 550 },
+    series: [
+      { data: [6, 5, 2, 7, 1, 10, 6, 5, 1, 5, 6, 3, 8, 10, 4], name: 'Data 1' },
+      { data: [4, 7, 5, 2, 6, 2, 6, 2, 6, 8, 4, 5, 9, 1, 5], name: 'Data 2' },
+    ],
+    zoom: true,
+    chartConfig: { type: 'area' },
   },
 }
 
 export const StackedArea: Story = {
-  render: () => {
-    return (
-      <Chart
-        style={{ height: 550 }}
-        series={[
-          { data: [3, 4, 8, 8, 5, 4, 10, 9, 2, 7, 6, 3, 9, 2, 3] },
-          { data: [5, 4, 5, 9, 6, 8, 7, 2, 8, 5, 3, 9, 4, 9, 5] },
-          { data: [5, 4, 5, 9, 6, 8, 7, 2, 8, 5, 3, 9, 4, 9, 5] },
-          { data: [5, 4, 5, 9, 6, 8, 7, 2, 8, 5, 3, 9, 4, 9, 5] },
-          { data: [5, 4, 5, 9, 6, 8, 7, 2, 8, 5, 3, 9, 4, 9, 5] },
-        ]}
-        chartConfig={{ type: 'area', variant: 'stacked' }}
-      />
-    )
+  args: {
+    style: { height: 550 },
+    series: [
+      { data: [3, 4, 8, 8, 5, 4, 10, 9, 2, 7, 6, 3, 9, 2, 3] },
+      { data: [5, 4, 5, 9, 6, 8, 7, 2, 8, 5, 3, 9, 4, 9, 5] },
+      { data: [5, 4, 5, 9, 6, 8, 7, 2, 8, 5, 3, 9, 4, 9, 5] },
+      { data: [5, 4, 5, 9, 6, 8, 7, 2, 8, 5, 3, 9, 4, 9, 5] },
+      { data: [5, 4, 5, 9, 6, 8, 7, 2, 8, 5, 3, 9, 4, 9, 5] },
+    ],
+    chartConfig: { type: 'area', variant: 'stacked' },
+    zoom: true,
   },
 }
