794f7bfb743ae1b89d6f9fbad70e5c641d7a5872
leofarias-cpu <l3onardo.almeid4@gmail.com>
2025-06-12 15:05:36 -0300

    docs(chart): added temporary chart examples

--- packages/docs/examples/chart-area.tsx
+++ packages/docs/examples/chart-area.tsx
@@ -3,12 +3,13 @@ import { Chart } from '@vtex/shoreline-charts'
 export default function Example() {
   return (
     <Chart
-      style={{ height: 550 }}
+      style={{ height: 300 }}
       series={[
-        { data: [6, 5, 2, 7, 1, 10, 6, 5, 1, 5], name: 'Serie 1' },
-        { data: [4, 7, 5, 2, 6, 2, 6, 2, 6, 8], name: 'Serie 2' },
+        { data: [70, 40, 55, 35, 39, 30, 25], name: 'Product A' },
+        { data: [0, 0, 5, 10, 25, 45, 55], name: 'Product B' },
       ]}
-      chartConfig={{ type: 'line', variant: 'area' }}
+      xAxis={{ data: ['一', '二', '三', '四', '五', '六', '七'] }}
+      chartConfig={{ type: 'area', variant: 'overlapping' }}
     />
   )
 }
