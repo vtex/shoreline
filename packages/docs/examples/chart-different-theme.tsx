53336e1ca7239bf8f29901d2bb30c41d6459b885
leofarias-cpu <l3onardo.almeid4@gmail.com>
2025-06-13 15:53:11 -0300

    docs(chart): fixes some examples

--- packages/docs/examples/chart-different-theme.tsx
+++ packages/docs/examples/chart-different-theme.tsx
@@ -3,7 +3,7 @@ import { Chart } from '@vtex/shoreline-charts'
 export default function Example() {
   return (
     <Chart
-      chartConfig={{ type: 'bar', variant: 'vertical' }}
+      chartConfig={{ type: 'bar', variant: 'vertical', gap: 1 }}
       xAxis={{ data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] }}
       series={{
         data: [1, 2, 3, 4, 5, 6, 7],
@@ -11,10 +11,7 @@ export default function Example() {
           borderRadius: [10, 10, 0, 0],
         },
       }}
-      option={{
-        barGap: '5%',
-        color: ['#D95030'],
-      }}
+      option={{ color: ['#D95030'] }}
     />
   )
 }
