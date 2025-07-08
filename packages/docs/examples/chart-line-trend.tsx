536d7d82e8db58b748d70b041fbb096805c4ddb8
Jose Ramon Severo Alves <jose.ramon@splab.ufcg.edu.br>
2025-07-08 08:53:06 -0300

    docs(chart-line): make the chart has one more line in gray-6

--- packages/docs/examples/chart-line-trend.tsx
+++ packages/docs/examples/chart-line-trend.tsx
@@ -4,10 +4,18 @@ export default function Example() {
   return (
     <Chart
       chartConfig={{ type: 'line' }}
-      series={{
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        lineStyle: { type: 'dashed' },
-      }}
+      series={[
+        {
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          lineStyle: { type: 'dashed' },
+          name: 'Trend',
+        },
+        {
+          data: [820, 832, 920, 834, 990, 830, 910],
+          color: '#adadad',
+          name: 'Solid',
+        },
+      ]}
       xAxis={{ data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] }}
       style={{ height: 550 }}
     />
