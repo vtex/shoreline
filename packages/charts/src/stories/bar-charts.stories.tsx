cb641e6d37268c5a12299b496da312688ed49bfc
Jose Ramon Severo Alves <jose.ramon@splab.ufcg.edu.br>
2025-07-10 16:28:14 -0300

    fix(charts): fix names and where should be the story of animation test

--- packages/charts/src/stories/bar-charts.stories.tsx
+++ packages/charts/src/stories/bar-charts.stories.tsx
@@ -3,6 +3,8 @@ import type { StoryObj } from '@storybook/react'
 import { useRef } from 'react'
 import type EChartsReact from 'echarts-for-react'
 import { compactNumber } from '../utils/format'
+import { Grid } from '@vtex/shoreline'
+import type { SeriesOption } from 'echarts'
 
 const Bar = {
   title: 'Charts/bar',
@@ -160,5 +162,78 @@ export const Animation: Story = {
       left: 'center',
     },
     chartConfig: { type: 'bar' },
+  },
+}
+
+export const AnimationSpeeds: Story = {
+  render: () => {
+    const data: SeriesOption[] = [
+      {
+        name: 'Email',
+        data: [120, 132, 101, 134, 90, 230, 210],
+      },
+      {
+        name: 'Union Ads',
+        data: [220, 182, 191, 234, 290, 330, 260],
+      },
+      {
+        name: 'Video Ads',
+        data: [150, 232, 201, 154, 190, 330, 410],
+      },
+      {
+        name: 'Direct',
+        data: [320, 332, 301, 334, 390, 330, 320],
+      },
+    ]
+    return (
+      <Grid columns={'50% 50%'} rows={'50% 50%'} style={{ width: 1000 }}>
+        <Chart
+          xAxis={{ data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] }}
+          title={{ text: '100' }}
+          chartConfig={{ type: 'bar' }}
+          series={data}
+          option={{
+            animationDurationUpdate: (idx) => idx * 100,
+            animationDelayUpdate: (idx) => idx * 50,
+          }}
+          group="A"
+        />
+        <Chart
+          title={{
+            text: '50',
+          }}
+          xAxis={{ data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] }}
+          chartConfig={{ type: 'bar' }}
+          series={data}
+          option={{
+            animationDurationUpdate: (idx) => idx * 50,
+            animationDelayUpdate: (idx) => idx * 50,
+          }}
+          group="A"
+        />
+        <Chart
+          title={{ text: '25' }}
+          xAxis={{ data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] }}
+          chartConfig={{ type: 'bar' }}
+          series={data}
+          option={{
+            animationDurationUpdate: (idx) => idx * 25,
+            animationDelayUpdate: (idx) => idx * 50,
+          }}
+          group="A"
+        />
+        <Chart
+          title={{ text: '15' }}
+          xAxis={{ data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] }}
+          chartConfig={{ type: 'bar' }}
+          series={data}
+          option={{
+            animationDurationUpdate: (idx) => idx * 15,
+            animationDelayUpdate: (idx) => idx * 50,
+          }}
+          group="A"
+        />
+      </Grid>
+    )
   },
 }
