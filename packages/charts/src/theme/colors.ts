6d7666bbef202dbbfedbc0b17c9ec209e1e1a956
leofarias-cpu <l3onardo.almeid4@gmail.com>
2025-07-14 17:20:34 -0300

    fix(chart-area): fix area colors

--- packages/charts/src/theme/colors.ts
+++ packages/charts/src/theme/colors.ts
@@ -29,7 +29,7 @@ export const defaultAreaColors = [
   colors['--sl-pink-11'],
   colors['--sl-teal-11'],
 ]
-// blue-9 e purple-10
+
 export const defaultChartColorConfig = {
   lineColor: colors['--sl-gray-6'],
   textSoft: colors['--sl-gray-9'],
