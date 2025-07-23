578d0b955d9121d18ba6e5d010fe39938dc87ce8
leofarias-cpu <l3onardo.almeid4@gmail.com>
2025-07-23 13:49:51 -0300

    docs(charts): restyled some examples for more consistency

--- packages/docs/examples/chart-area.tsx
+++ packages/docs/examples/chart-area.tsx
@@ -3,13 +3,13 @@ import { Chart } from '@vtex/shoreline-charts'
 export default function Example() {
   return (
     <Chart
-      style={{ height: 300 }}
+      chartConfig={{ type: 'area', variant: 'overlapping' }}
+      xAxis={{ data: ['1º', '2º', '3º', '4º', '5º', '6º', '7º'] }}
       series={[
         { data: [70, 40, 55, 35, 39, 30, 25], name: 'Product A' },
         { data: [0, 0, 5, 10, 25, 45, 55], name: 'Product B' },
       ]}
-      xAxis={{ data: ['1º', '2º', '3º', '4º', '5º', '6º', '7º'] }}
-      chartConfig={{ type: 'area', variant: 'overlapping' }}
+      style={{ height: 300 }}
     />
   )
 }
--- packages/docs/examples/chart-line-trend.tsx
+++ packages/docs/examples/chart-line-trend.tsx
@@ -4,6 +4,7 @@ export default function Example() {
   return (
     <Chart
       chartConfig={{ type: 'line' }}
+      xAxis={{ data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] }}
       series={[
         {
           data: [820, 932, 901, 934, 1290, 1330, 1320],
@@ -16,7 +17,6 @@ export default function Example() {
           name: 'Solid',
         },
       ]}
-      xAxis={{ data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] }}
       style={{ height: 550 }}
     />
   )
