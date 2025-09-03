5a410ac9508f1204c9c200636dd5e2a7d7063b8a
José Ramon Severo Alves <jose.ramon_ext@vtex.com>
2025-09-03 14:47:37 -0300

    fix(chart-colors): fix function docs and name

--- packages/charts/src/config.ts
+++ packages/charts/src/config.ts
@@ -2,14 +2,14 @@ import { defaultColorShade } from './theme/colors'
 import * as echarts from 'echarts'
 
 /**
- * Adds a hover color mapping to the setted color.
+ * Adds a hover color mapping to the setted color in the legend colors component.
  * It doesn't allow to overwrite a mapping.
  *
  * @param {string} color - The base color key to which the hover state will be mapped.
  * @param {string} hover - The color value to be used when hovering over elements with the base color.
  * @example addHoverToColors('--sl-color-red-5', '--sl-color-red-6');
  */
-export function addHoverToColors(color: string, hover: string) {
+export function addHoverToLegendColor(color: string, hover: string) {
   if (Object.keys(defaultColorShade).includes(color)) return
   defaultColorShade[color] = hover
 }
