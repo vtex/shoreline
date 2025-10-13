65b02b3aa3515c2f7bacdf19e5268a1c1b42cfca
Vtex <l3onardo.almeid4@gmail.com>
2025-10-13 17:09:14 -0300

    docs(chart): removed 'finish loading' button in loading examples

--- packages/docs/examples/chart-area-loading.tsx
+++ packages/docs/examples/chart-area-loading.tsx
@@ -1,38 +1,13 @@
-import { Button } from '@vtex/shoreline'
 import { Chart } from '@vtex/shoreline-charts'
-import { useState } from 'react'
 
 export default function Example() {
-  const [currentSeries, setCurrentSeries] = useState<any>([])
-  const [isLoading, setIsLoading] = useState(true)
-
   return (
-    <>
-      <Button
-        style={{ marginBottom: 'var(--sl-space-2)' }}
-        variant={'primary'}
-        onClick={() => {
-          if (isLoading) {
-            setCurrentSeries([
-              { data: [70, 40, 55, 35, 39, 30, 25], name: 'Product A' },
-              { data: [0, 0, 5, 10, 25, 45, 55], name: 'Product B' },
-            ])
-            setIsLoading(false)
-          } else {
-            setIsLoading(true)
-          }
-        }}
-      >
-        {isLoading ? 'Finish Loading' : 'Unload'}
-      </Button>
-      <Chart
-        chartConfig={{ type: 'area', variant: 'overlapping' }}
-        xAxis={{ data: ['1º', '2º', '3º', '4º', '5º', '6º', '7º'] }}
-        series={currentSeries}
-        style={{ height: 300 }}
-        loading={isLoading}
-        loadingOptions={{ numLines: 8 }}
-      />
-    </>
+    <Chart
+      chartConfig={{ type: 'bar', variant: 'vertical' }}
+      xAxis={{ data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] }}
+      series={{}}
+      loading={true}
+      loadingOptions={{ numLines: 8 }}
+    />
   )
 }
