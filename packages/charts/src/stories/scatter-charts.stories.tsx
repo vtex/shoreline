63adacbf2ba7ddd6700b472177b7b2ba7eae86c7
José Ramon Severo Alves <jose.ramon_ext@vtex.com>
2025-10-13 22:25:06 -0300

    test(chart-scatter): test in storybook for the new variant

--- packages/charts/src/stories/scatter-charts.stories.tsx
+++ packages/charts/src/stories/scatter-charts.stories.tsx
@@ -8,50 +8,39 @@ export default {
 
 type Story = StoryObj<typeof Chart>
 
-const data1: any[] = [
-  [0, 19],
-  [1, 53],
-  [2, 27],
-  [3, 4],
-  [4, 27],
-  [5, 36],
-  [6, 94],
-  [7, 80],
-  [8, 31],
-  [9, 57],
-]
-
-const data2: [number, number][] = [
-  [0, 2],
-  [1, 4],
-  [2, 92],
-  [3, 68],
-  [4, 2],
-  [5, 3],
-  [6, 67],
-  [7, 3],
-  [8, 69],
-  [9, 39],
-]
-
-const data1alternative: any[] = [
-  [0, 19, 4],
-  [1, 53, 6],
-  [2, 27, 8],
-  [3, 4, 18],
-  [4, 27, 5],
-  [5, 36, 3],
-  [6, 94, 9],
-  [7, 80, 7],
-  [8, 31, 5],
-  [9, 57, 10],
-]
-
 export const Basic: Story = {
   args: {
     series: [
-      { data: data1, name: 'Serie 1' },
-      { data: data2, name: 'Serie 2' },
+      {
+        data: [
+          [0, 19],
+          [1, 53],
+          [2, 27],
+          [3, 4],
+          [4, 27],
+          [5, 36],
+          [6, 94],
+          [7, 80],
+          [8, 31],
+          [9, 57],
+        ],
+        name: 'Serie 1',
+      },
+      {
+        data: [
+          [0, 2],
+          [1, 4],
+          [2, 92],
+          [3, 68],
+          [4, 2],
+          [5, 3],
+          [6, 67],
+          [7, 3],
+          [8, 69],
+          [9, 39],
+        ],
+        name: 'Serie 2',
+      },
     ],
     chartConfig: { type: 'scatter' },
   },
@@ -60,8 +49,36 @@ export const Basic: Story = {
 export const Tooltip2Variant: Story = {
   args: {
     series: [
-      { data: data1alternative, name: 'Serie 1' },
-      { data: data2, name: 'Serie 2' },
+      {
+        data: [
+          [0, 19, 4],
+          [1, 53, 6],
+          [2, 27, 8],
+          [3, 4, 18],
+          [4, 27, 5],
+          [5, 36, 3],
+          [6, 94, 9],
+          [7, 80, 7],
+          [8, 31, 5],
+          [9, 57, 10],
+        ],
+        name: 'Serie 1',
+      },
+      {
+        data: [
+          [0, 2],
+          [1, 4],
+          [2, 92],
+          [3, 68],
+          [4, 2],
+          [5, 3],
+          [6, 67],
+          [7, 3],
+          [8, 69],
+          [9, 39],
+        ],
+        name: 'Serie 2',
+      },
     ],
     chartConfig: { type: 'scatter', variant: 'tooltip2' },
   },
@@ -73,11 +90,39 @@ export const UsingChartCompositor = {
       <ChartCompositor
         charts={[
           {
-            series: { data: data1, name: 'Serie 1' },
+            series: {
+              data: [
+                [0, 19],
+                [1, 53],
+                [2, 27],
+                [3, 4],
+                [4, 27],
+                [5, 36],
+                [6, 94],
+                [7, 80],
+                [8, 31],
+                [9, 57],
+              ],
+              name: 'Serie 1',
+            },
             chartConfig: { type: 'scatter' },
           },
           {
-            series: { data: data2, name: 'Serie 2' },
+            series: {
+              data: [
+                [0, 2],
+                [1, 4],
+                [2, 92],
+                [3, 68],
+                [4, 2],
+                [5, 3],
+                [6, 67],
+                [7, 3],
+                [8, 69],
+                [9, 39],
+              ],
+              name: 'Serie 2',
+            },
             chartConfig: { type: 'line' },
           },
         ]}
