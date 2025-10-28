eb3326cac7f15b656568b3439da75eb4ea80c7f9
Vtex <l3onardo.almeid4@gmail.com>
2025-10-28 10:39:21 -0300

    docs: add docs for compactNumber

--- packages/charts/src/utils/format.ts
+++ packages/charts/src/utils/format.ts
@@ -1,3 +1,9 @@
+/**
+ * Abbreviates large numbers. For example:
+ * - 100.000 = 100K
+ * - 1.000.000 = 1M
+ * - 1.000.000.000 = 1B
+ */
 export const compactNumber = (number: number) => {
   const formatter = new Intl.NumberFormat('en-US', {
     notation: 'compact',
